45403aad095e3b5580ff5e3dce82ffdae02657ab
Chad Myers <chad.myers@gmail.com>
2010-01-12 14:49:11 -0600

    Added .gitignore file, fixed a Mono problem in HtmlTag.cs

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,7 +1,10 @@
-using System.Reflection;
 
-[assembly:
-    AssemblyCopyright("Copyright 2008 Chad Myers, Jeremy D. Miller, Joshua Flanagan, et al. All rights reserved.")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
-[assembly: AssemblyProduct("FubuMVC")]
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyrightAttribute("Copyright 2008 Chad Myers, Jeremy D. Miller, Joshua Flanagan, et al. All rights reserved.")]
+[assembly: AssemblyVersionAttribute("0.1.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.1.0.0")]
+[assembly: AssemblyProductAttribute("FubuMVC")]
+		
