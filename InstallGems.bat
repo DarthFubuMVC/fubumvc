32a4d01c5ef6de349c88c0d3bf0bc7c8ba165cec
Chad Myers <chad.myers@gmail.com>
2010-01-18 16:41:55 -0600

    Added albacore dependency, added zip task for teamcity artifacts

--- InstallGems.bat
+++ InstallGems.bat
@@ -1,11 +1,14 @@
 @ECHO *** Installing Rake
-@call gem install rake --include-dependencies
+@call gem install rake --no-rdoc --no-ri
 
 @ECHO *** Installing ActiveRecord
-@call gem install activerecord --include-dependencies
+@call gem install activerecord --no-rdoc --no-ri
 
 @ECHO *** Installing RubyZip
-@call gem install rubyzip --include-dependencies
+@call gem install rubyzip --no-rdoc --no-ri
 
 @ECHO *** Installing Rails
-@call gem install rails --include-dependencies
+@call gem install rails --no-rdoc --no-ri
+
+@ECHO *** Installing Albacore (build support tools)
+@call gem install albacore --no-rdoc --no-ri
