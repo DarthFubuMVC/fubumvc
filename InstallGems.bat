3ddf788f2fa2370b53952598eafb97575ba3b536
joshuaflanagan <joshuaflanagan@gmail.com>
2010-01-19 14:56:14 -0600

    Adding buildin information to CommonAssemblyInfo

--- InstallGems.bat
+++ InstallGems.bat
@@ -1,14 +1,8 @@
 @ECHO *** Installing Rake
 @call gem install rake --no-rdoc --no-ri
 
-@ECHO *** Installing ActiveRecord
-@call gem install activerecord --no-rdoc --no-ri
-
 @ECHO *** Installing RubyZip
 @call gem install rubyzip --no-rdoc --no-ri
-
-@ECHO *** Installing Rails
-@call gem install rails --no-rdoc --no-ri
 
 @ECHO *** Installing Albacore (build support tools)
 @call gem install albacore --no-rdoc --no-ri
--- rakefile.rb
+++ rakefile.rb
@@ -5,7 +5,7 @@ include FileTest
 require 'albacore'
 
 RESULTS_DIR = "results"
-BUILD_NUMBER = "0.1.0."
+BUILD_NUMBER = "0.1.0."  + (ENV["BUILD_NUMBER"].nil? ? '0' : ENV["BUILD_NUMBER"].to_s)
 PRODUCT = "FubuMVC"
 COPYRIGHT = 'Copyright 2008 Chad Myers, Jeremy D. Miller, Joshua Flanagan, et al. All rights reserved.';
 COMMON_ASSEMBLY_INFO = 'src/CommonAssemblyInfo.cs';
@@ -21,28 +21,20 @@ task :all => [:default]
 desc "**Default**, compiles and runs tests"
 task :default => [:compile, :unit_test]
 
-desc "Displays a list of tasks"
-task :help do
-  taskHash = Hash[*(`rake.cmd -T`.split(/\n/).collect { |l| l.match(/rake (\S+)\s+\#\s(.+)/).to_a }.collect { |l| [l[1], l[2]] }).flatten] 
- 
-  indent = "                          "
-  
-  puts "rake #{indent}#Runs the 'default' task"
-  
-  taskHash.each_pair do |key, value|
-    if key.nil?  
-      next
-    end
-    puts "rake #{key}#{indent.slice(0, indent.length - key.length)}##{value}"
-  end
-end
-
 desc "Update the version information for the build"
-task :version do
-  builder = AsmInfoBuilder.new(BUILD_NUMBER, {'Product' => PRODUCT, 'Copyright' => COPYRIGHT})
-  buildNumber = builder.buildnumber
-  puts "The build number is #{buildNumber}"
-  builder.write COMMON_ASSEMBLY_INFO  
+assemblyinfotask :version do |asm|
+  asm.version = BUILD_NUMBER
+  asm.custom_attributes :InformationalVersion => BUILD_NUMBER
+  asm.product_name = PRODUCT
+  asm.copyright = COPYRIGHT
+  asm.output_file = COMMON_ASSEMBLY_INFO
+
+  begin
+    commit = `git log -1 --pretty=format:%H`
+  rescue
+    commit = "git unavailable"
+  end
+  asm.description = "Commit: " + commit
 end
 
 desc "Prepares the working directory for a new build"
