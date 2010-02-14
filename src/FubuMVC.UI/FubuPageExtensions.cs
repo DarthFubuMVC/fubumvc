f0a94e63cc5701c7138951b745ca743c8ffeea04
Nieve <nievegoor@gmail.com>
2010-02-14 01:07:10 +0100

    FormTag, TextBoxFor & FormFor FubuPage extension methods

--- src/FubuMVC.UI/FubuPageExtensions.cs
+++ src/FubuMVC.UI/FubuPageExtensions.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Linq.Expressions;
+using System.Text;
 using FubuMVC.Core.Runtime;
+using FubuMVC.Core.Urls;
 using FubuMVC.Core.View;
 using FubuMVC.UI.Configuration;
-using FubuMVC.UI.Forms;
 using FubuMVC.UI.Tags;
 using HtmlTags;
 using FubuMVC.Core.Util;
+using FubuMVC.Core;
+using System.Linq;
 
 namespace FubuMVC.UI
 {
@@ -17,21 +20,6 @@ namespace FubuMVC.UI
             var generator = page.Get<TagGenerator<T>>();
             generator.Model = page.Model;
             return generator;
-        }
-
-        public static void Partial<TInputModel>(this IFubuPage page) where TInputModel : class
-        {
-            page.Get<IPartialFactory>().BuildPartial(typeof (TInputModel)).InvokePartial();
-        }
-
-        public static HtmlTag LinkTo<TInputModel>(this IFubuPage page) where TInputModel : class, new()
-        {
-            return page.LinkTo(new TInputModel());
-        }
-        
-        public static HtmlTag LinkTo(this IFubuPage page, object inputModel)
-        {
-            return new LinkTag("", page.Urls.UrlFor(inputModel));
         }
 
         public static HtmlTag InputFor<T>(this IFubuPage<T> page, Expression<Func<T, object>> expression) where T : class
@@ -55,5 +43,35 @@ namespace FubuMVC.UI
             return page.Get<IElementNamingConvention>().GetName(typeof (T), expression.ToAccessor());
         }
 
+        public static TextboxTag TextBoxFor<T>(this IFubuPage<T> page, Expression<Func<T, object>> expression) where T : class
+        {
+            string name = ElementNameFor(page, expression);
+            string value = page.Model.ValueOrDefault(expression).ToString();
+            return new TextboxTag(name, value);
+        }
+
+        public static FormTag FormFor(this IFubuPage page)
+        {
+            return new FormTag();
+        }
+
+        public static FormTag FormFor<TInputModel>(this IFubuPage page) where TInputModel : new()
+        {
+            string url = page.Get<IUrlRegistry>().UrlFor(new TInputModel());
+            url = UrlContext.GetFullUrl(url);
+            return new FormTag(url);
+        }
+
+        public static FormTag FormFor<TInputModel>(this IFubuPage page, TInputModel model)
+        {
+            string url = page.Get<IUrlRegistry>().UrlFor(model);
+            url = UrlContext.GetFullUrl(url);
+            return new FormTag(url);
+        }
+
+        public static string EndForm(this IFubuPage page)
+        {
+            return "</form>";
+        }
     }
 }
