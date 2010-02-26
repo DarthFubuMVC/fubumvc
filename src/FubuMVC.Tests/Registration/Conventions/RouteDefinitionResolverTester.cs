89daa3c3cd1792ef125e3f1d164da5cf4a90cb50
Chad Myers <chad.myers@gmail.com>
2010-02-26 10:40:16 -0600

    Added some tests around the new IgnoreControllerNamesEntirely functionality

--- src/FubuMVC.Tests/Registration/Conventions/RouteDefinitionResolverTester.cs
+++ src/FubuMVC.Tests/Registration/Conventions/RouteDefinitionResolverTester.cs
@@ -146,6 +146,14 @@ namespace FubuMVC.Tests.Registration.Conventions
         }
 
         [Test]
+        public void build_route_when_ignoring_controller_names_entirely()
+        {
+            resolver.DefaultUrlPolicy.IgnoreControllerNamesEntirely = true;
+            var route = buildRoute(x => x.SomeMethod(null)).ShouldBeOfType<RouteDefinition<RouteInputModel>>();
+            route.Pattern.ShouldEqual("fubumvc/tests/registration/conventions/somemethod/{Name}/{Age}");
+        }
+
+        [Test]
         public void build_route_when_ignoring_the_controller_namespace_entirely()
         {
             resolver.DefaultUrlPolicy.IgnoreControllerNamespaceEntirely = true;
