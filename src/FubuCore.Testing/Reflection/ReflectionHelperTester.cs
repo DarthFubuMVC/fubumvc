2a6029ea4dd04cb3a4aedf2f9d0f5827facc1c66
Nieve <nievegoor@gmail.com>
2010-04-15 21:30:53 +0200

    FubuCore.Reflection unit tests first batch

--- src/FubuCore.Testing/Reflection/ReflectionHelperTester.cs
+++ src/FubuCore.Testing/Reflection/ReflectionHelperTester.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq.Expressions;
 using System.Reflection;
 using FubuCore.Reflection;
 using NUnit.Framework;
@@ -17,12 +18,14 @@ namespace FubuCore.Testing.Reflection
             PropertyInfo second = ReflectionHelper.GetProperty<ChildTarget>(x => x.GrandChild);
             PropertyInfo birthday = ReflectionHelper.GetProperty<GrandChildTarget>(x => x.BirthDay);
 
-            _chain = new PropertyChain(new[] {top, second, birthday});
+            _chain = new PropertyChain(new[] { top, second, birthday });
+            _expression = (t => t.Child);
         }
 
         #endregion
 
         private PropertyChain _chain;
+        private Expression<Func<Target, ChildTarget>> _expression;
 
         public class Target
         {
@@ -34,6 +37,7 @@ namespace FubuCore.Testing.Reflection
         {
             public int Age { get; set; }
             public GrandChildTarget GrandChild { get; set; }
+            public GrandChildTarget SecondGrandChild { get; set; }
         }
 
         public class GrandChildTarget
@@ -66,6 +70,24 @@ namespace FubuCore.Testing.Reflection
             };
 
             accessor.GetValue(target).ShouldEqual(DateTime.Today);
+        }
+
+        [Test]
+        public void can_get_accessor_from_lambda_expression()
+        {
+            Accessor accessor = ReflectionHelper.GetAccessor((LambdaExpression)_expression);
+            accessor.Name.ShouldEqual("Child");
+            accessor.DeclaringType.ShouldEqual(typeof (Target));
+        }
+
+        [Test]
+        public void can_get_member_expression_from_lambda()
+        {
+            MemberExpression expected = Expression.Property(Expression.Parameter(typeof(Target), "t"), "Child");
+            MemberExpression memberExpression = ((LambdaExpression) _expression).GetMemberExpression(false);
+            memberExpression.Expression.Type.ShouldEqual(expected.Expression.Type);
+            memberExpression.Type.ShouldEqual(expected.Type);
+            memberExpression.ToString().ShouldEqual("t.Child");
         }
 
         [Test]
@@ -125,17 +147,70 @@ namespace FubuCore.Testing.Reflection
             Accessor chain2 = ReflectionHelper.GetAccessor<Target>(t => t.Child.GrandChild.BirthDay);
             Accessor chain3 = ReflectionHelper.GetAccessor<Target>(t => t.Child.GrandChild);
             Accessor chain4 = ReflectionHelper.GetAccessor<Target>(t => t.Child.GrandChild.Name);
+            Accessor chain5 = ReflectionHelper.GetAccessor<Target>(t => t.Child.SecondGrandChild.BirthDay);
+            //PropertyChain nullChain = null;
 
             chain1.ShouldEqual(chain2);
             chain1.ShouldNotEqual(chain3);
             chain1.ShouldNotEqual(chain4);
+            chain1.ShouldNotEqual(chain5);
+            chain1.Equals(null).ShouldBeFalse();
+            ((PropertyChain)chain1).Equals((PropertyChain)null).ShouldBeFalse();
+            ((PropertyChain)chain1).Equals((PropertyChain)chain1).ShouldBeTrue();
+            ((PropertyChain)chain1).Equals(1).ShouldBeFalse();
+            chain1.ShouldEqual(chain1);
+
         }
 
         [Test]
-        public void PropertyChain_can_get_the_name()
+        public void propertyChain_hashcode()
+        {
+            var chain = (PropertyChain)ReflectionHelper.GetAccessor<Target>(t => t.Child.Age);
+            chain.GetHashCode().ShouldBeGreaterThan(0);
+        }
+
+        [Test]
+        public void propertyChain_can_get_inner_property()
+        {
+            var chain = (PropertyChain)ReflectionHelper.GetAccessor<Target>(t => t.Child.Age);
+            chain.InnerProperty.ShouldBeTheSameAs(typeof (ChildTarget).GetProperty("Age"));
+        }
+
+        [Test]
+        public void propertyChain_can_get_child_accessor()
+        {
+            Accessor expected = ReflectionHelper.GetAccessor<Target>(t => t.Child.GrandChild.Name);
+            ReflectionHelper.GetAccessor<Target>(t => t.Child.GrandChild).
+                GetChildAccessor<GrandChildTarget>(t => t.Name).ShouldEqual(expected);
+        }
+
+        [Test]
+        public void propertyChain_can_get_the_name()
         {
             ReflectionHelper.GetAccessor<Target>(t => t.Child.GrandChild.BirthDay).Name.ShouldEqual(
                 "ChildGrandChildBirthDay");
+        }
+
+        [Test]
+        public void propertyChain_can_get_owner_type()
+        {
+            ReflectionHelper.GetAccessor<Target>(t => t.Child.Age).OwnerType.ShouldEqual(typeof (ChildTarget));
+        }
+
+        [Test]
+        public void propertyChain_can_get_properties_names()
+        {
+            string[] names = ReflectionHelper.GetAccessor<Target>(t => t.Child.Age).PropertyNames;
+            names.ShouldHaveCount(2);
+            names.ShouldHave(n => n == "Child");
+            names.ShouldHave(n => n == "Age");
+        }
+
+        [Test]
+        public void propertyChain_toString_returns_graph()
+        {
+            ReflectionHelper.GetAccessor<Target>(t => t.Child.GrandChild.Name.Length).
+                ToString().ShouldEqual("FubuCore.Testing.Reflection.ReflectionHelperTester+TargetChild.GrandChild.Name");
         }
 
         [Test]
