a829c4f305e0a9cbb37a0830dbf5dd400133718d
jeremydmiller <jeremydmiller@yahoo.com>
2010-04-27 14:43:47 -0500

    removing push.bat

--- push.bat
+++
@@ -1,9 +0,0 @@
-copy src\FubuMVC.Tests\bin\Debug\FubuMVC.Core.* C:\svb\Blue\tools\fubu
-copy src\FubuMVC.Tests\bin\Debug\FubuCore.* C:\svn\Blue\tools\fubu
-copy src\FubuMVC.Tests\bin\Debug\FubuMVC.UI.* C:\svn\Blue\tools\fubu
-copy src\FubuMVC.Tests\bin\Debug\FubuMVC.StructureMap.* C:\svn\Blue\tools\fubu
-copy src\FubuMVC.Tests\bin\Debug\HtmlTags.* C:\svn\Blue\tools\fubu
-
-
-copy src\FubuMVC.Tests\bin\Debug\HtmlTags.* C:\git\StoryTeller\tools\HtmlTags
-copy src\FubuMVC.Tests\bin\Debug\FubuCore.* C:\git\StoryTeller\tools\HtmlTags
