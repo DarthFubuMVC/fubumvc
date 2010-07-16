0582b235add19578f3060d854f870255e1ba35ca
joshuaflanagan <joshuaflanagan@gmail.com>
2010-07-16 15:57:05 -0500

    Remove unused ConverterExpression

--- src/FubuCore/Binding/ConverterFamily.cs
+++ src/FubuCore/Binding/ConverterFamily.cs
@@ -3,23 +3,6 @@ using System.Reflection;
 
 namespace FubuCore.Binding
 {
-    public class ConverterExpression
-    {
-        private readonly Predicate<PropertyInfo> _matches;
-        private readonly Action<ConverterFamily> _register;
-
-        public ConverterExpression(Predicate<PropertyInfo> matches, Action<ConverterFamily> register)
-        {
-            _matches = matches;
-            _register = register;
-        }
-
-        public void Use(Func<IValueConverterRegistry, PropertyInfo, ValueConverter> builder)
-        {
-            _register(new ConverterFamily(_matches, builder));
-        }
-    }
-
     public class ConverterFamily : IConverterFamily
     {
         private readonly Func<IValueConverterRegistry, PropertyInfo, ValueConverter> _builder;
