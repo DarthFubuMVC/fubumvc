b02bd41a1f836bdb53d39226306a362fbc5ba386
Jeremy Miller <jeremydmiller@yahoo.com>
2010-08-17 16:08:46 -0500

    authorization behavior

--- src/FubuMVC.Core/Security/Classes.cs
+++ src/FubuMVC.Core/Security/Classes.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using FubuMVC.Core.Behaviors;
 using FubuMVC.Core.Runtime;
 
 namespace FubuMVC.Core.Security
@@ -15,15 +16,23 @@ namespace FubuMVC.Core.Security
             _precedence = precedence;
         }
 
+        public int Precedence
+        {
+            get { return _precedence; }
+        }
+
         public static AuthorizationRight None = new AuthorizationRight(3);
         public static AuthorizationRight Allow = new AuthorizationRight(2);
         public static AuthorizationRight Deny = new AuthorizationRight(1);
 
         public static AuthorizationRight Combine(IEnumerable<AuthorizationRight> rights)
         {
-            return rights.Any() 
-                ? rights.OrderBy(x => x._precedence).First() 
-                : AuthorizationRight.None;
+            if (!rights.Any())
+            {
+                return AuthorizationRight.None;
+            }
+
+            return rights.OrderBy(x => x.Precedence).First(); 
         }
 
         public static AuthorizationRight CombineRights(params AuthorizationRight[] rights)
@@ -80,4 +89,34 @@ namespace FubuMVC.Core.Security
     }
     **********************************************************************************************/
 
+
+    public class AuthorizationBehavior : BasicBehavior
+    {
+        private readonly IAuthorizationFailureHandler _failureHandler;
+        private readonly IFubuRequest _request;
+        private readonly IEnumerable<IAuthorizationPolicy> _policies;
+
+        public AuthorizationBehavior(IAuthorizationFailureHandler failureHandler, IFubuRequest request, IEnumerable<IAuthorizationPolicy> policies) : base(PartialBehavior.Executes)
+        {
+            _failureHandler = failureHandler;
+            _request = request;
+            _policies = policies;
+        }
+
+        protected override DoNext performInvoke()
+        {
+            var rights = _policies.Select(x => x.RightsFor(_request));
+            var access = AuthorizationRight.Combine(rights);
+
+            if (access == AuthorizationRight.Allow)
+            {
+                return DoNext.Continue;
+            }
+
+
+            _failureHandler.Handle();
+
+            return DoNext.Stop;
+        }
+    }
 }
