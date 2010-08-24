7c5f2732191b54f7412c57dbf2457de052dc3ada
Jeremy Miller <jeremydmiller@yahoo.com>
2010-08-24 14:29:06 -0500

    convenience method on AuthorizationNode

--- src/FubuMVC.Core/Security/AuthorizationNode.cs
+++ src/FubuMVC.Core/Security/AuthorizationNode.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using FubuCore;
 using FubuMVC.Core.Registration.Nodes;
 using FubuMVC.Core.Registration.ObjectGraph;
 using System.Linq;
@@ -31,6 +32,11 @@ namespace FubuMVC.Core.Security
 
 
             return allow;
+        }
+
+        public IEnumerable<string> AllowedRoles()
+        {
+            return _policies.Items.Where(x => x.Value is AllowRole).Select(x => x.Value.As<AllowRole>().Role);
         }
 
         // To any followers here, there definitely needs to be more stuff here, but roles are
