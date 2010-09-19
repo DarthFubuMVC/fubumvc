73655f4549cab664031925869ab2c9e2e0d21d9a
RobertTheGrey <robertgreyling@cozwecan.com>
2010-09-19 20:27:50 +0100

    Reworked the SparkView in prep for modifying render behaviour

--- src/Spark.Web.FubuMVC/ViewCreation/SparkView.cs
+++ src/Spark.Web.FubuMVC/ViewCreation/SparkView.cs
@@ -9,11 +9,19 @@ using Spark;
 
 namespace Spark.Web.FubuMVC.ViewCreation
 {
-    public abstract class SparkView : SparkView<object>
+    public abstract class SparkView : SparkViewBase, ISparkView, IFubuPage
     {
+        private readonly Cache<Type, object> _services = new Cache<Type, object>();
         private string _siteRoot;
 
+        public SparkView()
+        {
+            _services.OnMissing = type => { return ServiceLocator.GetInstance(type); };
+        }
+
         public IResourcePathManager ResourcePathManager { get; set; }
+        string IFubuPage.ElementPrefix { get; set; }
+        public IServiceLocator ServiceLocator { get; set; }
 
         public string SiteRoot
         {
@@ -22,7 +30,7 @@ namespace Spark.Web.FubuMVC.ViewCreation
                 var context = Get<HttpContextBase>();
                 if (_siteRoot == null)
                 {
-                    var appPath =  context.Request.ApplicationPath;
+                    var appPath = context.Request.ApplicationPath;
                     if (string.IsNullOrEmpty(appPath) || string.Equals(appPath, "/"))
                     {
                         _siteRoot = string.Empty;
@@ -39,67 +47,39 @@ namespace Spark.Web.FubuMVC.ViewCreation
         {
             return ResourcePathManager.GetResourcePath(SiteRoot, path);
         }
-
+        public string H(object value)
+        {
+            return HttpUtility.HtmlEncode(value.ToString());
+        }
+        public object HTML(object value)
+        {
+            return value;
+        }
+        public T Get<T>()
+        {
+            return (T)_services[typeof(T)];
+        }
+        public T GetNew<T>()
+        {
+            return ServiceLocator.GetInstance<T>();
+        }
+        public IUrlRegistry Urls
+        {
+            get { return Get<IUrlRegistry>(); }
+        }
     }
 
-    public abstract class SparkView<TModel> : AbstractSparkView, ISparkView, IFubuPage<TModel> where TModel : class
+    public abstract class SparkView<TModel> : SparkView, IFubuPage<TModel> where TModel : class
     {
-        private readonly Cache<Type, object> _services = new Cache<Type, object>();
-        
-        public SparkView()
-        {
-            _services.OnMissing = type => { return ServiceLocator.GetInstance(type); };
-        }                                                                                   
-
         public TModel Model { get; private set; }
 
         public void SetModel(IFubuRequest request)
         {
             Model = request.Get<TModel>();
         }
-
         public void SetModel(object model)
         {
             throw new NotImplementedException();
-        }
-
-        public string H(object value)
-        {
-            return HttpUtility.HtmlEncode(value.ToString());
-        }
-
-        public object HTML(object value)
-        {
-            return value;
-        }
-
-        public object Eval(string expression)
-        {
-            return "Only need to implement for anonymous type support";
-        }
-
-        public string Eval(string expression, string format)
-        {
-            return "Only need to implement for anonymous type support";
-        }
-
-        string IFubuPage.ElementPrefix { get; set; }
-
-        public IServiceLocator ServiceLocator { get; set; }
-
-        public T Get<T>()
-        {
-            return (T)_services[typeof(T)];
-        }
-
-        public T GetNew<T>()
-        {
-            return ServiceLocator.GetInstance<T>();
-        }
-
-        public IUrlRegistry Urls
-        {
-            get { return Get<IUrlRegistry>(); }
         }
     }
 }
