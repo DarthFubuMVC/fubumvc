3996fbc9e704382a5a49506ad5c31fef934dea1d
Jeremy Miller <jeremydmiller@yahoo.com>
2010-09-20 10:54:56 -0500

    changes to TagFactory to make the DegradingAccessElementBuilder work correctly

--- src/FubuMVC.UI/Tags/TagFactory.cs
+++ src/FubuMVC.UI/Tags/TagFactory.cs
@@ -39,22 +39,41 @@ namespace FubuMVC.UI.Tags
 
         private Func<ElementRequest, HtmlTag> resolveCreator(AccessorDef accessorDef)
         {
-            TagBuilder initialCreator = _sources.FirstValue(x => x.CreateInitial(accessorDef));
-            if (initialCreator == null)
-            {
-                throw new FubuException(3000, "Html Conventions have no tag builder for {0}.{1}", accessorDef.ModelType.FullName, accessorDef.Accessor.Name);
-            }
+            // TODO -- going to be largely rewritten soon-ish
+            //TagBuilder initialCreator = _sources.FirstValue(x => x.CreateInitial(accessorDef));
+            //if (initialCreator == null)
+            //{
+            //    throw new FubuException(3000, "Html Conventions have no tag builder for {0}.{1}", accessorDef.ModelType.FullName, accessorDef.Accessor.Name);
+            //}
 
             TagModifier[] modifiers =
                 _modifiers.Select(x => x.CreateModifier(accessorDef)).Where(x => x != null).ToArray();
 
             return request =>
             {
-                HtmlTag tag = initialCreator(request);
+                //HtmlTag tag = initialCreator(request);
+                var tag = buildTag(request);
+                
                 modifiers.Each(x => x(request, tag));
 
                 return tag;
             };
+        }
+
+        private HtmlTag buildTag(ElementRequest request)
+        {
+            var accessorDef = request.ToAccessorDef();
+
+            foreach (var builder in _sources)
+            {
+                var creator = builder.CreateInitial(accessorDef);
+                if (creator == null) continue;
+
+                var tag = creator(request);
+                if (tag != null) return tag;
+            }
+
+            throw new FubuException(3000, "Html Conventions have no tag builder for {0}.{1}", accessorDef.ModelType.FullName, accessorDef.Accessor.Name);
         }
 
         public HtmlTag Build(ElementRequest request)
