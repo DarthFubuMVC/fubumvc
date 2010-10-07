e07890db67b0d31816b62e8a17c8b8cf658882f6
Jeremy Miller <jeremydmiller@yahoo.com>
2010-10-07 10:46:14 -0500

    added the filtered content extension

---
+++ src/FubuMVC.Tests/UI/Extensibility/FilteredContentExtensionTester.cs
@@ -0,0 +1,47 @@
+using System;
+using FubuMVC.Core.View;
+using FubuMVC.UI.Extensibility;
+using NUnit.Framework;
+using Rhino.Mocks;
+using System.Linq;
+
+namespace FubuMVC.Tests.UI.Extensibility
+{
+    [TestFixture]
+    public class FilteredContentExtensionTester : InteractionContext<FilteredContentExtension<FilteredModel>>
+    {
+        private IFubuPage<FilteredModel> thePage;
+        private Func<IFubuPage<FilteredModel>, bool> theFilter;
+        private object[] theInnerExtensionObjects;
+
+        protected override void beforeEach()
+        {
+            thePage = MockFor<IFubuPage<FilteredModel>>();
+            theInnerExtensionObjects = new object[]{"<div/>", "<div />"};
+            MockFor<IContentExtension<FilteredModel>>().Stub(x => x.GetExtensions(thePage)).Return(theInnerExtensionObjects);
+
+            theFilter = MockFor<Func<IFubuPage<FilteredModel>, bool>>();
+        }
+
+        [Test]
+        public void if_the_filter_returns_false_return_an_empty_enumerable()
+        {
+            theFilter.Stub(x => x.Invoke(thePage)).Return(false);
+
+            ClassUnderTest.GetExtensions(thePage).Any().ShouldBeFalse();
+        }
+
+        [Test]
+        public void if_the_filter_returns_try_return_the_inner_enumerable()
+        {
+            theFilter.Stub(x => x.Invoke(thePage)).Return(true);
+
+            ClassUnderTest.GetExtensions(thePage).ShouldBeTheSameAs(theInnerExtensionObjects);
+        }
+    }
+
+    public class FilteredModel
+    {
+        
+    }
+}
---
+++ src/FubuMVC.UI/Extensibility/FilteredContentExtension.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using FubuMVC.Core.View;
+
+namespace FubuMVC.UI.Extensibility
+{
+    public class FilteredContentExtension<T> : IContentExtension<T> where T : class
+    {
+        private readonly Func<IFubuPage<T>, bool> _filter;
+        private readonly IContentExtension<T> _inner;
+
+        public FilteredContentExtension(Func<IFubuPage<T>, bool> filter, IContentExtension<T> inner)
+        {
+            _filter = filter;
+            _inner = inner;
+        }
+
+        public IEnumerable<object> GetExtensions(IFubuPage<T> page)
+        {
+            return _filter(page) ? _inner.GetExtensions(page) : new object[0];
+        }
+    }
+}
