03ba520b469dde174b052ef1bac7186023a100e9
jmarnold <rncodepoet@gmail.com>
2010-11-08 22:56:34 -0600

    modified hello world app to include validation

--- src/FubuMVC.HelloWorld/Global.asax.cs
+++ src/FubuMVC.HelloWorld/Global.asax.cs
@@ -1,70 +1,13 @@
-﻿using FubuMVC.Core;
-using FubuMVC.HelloWorld.Controllers.Home;
-using FubuMVC.HelloWorld.Controllers.Products;
-using FubuMVC.HelloWorld.Services;
-using FubuMVC.StructureMap;
-using FubuMVC.StructureMap.Bootstrap;
-using FubuMVC.UI;
-using FubuValidation;
-using FubuValidation.Registration;
-using Microsoft.Practices.ServiceLocation;
-using StructureMap;
+﻿using System.Web;
+using System.Web.Routing;
 
 namespace FubuMVC.HelloWorld
 {
-    public class Global : FubuStructureMapApplication
+    public class Global : HttpApplication
     {
-        protected override void InitializeValidation()
+        protected void Application_Start()
         {
-            Validator.Initialize<HelloWorldValidationRegistry>();
-            ServiceLocator.SetLocatorProvider(() => new StructureMapServiceLocator(ObjectFactory.Container));
-        }
-
-        public override FubuRegistry GetMyRegistry()
-        {
-            return new HelloWorldFubuRegistry();
-        }
-
-        protected override void InitializeStructureMap(IInitializationExpression ex)
-        {
-            ex.For<IHttpSession>().Use<CurrentHttpContextSession>();
-            ex.For<IValidationProvider>().Use(() => Validator.Provider);
-            ex.For<IValidationQuery>().Use(() => Validator.Model);
-        }
-    }
-
-    public class HelloWorldFubuRegistry : FubuRegistry
-    {
-        public HelloWorldFubuRegistry()
-        {
-            IncludeDiagnostics(true);
-
-            Applies.ToThisAssembly();
-
-            Actions
-                .IncludeTypesNamed(x => x.EndsWith("Controller"));
-
-            Routes
-                .IgnoreControllerNamespaceEntirely()
-                .IgnoreMethodSuffix("Command")
-                .IgnoreMethodSuffix("Query")
-                .ConstrainToHttpMethod(action => action.Method.Name.EndsWith("Command"), "POST")
-                .ConstrainToHttpMethod(action => action.Method.Name.StartsWith("Query"), "GET");
-
-            Views
-                .TryToAttach(x=>
-                {
-                    x.by_ViewModel_and_Namespace_and_MethodName();
-                    x.by_ViewModel_and_Namespace();
-                    x.by_ViewModel();
-                });
-
-            
-            this.HtmlConvention<SampleHtmlConventions>();
-
-            RegisterPartials(x => x.For<Product>().Use<ProductPartial>());
-
-            HomeIs<HomeInputModel>();
+            FubuStructureMapBootstrapper.Bootstrap(RouteTable.Routes);
         }
     }
 }
---
+++ src/FubuMVC.HelloWorld/HelloWorldFubuRegistry.cs
@@ -0,0 +1,42 @@
+﻿using FubuMVC.Core;
+using FubuMVC.HelloWorld.Controllers.Home;
+using FubuMVC.HelloWorld.Controllers.Products;
+using FubuMVC.UI;
+
+namespace FubuMVC.HelloWorld
+{
+    public class HelloWorldFubuRegistry : FubuRegistry
+    {
+        public HelloWorldFubuRegistry()
+        {
+            IncludeDiagnostics(true);
+
+            Applies.ToThisAssembly();
+
+            Actions
+                .IncludeTypesNamed(x => x.EndsWith("Controller"));
+
+            Routes
+                .IgnoreControllerNamespaceEntirely()
+                .IgnoreMethodSuffix("Command")
+                .IgnoreMethodSuffix("Query")
+                .ConstrainToHttpMethod(action => action.Method.Name.EndsWith("Command"), "POST")
+                .ConstrainToHttpMethod(action => action.Method.Name.StartsWith("Query"), "GET");
+
+            Views
+                .TryToAttach(x=>
+                                 {
+                                     x.by_ViewModel_and_Namespace_and_MethodName();
+                                     x.by_ViewModel_and_Namespace();
+                                     x.by_ViewModel();
+                                 });
+
+            
+            this.HtmlConvention<SampleHtmlConventions>();
+
+            RegisterPartials(x => x.For<Product>().Use<ProductPartial>());
+
+            HomeIs<HomeInputModel>();
+        }
+    }
+}
