3cfcb46f6c33ec47694b9416feb08beb75a1edde
Jeremy Miller <jeremydmiller@yahoo.com>
2010-11-14 16:48:11 -0600

    smoothing out the FubuMVC.UI consolidation by moving extension methods to expressions on FubuRegistry

---
+++ src/FubuMVC.Core/FubuRegistry.UserInterfaceExpressions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using FubuCore;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.UI;
+using FubuMVC.Core.UI.Configuration;
+using FubuMVC.Core.UI.Diagnostics;
+
+namespace FubuMVC.Core
+{
+    public partial class FubuRegistry
+    {
+        public void UseDefaultHtmlConventions()
+        {
+            Policies.Add<HtmlConventionCompiler>();
+            includeHtmlDiagnostics();
+        }
+
+        public void HtmlConvention<T>() where T : HtmlConventionRegistry, new()
+        {
+            HtmlConvention(new T());
+            includeHtmlDiagnostics();
+        }
+
+        public void HtmlConvention(HtmlConventionRegistry conventions)
+        {
+            Services(x => x.AddService(conventions));
+
+            Policies.Add<HtmlConventionCompiler>();
+            includeHtmlDiagnostics();
+        }
+
+        public void HtmlConvention(Action<HtmlConventionRegistry> configure)
+        {
+            var conventions = new HtmlConventionRegistry();
+            configure(conventions);
+
+            HtmlConvention(conventions);
+            includeHtmlDiagnostics();
+        }
+
+        private void includeHtmlDiagnostics()
+        {
+            Import<HtmlDiagnosticsRegistry>(string.Empty);
+        }
+
+        public void StringConversions<T>() where T : DisplayConversionRegistry, new()
+        {
+            var conversions = new T();
+
+            addStringConversions(conversions);
+            includeHtmlDiagnostics();
+        }
+
+        private void addStringConversions(DisplayConversionRegistry conversions)
+        {
+            var policy = new LambdaConfigurationAction(graph =>
+            {
+                graph.Services.SetServiceIfNone(new Stringifier());
+                var stringifier = graph.Services.FindAllValues<Stringifier>().First();
+
+
+                conversions.Configure(stringifier);
+            });
+            Policies.Add(policy);
+        }
+
+        public void StringConversions(Action<DisplayConversionRegistry> configure)
+        {
+            var conversions = new DisplayConversionRegistry();
+            configure(conversions);
+
+            addStringConversions(conversions);
+            includeHtmlDiagnostics();
+        }
+    }
+}
