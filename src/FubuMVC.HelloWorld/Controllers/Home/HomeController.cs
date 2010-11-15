8806c136d679241f44ea5c7cda5bb9d087807bbb
Chad Myers <chad.myers@gmail.com>
2010-11-15 11:15:00 -0600

    Added support for multiple file uploads in a single request
    - Added pass-through converter for HttpFileCollectionWrapper
    - Added example in the HomeController and Home.aspx view to demonstrate usage
    - Had to fix a assembly reference issue in FubuMVC.HelloWorld.csproj when compiling in 3.5 mode

--- src/FubuMVC.HelloWorld/Controllers/Home/HomeController.cs
+++ src/FubuMVC.HelloWorld/Controllers/Home/HomeController.cs
@@ -1,3 +1,4 @@
+using System.Web;
 using FubuMVC.Core.View;
 
 namespace FubuMVC.HelloWorld.Controllers.Home
@@ -12,12 +13,29 @@ namespace FubuMVC.HelloWorld.Controllers.Home
                 CurrentUrl = model.Url
             };
         }
+
+        public HomeViewModel Post(HomeFilesModel model)
+        {
+            return new HomeViewModel
+            {
+                Text = "Hello, world.",
+                CurrentUrl = model.Url,
+                NumberOfFiles = model.Files == null ? 0 : model.Files.Count
+            };
+        }
+    }
+
+    public class HomeFilesModel
+    {
+        public HttpFileCollectionWrapper Files { get; set; }
+        public string Url { get; set; }
     }
 
     public class HomeViewModel
     {
         public string Text { get; set; }
         public string CurrentUrl { get; set; }
+        public int NumberOfFiles { get; set; }
     }
 
     public class HomeInputModel
