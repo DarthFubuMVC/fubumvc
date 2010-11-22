9252c6ec1aded8de831785f08cdb81013b5a12a5
Jeremy Miller <jeremydmiller@yahoo.com>
2010-11-22 09:05:55 -0600

    getting the shell together of FubuFastPack, IntegrationTesting, and FubuFastPack.Testing

--- rakefile.rb
+++ rakefile.rb
@@ -93,7 +93,7 @@ task :test => [:unit_test]
 desc "Runs unit tests"
 task :unit_test => :compile do
   runner = NUnitRunner.new :compilemode => COMPILE_TARGET, :source => 'src', :platform => 'x86'
-  runner.executeTests ['FubuMVC.Tests', 'FubuCore.Testing', 'FubuLocalization.Tests', 'HtmlTags.Testing', 'Spark.Web.FubuMVC.Tests']
+  runner.executeTests ['FubuMVC.Tests', 'FubuCore.Testing', 'FubuLocalization.Tests', 'HtmlTags.Testing', 'Spark.Web.FubuMVC.Tests', 'FubuValidation.Tests', 'FubuFastPack.Testing']
 end
 
 desc "Target used for the CI server"
