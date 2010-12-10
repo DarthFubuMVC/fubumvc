e49dfe5b46472621d836b944f4501c11dfd9388b
Jason Staten <jstaten07@gmail.com>
2010-12-10 15:55:33 -0700

    Don't install rake if it's already installed. Prevents 1.9.2 on windows issues

--- InstallGems.bat
+++ InstallGems.bat
@@ -1,5 +1,12 @@
+@SETLOCAL
+@set rakeversion=""
+@FOR /f "tokens=2" %%a in ('rake --version') do @set rakeversion=%%a 
+@IF /I NOT "%rakeversion%" EQU "version " (
+@ENDLOCAL
 @ECHO *** Installing Rake
 @call gem install rake --no-rdoc --no-ri
+)
+@ENDLOCAL
 
 @ECHO *** Installing RubyZip
 @call gem install rubyzip --no-rdoc --no-ri
