0b453a95df3ca70e685c143eb9f126f744c0cc13
Joshua Flanagan <joshuaflanagan@gmail.com>
2010-12-22 13:36:56 -0600

    Fix Fubu.exe compile issue and make it return non-zero on error.

--- src/Fubu/Program.cs
+++ src/Fubu/Program.cs
@@ -1,17 +1,29 @@
-﻿using FubuCore.CommandLine;
+﻿using System;
+using FubuCore.CommandLine;
 
 namespace Fubu
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            var factory = new CommandFactory();
-            factory.RegisterCommands(typeof (IFubuCommand).Assembly);
-            factory.RegisterCommands(typeof (Program).Assembly);
+            try
+            {
+                var factory = new CommandFactory();
+                factory.RegisterCommands(typeof (IFubuCommand).Assembly);
+                factory.RegisterCommands(typeof (Program).Assembly);
 
-            var executor = new CommandExecutor(factory);
-            executor.Execute(args);
+                var executor = new CommandExecutor(factory);
+                executor.Execute(args);
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR: " + ex);
+                Console.ResetColor();
+                return 1;
+            }
+            return 0;
         }
     }
 }
