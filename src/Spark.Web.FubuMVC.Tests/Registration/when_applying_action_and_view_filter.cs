52d93ecd3e131889022314d3a22307619db9641d
jmarnold <rncodepoet@gmail.com>
2011-01-11 15:08:26 -0600

    fixed breaking issue with spark view token resolution on similar templates.

--- src/Spark.Web.FubuMVC.Tests/Registration/when_applying_action_and_view_filter.cs
+++ src/Spark.Web.FubuMVC.Tests/Registration/when_applying_action_and_view_filter.cs
@@ -13,22 +13,29 @@ namespace Spark.Web.FubuMVC.Tests.Registration
     public class when_applying_action_and_view_filter : InteractionContext<ActionAndViewMatchedBySparkViewDescriptors>
     {
         private ActionCall _call;
-        private SparkViewDescriptor _desciptor;
+        private SparkViewDescriptor _descriptor;
         private ViewBag _views;
+    	private SparkViewToken _token;
 
         protected override void beforeEach()
         {
             _call = ActionCall.For<SampleEndpoint>(e => e.Get(new SampleInput()));
-            _desciptor = new SparkViewDescriptor
-                                {
-                                    Accessors = new List<SparkViewDescriptor.Accessor>(),
-                                    Templates = new List<string>()
-                                };
-            var token = new SparkViewToken(_call, _desciptor, "Sample", "Sample");
-            token.Descriptors.Add(_desciptor);
+        	_descriptor = newDescriptor();
+            
+			_token = new SparkViewToken(_call, _descriptor, "Sample", "Sample");
+            _token.Descriptors.Add(_descriptor);
 
-            _views = new ViewBag(new List<IViewToken> { token });
+            _views = new ViewBag(new List<IViewToken> { _token });
         }
+
+		protected SparkViewDescriptor newDescriptor()
+		{
+			return new SparkViewDescriptor
+			       	{
+			       		Accessors = new List<SparkViewDescriptor.Accessor>(),
+			       		Templates = new List<string>()
+			       	};
+		}
 
         [Test]
         public void should_return_empty_collection_when_action_call_returns_a_fubu_continuation()
@@ -75,10 +82,7 @@ namespace Spark.Web.FubuMVC.Tests.Registration
         [Test]
         public void should_return_single_view_token_when_desciptor_is_matched()
         {
-            const string viewLocator = "TestFolder";
-            const string viewName = "Sample";
-
-            _desciptor.Templates.Add(viewLocator + "\\" + viewName);
+            _descriptor.Templates.Add("TestFolder\\Sample.spark");
 
             MockFor<ISparkPolicyResolver>()
                 .Expect(resolver => resolver.HasMatchFor(_call))
@@ -86,18 +90,53 @@ namespace Spark.Web.FubuMVC.Tests.Registration
 
             MockFor<ISparkPolicyResolver>()
                 .Expect(resolver => resolver.ResolveViewLocator(_call))
-                .Return(viewLocator);
+                .Return("TestFolder");
 
             MockFor<ISparkPolicyResolver>()
                 .Expect(resolver => resolver.ResolveViewName(_call))
-                .Return(viewName);
+                .Return("Sample");
 
             var token = ClassUnderTest
                         .Apply(_call, _views)
                         .FirstOrDefault();
 
             token.ShouldNotBeNull();
-            token.Name.ShouldEqual(viewName);
-        }   
+            token.Name.ShouldEqual("Sample");
+        }
+
+		[Test]
+		public void should_return_single_view_token_matched_on_policy_criteria()
+		{
+			_descriptor.Templates.Add("Projects\\Dashboard.spark");
+			_descriptor.Templates.Add("Shared\\Application.spark");
+
+			var descriptor2 = newDescriptor();
+			descriptor2.Templates.Add("Projects\\StoryMap.spark");
+			descriptor2.Templates.Add("Shared\\Application.spark");
+			_token.Descriptors.Add(descriptor2);
+
+			var descriptor3 = newDescriptor();
+			descriptor3.Templates.Add("Projects.spark");
+			descriptor3.Templates.Add("Shared\\Application.spark");
+			_token.Descriptors.Add(descriptor3);
+
+
+			MockFor<ISparkPolicyResolver>()
+				.Expect(resolver => resolver.HasMatchFor(_call))
+				.Return(true);
+
+			MockFor<ISparkPolicyResolver>()
+				.Expect(resolver => resolver.ResolveViewLocator(_call))
+				.Return("");
+
+			MockFor<ISparkPolicyResolver>()
+				.Expect(resolver => resolver.ResolveViewName(_call))
+				.Return("Projects");
+
+			ClassUnderTest
+				.Apply(_call, _views)
+				.FirstOrDefault()
+				.ShouldNotBeNull();
+		}
     }
 }
