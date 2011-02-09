d1aa547c3f4910442967f247a81a2eb055e3f138
Bob Pace <bob.pace@gmail.com>
2011-02-08 18:29:40 -0700

    added support to supply your own type of ActionCall when the behavior matcher creates behavior chains, this enables us to put the BehaviorGraph to work in an Asp.Net MVC application easier than what we were doing before

--- src/FubuMVC.Core/Registration/Conventions/BehaviorMatcher.cs
+++ src/FubuMVC.Core/Registration/Conventions/BehaviorMatcher.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using FubuCore.Util;
@@ -11,12 +10,14 @@ namespace FubuMVC.Core.Registration.Conventions
     // TODO:  Blow up if there are no filters of any sort
     public class BehaviorMatcher
     {
+        private readonly Func<Type, MethodInfo, ActionCall> _actionCallProvider;
         private readonly CompositeFilter<ActionCall> _methodFilters = new CompositeFilter<ActionCall>();
         private readonly CompositeFilter<Type> _typeFilters = new CompositeFilter<Type>();
         private BehaviorGraph _graph;
 
-        public BehaviorMatcher()
+        public BehaviorMatcher(Func<Type, MethodInfo, ActionCall> actionCallProvider)
         {
+            _actionCallProvider = actionCallProvider;
             _methodFilters.Excludes += method => method.Method.DeclaringType == typeof (object);
             _methodFilters.Excludes += method => method.Method.DeclaringType == typeof (MarshalByRefObject);
             _methodFilters.Excludes += method => method.Method.DeclaringType == typeof (IDisposable);
@@ -51,7 +52,7 @@ namespace FubuMVC.Core.Registration.Conventions
         private void scanMethods(Type type)
         {
             type.GetMethods(BindingFlags.Instance | BindingFlags.Public)
-                .Select(x => new ActionCall(type, x))
+                .Select(x => _actionCallProvider(type, x))
                 .Where(MethodFilters.Matches)
                 .Each(registerBehavior);
         }
