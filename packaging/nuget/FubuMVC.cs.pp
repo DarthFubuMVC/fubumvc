6ecac7cf7bae208104352e6ede2632a8ea507f44
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-03-01 19:43:54 -0600

    Updated nuget package to follow WebActivator App_Start folder convention
    see http://blog.davidebbo.com/2011/02/appstart-folder-convention-for-nuget.html

--- packaging/nuget/FubuMVC.cs.pp
+++ packaging/nuget/FubuMVC.cs.pp
@@ -4,11 +4,11 @@ using FubuMVC.StructureMap;
 using StructureMap;
 
 // You can remove the reference to WebActivator by calling the Start() method from your Global.asax Application_Start
-[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.AppStart_FubuMVC), "Start", callAfterGlobalAppStart: true)]
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.AppStartFubuMVC), "Start", callAfterGlobalAppStart: true)]
 
-namespace $rootnamespace$
+namespace $rootnamespace$.App_Start
 {
-    public static class AppStart_FubuMVC
+    public static class AppStartFubuMVC
     {
         public static void Start()
         {
