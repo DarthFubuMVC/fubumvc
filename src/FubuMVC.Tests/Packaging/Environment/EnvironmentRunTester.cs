2b53e331ba62de9ea488759a95116bada68f9f95
Alex Henning Johannessen <ahjohannessen@gmail.com>
2011-03-07 03:54:24 -0700

    Made EnvironmentRunTester.cs pass on Mono/Linux

--- src/FubuMVC.Tests/Packaging/Environment/EnvironmentRunTester.cs
+++ src/FubuMVC.Tests/Packaging/Environment/EnvironmentRunTester.cs
@@ -1,5 +1,7 @@
 ﻿using FubuMVC.Core.Packaging.Environment;
 using NUnit.Framework;
+using System;
+using System.IO;
 
 namespace FubuMVC.Tests.Packaging.Environment
 {
@@ -31,14 +33,15 @@ namespace FubuMVC.Tests.Packaging.Environment
         [Test]
         public void build_app_domain_setup()
         {
+			var baseFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "folder1");
             var run = new EnvironmentRun(){
-                ApplicationBase = "c:\\folder1",
+                ApplicationBase = baseFolder,
                 ConfigurationFile = "web.config"
             };
 
             var setup = run.BuildAppDomainSetup();
             setup.ApplicationBase.ShouldEqual(run.ApplicationBase);
-            setup.ConfigurationFile.ShouldEqual("c:\\folder1\\web.config");
+            setup.ConfigurationFile.ShouldEqual(Path.Combine(baseFolder, "web.config"));
             setup.ApplicationName.ShouldStartWith("Bottles-Environment-Installation");
             setup.ShadowCopyFiles.ShouldEqual("true");
         }
@@ -84,7 +87,7 @@ namespace FubuMVC.Tests.Packaging.Environment
         public void assert_is_valid_missing_both_environment_class_name_and_assembly_name_throws()
         {
             var run = new EnvironmentRun(){
-                ApplicationBase = "c:\\folder1"
+                ApplicationBase = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "folder1")
             };
 
             Exception<EnvironmentRunnerException>.ShouldBeThrownBy(() =>
