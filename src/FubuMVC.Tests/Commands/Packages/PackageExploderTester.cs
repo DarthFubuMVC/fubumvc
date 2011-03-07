89b2637abe632443966ad5ef30782e45033033c0
Alex Henning Johannessen <ahjohannessen@gmail.com>
2011-03-07 02:46:56 -0700

    Made PackageExploderTester pass on Mono/Linux

--- src/FubuMVC.Tests/Commands/Packages/PackageExploderTester.cs
+++ src/FubuMVC.Tests/Commands/Packages/PackageExploderTester.cs
@@ -50,7 +50,7 @@ namespace FubuMVC.Tests.Commands.Packages
                 expected = folder;
             });
 
-            expected.ShouldEqual("app1\\bin\\fubu-packages\\AssemblyPackage\\WebContent".ToFullPath());
+            expected.ShouldEqual(FileSystem.Combine("app1", "bin", "fubu-packages", "AssemblyPackage", "WebContent").ToFullPath());
         }
     }
 
