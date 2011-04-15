ce5768cac80cb20395cfb6c1591012e23bd1e1cf
jfebres <jaime.febres@gmail.com>
2011-04-15 11:58:55 -0500

    Starting adding the spark hook ups into the sample web project, just to ensure the spark tokens are picked up and correctly recognized by fubu.

--- src/FubuMVC.Spark/Tokenization/Model/SparkViewToken.cs
+++ src/FubuMVC.Spark/Tokenization/Model/SparkViewToken.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using FubuMVC.Core;
+using FubuMVC.Core.Behaviors;
 using FubuMVC.Core.Registration.Nodes;
+using FubuMVC.Core.Registration.ObjectGraph;
+using FubuMVC.Core.Runtime;
 using FubuMVC.Core.View;
 using Spark;
 
@@ -16,7 +20,7 @@ namespace FubuMVC.Spark.Tokenization.Model
 
         public BehaviorNode ToBehavioralNode()
         {
-            return null;
+            return new SparkViewOutput(_file);
         }
 
         public Type ViewType
@@ -41,6 +45,48 @@ namespace FubuMVC.Spark.Tokenization.Model
         public override string ToString()
         {
             return _file.RelativePath();
+        }
+    }
+
+    //NOTE:TEMP
+    public class SparkViewOutput : OutputNode<SparkViewRenderer>
+    {
+        private readonly SparkFile _file;
+
+        public SparkViewOutput(SparkFile file)
+        {
+            _file = file;
+        }
+        protected override void configureObject(ObjectDef def)
+        {
+            def.DependencyByValue(_file);
+        }
+        public override string Description
+        {
+            get
+            {
+                return string.Format("Spark View [{0}]", _file.RelativePath());
+            }
+        }
+    }
+
+    //NOTE:TEMP
+    public class SparkViewRenderer : BasicBehavior
+    {
+        private readonly IOutputWriter _writer;
+        private readonly SparkFile _file;
+
+        public SparkViewRenderer(IOutputWriter writer,SparkFile file)
+            : base(PartialBehavior.Executes)
+        {
+            _writer = writer;
+            _file = file;
+        }
+
+        protected override DoNext performInvoke()
+        {
+            _writer.WriteHtml(_file);
+            return DoNext.Continue;
         }
     }
 }
