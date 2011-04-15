3c0e7ceec7e92bef65469eef1aed9040881545bc
Dru Sellers <dru@drusellers.com>
2011-04-15 08:31:00 -0500

    adding SEZ demo code

--- src/Bottles.Deployment/Deployers/TopshelfService.cs
+++ src/Bottles.Deployment/Deployers/TopshelfService.cs
@@ -2,5 +2,15 @@
 {
     public class TopshelfService : IDirective
     {
+        public string InstallLocation { get; set; }
+
+        //optional
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        //optional
+        public string ServiceName { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
     }
 }
