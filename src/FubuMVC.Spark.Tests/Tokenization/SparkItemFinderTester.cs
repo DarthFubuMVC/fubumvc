f83adc8e139f558b3095e0bd492dbc0865aefce8
Alex Henning Johannessen <ahjohannessen@gmail.com>
2011-04-19 14:02:51 +0100

    Added some basic tests for SparkItemFinder

---
+++ src/FubuMVC.Spark.Tests/Tokenization/SparkItemFinderTester.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FubuCore;
+using FubuMVC.Spark.Tokenization;
+using FubuMVC.Spark.Tokenization.Scanning;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuMVC.Spark.Tests.Tokenization
+{
+    public class SparkItemFinderTester
+    {
+        private readonly ISparkItemFinder _itemFinder;
+        private readonly IEnumerable<SparkItem> _items;
+        private readonly string _templatePath;
+
+        public SparkItemFinderTester()
+        {
+            _templatePath = Path.Combine(Directory.GetCurrentDirectory(), "Templates");
+            _itemFinder = new SparkItemFinder(new FileScanner(), new[] { new SparkRoot { Path = _templatePath }});
+            _items = _itemFinder.FindItems();
+        }
+
+        [Test]
+        public void finder_locates_all_relevant_spark_items()
+        {
+            _items.ShouldHaveCount(22);
+        }
+
+        [Test]
+        public void finder_locates_all_bindings_xml()
+        {
+            var expected = FileSystem.Combine(_templatePath, "Shared", "bindings.xml");
+            _items.ShouldContain(si => si.FilePath == expected);
+        }
+    }
+}
