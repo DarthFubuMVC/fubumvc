0312bdacc7f7b0c4e25b27700d63ca7071dcee5d
Dru Sellers <dru@drusellers.com>
2011-04-19 08:32:07 -0500

    adding dependency graph

---
+++ src/Bottles/DependencyAnalysis/Bottle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Bottles.DependencyAnalysis
+{
+    public class Bottle
+    {
+        IList<Urn> _dependencies;
+        public Bottle(string name) : this(new Urn(name))
+        {
+            
+        }
+        public Bottle(Urn name)
+        {
+            Name = name;
+            _dependencies = new List<Urn>();
+        }
+
+        public Urn Name { get; private set; }
+        public ICollection<Urn> Dependencies { get { return _dependencies; } }
+
+        public void AddDependency(string name)
+        {
+            AddDependency(new Urn(name));
+        }
+        public void AddDependency(Urn name)
+        {
+            _dependencies.Add(name);
+        }
+    }
+}
