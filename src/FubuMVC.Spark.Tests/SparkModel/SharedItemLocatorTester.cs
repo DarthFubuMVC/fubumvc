b53566525e5833b7e350c339f6dde7818d0fdba3
jfebres <jaime.febres@gmail.com>
2011-05-02 14:17:50 -0500

    Added a few more tests for SharedItemLocatorTester.

--- src/FubuMVC.Spark.Tests/SparkModel/SharedItemLocatorTester.cs
+++ src/FubuMVC.Spark.Tests/SparkModel/SharedItemLocatorTester.cs
@@ -10,8 +10,13 @@ namespace FubuMVC.Spark.Tests.SparkModel
 {
     public class SharedItemLocatorTester : InteractionContext<SharedItemLocator>
     {
-        private SparkItem _foundItem;
-        private SparkItem _notFoundItem;
+        private SparkItem _applicationItem;
+        private SparkItem _messageItem;
+        private SparkItem _lowerCasedCompanyItem;
+        private SparkItem _upperCasedCompanyItem;
+
+        private SparkItem _headerItem;
+
         protected override void beforeEach()
         {
             var sharedDirectoryProvider = MockFor<ISharedDirectoryProvider>();
@@ -26,43 +31,61 @@ namespace FubuMVC.Spark.Tests.SparkModel
 
             var items = new List<SparkItem>
             {
-                new SparkItem(Path.Combine(sharedDirectories.ElementAt(0), "Baz.spark"), "", ""),
-                new SparkItem(Path.Combine(sharedDirectories.ElementAt(1), "Fooz.spark"), "", ""),
-                new SparkItem(Path.Combine(sharedDirectories.ElementAt(2), "Foo.spark"), "", ""),
-                new SparkItem(Path.Combine(sharedDirectories.ElementAt(3), "Bazz.spark"), "", ""),
-                new SparkItem(Path.Combine(sharedDirectories.ElementAt(3), "Foo.spark"), "", "")
+                new SparkItem(Path.Combine(sharedDirectories.ElementAt(0), "Company.spark"), "", ""),
+                new SparkItem(Path.Combine(sharedDirectories.ElementAt(1), "company.spark"), "", ""),
+                new SparkItem(Path.Combine(sharedDirectories.ElementAt(2), "application.spark"), "", ""),
+                new SparkItem(Path.Combine(sharedDirectories.ElementAt(3), "application.spark"), "", ""),
+                new SparkItem(Path.Combine("App","Views","Home", "header.spark"), "", ""),
             };
 
             sharedDirectoryProvider.Stub(x => x.GetDirectories(fromItem, items)).Return(sharedDirectories);
             Services.Inject(sharedDirectoryProvider);
 
-
-            _foundItem = ClassUnderTest.LocateItem("Foo", fromItem, items);
-            _notFoundItem = ClassUnderTest.LocateItem("Bar", fromItem, items);
+            _applicationItem = ClassUnderTest.LocateItem("application", fromItem, items);
+            _messageItem = ClassUnderTest.LocateItem("message", fromItem, items);
+            _lowerCasedCompanyItem = ClassUnderTest.LocateItem("company", fromItem, items);
+            _upperCasedCompanyItem = ClassUnderTest.LocateItem("Company", fromItem, items);
+            _headerItem = ClassUnderTest.LocateItem("header", fromItem, items);
         }
 
         [Test]
         public void if_exists_the_item_is_not_null()
         {
-            _foundItem.ShouldNotBeNull();
+            _applicationItem.ShouldNotBeNull();
         }
 
         [Test]
         public void the_located_item_name_match_the_requested_spark_name()
         {
-            _foundItem.Name().ShouldEqual("Foo");
+            _applicationItem.Name().ShouldEqual("application");
         }
 
         [Test]
         public void the_located_item_is_under_the_reachable_directories()
         {
-            _foundItem.DirectoryPath().ShouldEqual(Path.Combine("App", "Controllers", "Shared"));
+            _applicationItem.DirectoryPath().ShouldEqual(Path.Combine("App", "Controllers", "Shared"));
         }
 
         [Test]
         public void when_not_found_returns_null()
         {
-            _notFoundItem.ShouldBeNull();
+            _messageItem.ShouldBeNull();
+        }
+
+        [Test]
+        public void items_are_localized_by_their_exact_case_sensitive_name()
+        {
+            _lowerCasedCompanyItem.Name().ShouldEqual("company");
+            _lowerCasedCompanyItem.DirectoryPath().ShouldEqual(Path.Combine("App", "Handlers", "Shared"));
+
+            _upperCasedCompanyItem.Name().ShouldEqual("Company");
+            _upperCasedCompanyItem.DirectoryPath().ShouldEqual(Path.Combine("App", "Views", "Shared"));
+        }
+
+        [Test]
+        public void if_the_item_by_name_exists_but_is_not_under_any_of_the_shared_directories_returns_null()
+        {
+            _headerItem.ShouldBeNull();
         }
     }
 }
