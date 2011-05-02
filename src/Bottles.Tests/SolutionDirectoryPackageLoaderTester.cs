58e3872686bc4db5a533d2d024c80229af0ccc12
Jeremy Miller <jeremydmiller@yahoo.com>
2011-05-02 13:45:13 -0500

    fixed UT on SolutionDirectoryPackageLoaderTester for new signature

--- src/Bottles.Tests/SolutionDirectoryPackageLoaderTester.cs
+++ src/Bottles.Tests/SolutionDirectoryPackageLoaderTester.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Bottles.Diagnostics;
 using FubuCore;
 using NUnit.Framework;
 using FubuTestingSupport;
@@ -29,7 +30,7 @@ namespace Bottles.Tests
         [Test]
         public void there_are_7_manifests_that_are_modules_in_fubu()
         {
-            var foundPackages = theLoader.Load();
+            var foundPackages = theLoader.Load(new PackageLog());
             foundPackages.Count().ShouldEqual(1);
             foundPackages.First().Name.ShouldEqual("test-mani");
         }
