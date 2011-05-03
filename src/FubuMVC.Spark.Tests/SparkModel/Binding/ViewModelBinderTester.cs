1d60517cae013407fbc0080dab0c724f14f3ae52
Alex Henning Johannessen <ahjohannessen@gmail.com>
2011-05-03 15:12:31 +0100

    Tests for binding.

---
+++ src/FubuMVC.Spark.Tests/SparkModel/Binding/ViewModelBinderTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FubuMVC.Core.Registration;
+using FubuCore;
+using FubuTestingSupport;
+using FubuMVC.Spark.SparkModel;
+using NUnit.Framework;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+
+namespace FubuMVC.Spark.Tests.SparkModel.Binders
+{
+	public class ViewModelBinderTester : InteractionContext<ViewModelBinder>
+	{
+		
+	}
+}
