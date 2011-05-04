1683495b7c6dc9728fb96f41313ae688005ae396
Alex Henning Johannessen <ahjohannessen@gmail.com>
2011-05-04 13:14:24 +0100

    MonoDevelop is not very good at formatting, it seems.

--- src/FubuMVC.Spark/SparkActivator.cs
+++ src/FubuMVC.Spark/SparkActivator.cs
@@ -16,63 +16,62 @@ using System.Linq;
 
 namespace FubuMVC.Spark
 {
-    public class SparkActivator : IActivator
-    {
-        private readonly ISparkItems _sparkItems;
-        private readonly ISparkViewEngine _engine;
+	public class SparkActivator : IActivator
+	{
+		private readonly ISparkItems _sparkItems;
+		private readonly ISparkViewEngine _engine;
 
-        public SparkActivator(ISparkItems sparkItems, ISparkViewEngine engine)
-        {
-            _sparkItems = sparkItems;
-            _engine = engine;
-        }
+		public SparkActivator (ISparkItems sparkItems, ISparkViewEngine engine)
+		{
+			_sparkItems = sparkItems;
+			_engine = engine;
+		}
 
-        public void Activate(IEnumerable<IPackageInfo> packages, IPackageLog log)
-        {
-            sparkSettings();
+		public void Activate (IEnumerable<IPackageInfo> packages, IPackageLog log)
+		{
+			sparkSettings ();
 
-            _engine.ViewFolder = new SparkItemViewFolder(_sparkItems);
-            _engine.DefaultPageBaseType = typeof(FubuSparkView).FullName;
-			((SparkViewEngine)_engine).BindingProvider = new FubuBindingProvider(_sparkItems);
-        }
+			_engine.ViewFolder = new SparkItemViewFolder (_sparkItems);
+			_engine.DefaultPageBaseType = typeof(FubuSparkView).FullName;
+			((SparkViewEngine)_engine).BindingProvider = new FubuBindingProvider (_sparkItems);
+		}
 
-        private void sparkSettings()
-        {
-            var settings = (SparkSettings)_engine.Settings;
+		private void sparkSettings ()
+		{
+			var settings = (SparkSettings)_engine.Settings;
             
-			settings.AddAssembly(typeof(HtmlTag).Assembly)
-                .AddAssembly(typeof(FubuPageExtensions).Assembly)
-                .AddNamespace(typeof(VirtualPathUtility).Namespace) // System.Web
-                .AddNamespace(typeof(FubuRegistryExtensions).Namespace) // FubuMVC.Spark
-                .AddNamespace(typeof(FubuPageExtensions).Namespace) // FubuMVC.Core.UI
-                .AddNamespace(typeof(HtmlTag).Namespace); // HtmlTags   
-        }
-    }
+			settings.AddAssembly (typeof(HtmlTag).Assembly)
+                .AddAssembly (typeof(FubuPageExtensions).Assembly)
+                .AddNamespace (typeof(VirtualPathUtility).Namespace) // System.Web
+                .AddNamespace (typeof(FubuRegistryExtensions).Namespace) // FubuMVC.Spark
+                .AddNamespace (typeof(FubuPageExtensions).Namespace) // FubuMVC.Core.UI
+                .AddNamespace (typeof(HtmlTag).Namespace); // HtmlTags   
+		}
+	}
 	
 	// Temporary : Needs discussion + test coverage (if used)
 	public class FubuBindingProvider : BindingProvider
-    {	
+	{	
 		private readonly ISparkItems _sparkItems;
-        public FubuBindingProvider(ISparkItems sparkItems)
-        {
-            _sparkItems = sparkItems;
-        }
-		
-        public override IEnumerable<Binding> GetBindings(IViewFolder viewFolder)
-        {					
-			var bindings = new List<Binding>();
+
+		public FubuBindingProvider (ISparkItems sparkItems)
+		{
+			_sparkItems = sparkItems;
+		}
+
+		public override IEnumerable<Binding> GetBindings (IViewFolder viewFolder)
+		{					
+			var bindings = new List<Binding> ();
 			
-			foreach (var binding in _sparkItems.ByName("bindings")) 
-			{
-			    var file = viewFolder.GetViewSource(binding.ViewPath);
-            	using (var stream = file.OpenViewStream())
-				using (var reader = new StreamReader(stream))
-                {
-					bindings.AddRange(LoadStandardMarkup(reader));
-                }					
+			foreach (var binding in _sparkItems.ByName("bindings")) {
+				var file = viewFolder.GetViewSource (binding.ViewPath);
+				using (var stream = file.OpenViewStream())
+				using (var reader = new StreamReader(stream)) {
+					bindings.AddRange (LoadStandardMarkup (reader));
+				}					
 			}		
 			
 			return bindings;
-        }
-    }
+		}
+	}
 }
--- src/FubuMVC.Spark/SparkModel/SparkItemBinders.cs
+++ src/FubuMVC.Spark/SparkModel/SparkItemBinders.cs
@@ -11,135 +11,146 @@ using Spark;
 
 namespace FubuMVC.Spark.SparkModel
 {
-    public class BindContext
-    {
-        public IEnumerable<string> Namespaces { get; set; }
-        public string Master { get; set; }
-        public string ViewModelType { get; set; }
-        public IEnumerable<SparkItem> AvailableItems { get; set; }
-        public ISparkPackageTracer Tracer { get; set; }
-    }
+	public class BindContext
+	{
+		public IEnumerable<string> Namespaces { get; set; }
 
-    public interface ISparkItemBinder
-    {
-		bool CanBind(SparkItem item, BindContext context);
-        void Bind(SparkItem item, BindContext context);
-    }
-	
-    public class MasterPageBinder : ISparkItemBinder
-    {
-        private readonly ISharedItemLocator _sharedItemLocator;
-        private const string FallbackMaster = "Application";
-        public string MasterName { get; set; }
+		public string Master { get; set; }
 
-        public MasterPageBinder() : this(new SharedItemLocator()){}
-        public MasterPageBinder(ISharedItemLocator sharedItemLocator)
-        {
-            _sharedItemLocator = sharedItemLocator;
-            MasterName = FallbackMaster;
-        }
+		public string ViewModelType { get; set; }
 
-		public bool CanBind(SparkItem item, BindContext context)
+		public IEnumerable<SparkItem> AvailableItems { get; set; }
+
+		public ISparkPackageTracer Tracer { get; set; }
+	}
+
+	public interface ISparkItemBinder
+	{
+		bool CanBind (SparkItem item, BindContext context);
+
+		void Bind (SparkItem item, BindContext context);
+	}
+
+	public class MasterPageBinder : ISparkItemBinder
+	{
+		private readonly ISharedItemLocator _sharedItemLocator;
+		private const string FallbackMaster = "Application";
+
+		public string MasterName { get; set; }
+
+		public MasterPageBinder () : this(new SharedItemLocator())
+		{
+		}
+
+		public MasterPageBinder (ISharedItemLocator sharedItemLocator)
+		{
+			_sharedItemLocator = sharedItemLocator;
+			MasterName = FallbackMaster;
+		}
+
+		public bool CanBind (SparkItem item, BindContext context)
 		{		
 			return context.Master != string.Empty 
-				&& Path.GetExtension(item.FilePath) == Constants.DotSpark
-				&& !Path.GetFileNameWithoutExtension(item.FilePath).StartsWith("_");
+				&& Path.GetExtension (item.FilePath) == Constants.DotSpark
+				&& !Path.GetFileNameWithoutExtension (item.FilePath).StartsWith ("_");
 		}
 
-        public void Bind(SparkItem item, BindContext context)
-        {
+		public void Bind (SparkItem item, BindContext context)
+		{
 			var tracer = context.Tracer;
-            var masterName = context.Master ?? MasterName;
+			var masterName = context.Master ?? MasterName;
 
-            var master = _sharedItemLocator.LocateItem(masterName, item, context.AvailableItems);
+			var master = _sharedItemLocator.LocateItem (masterName, item, context.AvailableItems);
 			
-			if (master == null)
-            {
-                tracer.Trace(item, "Expected master page [{0}] not found.", masterName);
+			if (master == null) {
+				tracer.Trace (item, "Expected master page [{0}] not found.", masterName);
 				return;
-            }
+			}
 
-			if(master.FilePath == item.FilePath)
-			{
-                tracer.Trace(item, "Master page skipped because it is the item.", masterName);
+			if (master.FilePath == item.FilePath) {
+				tracer.Trace (item, "Master page skipped because it is the item.", masterName);
 				return;
-            }
+			}
 			            
 			item.Master = master;
-			tracer.Trace(item, "Master page [{0}] found at {1}", masterName, master.FilePath);
-        }
-    }
+			tracer.Trace (item, "Master page [{0}] found at {1}", masterName, master.FilePath);
+		}
+	}
 
-    public class ViewModelBinder : ISparkItemBinder
-    {
-        private readonly ITypeResolver _typeResolver;
-        public ViewModelBinder()
-        {
-            var resolver = new TypeResolver();
-            resolver.AddStrategy<FullTypeNameStrategy>();
-            _typeResolver = resolver;
-        }
+	public class ViewModelBinder : ISparkItemBinder
+	{
+		private readonly ITypeResolver _typeResolver;
 
-        public ViewModelBinder(ITypeResolver typeResolver)
-        {
-            _typeResolver = typeResolver;
-        }
-
-		public bool CanBind(SparkItem item, BindContext context)
+		public ViewModelBinder ()
 		{
-			return context.ViewModelType.IsNotEmpty();
+			var resolver = new TypeResolver ();
+			resolver.AddStrategy<FullTypeNameStrategy> ();
+			_typeResolver = resolver;
 		}
 
-        public void Bind(SparkItem item, BindContext context)
-        {
-            item.ViewModelType = _typeResolver.ResolveType(context.ViewModelType);
-            context.Tracer.Trace(item, "View model type is : [{0}]", item.ViewModelType);
-        }
-    }
+		public ViewModelBinder (ITypeResolver typeResolver)
+		{
+			_typeResolver = typeResolver;
+		}
+
+		public bool CanBind (SparkItem item, BindContext context)
+		{
+			return context.ViewModelType.IsNotEmpty ();
+		}
+
+		public void Bind (SparkItem item, BindContext context)
+		{
+			item.ViewModelType = _typeResolver.ResolveType (context.ViewModelType);
+			context.Tracer.Trace (item, "View model type is : [{0}]", item.ViewModelType);
+		}
+	}
 	
 	// Note: Would be much better to rely on something like AssemblyScanner
 	// (Diagnostics has a simple one, but we need SM like).
-    public class FullTypeNameStrategy : ITypeResolverStrategy
-    {
-        private readonly TypePool _typePool;
-		
-		public FullTypeNameStrategy() : this(DefaultTypePool()){}
-		public FullTypeNameStrategy(TypePool typePool)
+	public class FullTypeNameStrategy : ITypeResolverStrategy
+	{
+		private readonly TypePool _typePool;
+
+		public FullTypeNameStrategy () : this(DefaultTypePool())
+		{
+		}
+
+		public FullTypeNameStrategy (TypePool typePool)
 		{
 			_typePool = typePool;
 		}		
-						
-        public bool Matches(object model)
-        {
-            return model is string;
-        }
 
-        public Type ResolveType(object model)
-        {
-            var types = _typePool.TypesWithFullName((string)model);            
-			return types.Count() == 1 ? types.First() : null;
-        }
+		public bool Matches (object model)
+		{
+			return model is string;
+		}
 
-        public static TypePool DefaultTypePool()
-        {			
+		public Type ResolveType (object model)
+		{
+			var types = _typePool.TypesWithFullName ((string)model);            
+			return types.Count () == 1 ? types.First () : null;
+		}
+
+		public static TypePool DefaultTypePool ()
+		{			
 			var binPath = AppDomain.CurrentDomain.SetupInformation.PrivateBinPath;			
-			var candiateNames = new FileSystem().FindAssemblyNames(binPath).ToList();			
+			var candiateNames = new FileSystem ().FindAssemblyNames (binPath).ToList ();			
 			
-			var assemblyFilters = new CompositeFilter<Assembly>();						
-			assemblyFilters.Includes += a => candiateNames.Contains(a.GetName().Name);
+			var assemblyFilters = new CompositeFilter<Assembly> ();						
+			assemblyFilters.Includes += a => candiateNames.Contains (a.GetName ().Name);
 			assemblyFilters.Excludes += a => a.IsDynamic;
 
-			var assemblies = AppDomain.CurrentDomain.GetAssemblies()
-				.Where(assemblyFilters.MatchesAll);
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies ()
+				.Where (assemblyFilters.MatchesAll);
 		
-			var typePool = new TypePool(Assembly.GetCallingAssembly()) 
+			var typePool = new TypePool (Assembly.GetCallingAssembly ()) 
 			{ 
 				ShouldScanAssemblies = true 
 			};
 			
-            typePool.AddAssemblies(assemblies.ToList());
+			typePool.AddAssemblies (assemblies.ToList ());
 			
 			return typePool;
-        }		
-    }
+		}		
+	}
 }
