81aa26e0bddfa7a213ce977f16b3f9d0a86d7fe3
jfebres <jaime.febres@gmail.com>
2011-05-05 11:12:56 -0500

    Added more test coverage to SparkItemFinder.

--- src/FubuMVC.Spark.Tests/SparkModel/SparkItemFinderTester.cs
+++ src/FubuMVC.Spark.Tests/SparkModel/SparkItemFinderTester.cs
@@ -20,20 +20,43 @@ namespace FubuMVC.Spark.Tests.SparkModel
         protected override void beforeEach()
         {
             Services.Inject<IFileScanner>(new FileScanner());
-            ClassUnderTest.HostPath = _templatePath;
         }
 
         [Test]
         public void finder_locates_all_relevant_spark_items()
         {
+            ClassUnderTest.HostPath = _templatePath;
             ClassUnderTest.FindInHost().ShouldHaveCount(48);
+        }
+
+        [Test]
+        public void exclude_directory_makes_the_finder_to_skip_completely_the_directory()
+        {
+            ClassUnderTest.HostPath = _templatePath;
+            ClassUnderTest.ExcludeHostDirectory("App");
+            ClassUnderTest.ExcludeHostDirectory("Package2", "Handlers", "Shared");
+            ClassUnderTest.ExcludeHostDirectory("Pak1", "Alpha", "Bravo");
+            ClassUnderTest.FindInHost().ShouldHaveCount(23);
+        }
+
+        [Test]
+        public void include_file_makes_the_matching_files_to_be_included_when_finding_items()
+        {
+            ClassUnderTest.HostPath = _templatePath;
+            ClassUnderTest.IncludeFile("file.*");
+            ClassUnderTest.IncludeFile("baz.*");
+            ClassUnderTest.IncludeFile("dog.zoo");
+            ClassUnderTest.FindInHost().ShouldHaveCount(52);
         }
 
         [Test]
         public void finder_locates_all_bindings_xml()
         {
             var expected = FileSystem.Combine(_templatePath, "Shared", "bindings.xml");
+            ClassUnderTest.HostPath = _templatePath;
             ClassUnderTest.FindInHost().ShouldContain(si => si.FilePath == expected);
         }
+
+        // TODO: TESTS FOR FindInPackages
     }
 }
