f7f855554c0a3873a454eca01444bc05f257cb15
Alex Henning Johannessen <ahjohannessen@gmail.com>
2011-05-22 03:44:15 +0100

    Added more test coverage for ViewExpression.TryToAttachViewsInPackages()

--- src/FubuMVC.Tests/Registration/DSL/ViewExpressionTester.cs
+++ src/FubuMVC.Tests/Registration/DSL/ViewExpressionTester.cs
@@ -5,6 +5,7 @@ using FubuMVC.Core;
 using FubuMVC.Core.Registration;
 using FubuMVC.Core.Registration.Nodes;
 using FubuMVC.Core.View;
+using FubuMVC.Core.View.Attachment;
 using FubuTestingSupport;
 using NUnit.Framework;
 
@@ -20,7 +21,7 @@ namespace FubuMVC.Tests.Registration.DSL
         private Test2Facility _facility2;
 
         [SetUp]
-        void before_each()
+        public void before_each()
         {
             _parent = new FubuRegistry();
             _child = new FubuRegistry();
@@ -32,6 +33,8 @@ namespace FubuMVC.Tests.Registration.DSL
 
             _parent.Views.TryToAttachWithDefaultConventions();
             _parent.Views.TryToAttachViewsInPackages();
+            _parent.Applies.ToAssembly(GetType().Assembly);
+            _parent.Actions.IncludeType<EvenMoreAwesomeController>();
         }
 
         [Test]
@@ -59,19 +62,53 @@ namespace FubuMVC.Tests.Registration.DSL
         [Test]
         public void try_to_attach_views_from_packages_is_additive()
         {
-            var childFacility = new Test2Facility();
-            _child.Views.Facility(childFacility);
+            _child.Views.Facility(_facility2);
 
             _parent.Import(_child, "import");
             _parent.BuildGraph();
 
             _facility1.Invocations.ShouldEqual(2);
-            childFacility.Invocations.ShouldEqual(1);
+            _facility2.Invocations.ShouldEqual(1);
         }
 
-        // UT : More coverage on filter 
+        [Test]
+        public void try_to_attach_views_from_packages_adds_filters_only_if_no_facility_is_in_import_positive()
+        {
+            _child.Actions.IncludeType<EvenMoreAwesomeController>();
+            var filter = new TestFilter();
+            _parent.Views.TryToAttach(v => v.by(filter));
+
+            _parent.Import(_child, "import");
+            _parent.BuildGraph();
+
+            filter.Invocations.ShouldEqual(8);
+        }
+
+
+        [Test]
+        public void try_to_attach_views_from_packages_adds_filters_only_if_no_facility_is_in_import_negative()
+        {
+            _child.Actions.IncludeType<EvenMoreAwesomeController>();
+            _child.Views.Facility(new Test1Facility());
+            var filter = new TestFilter();
+            _parent.Views.TryToAttach(v => v.by(filter));
+
+            _parent.Import(_child, "import");
+            _parent.BuildGraph();
+
+            filter.Invocations.ShouldEqual(4);
+        }
     }
 
+    public class TestFilter : IViewsForActionFilter
+    {
+        public int Invocations { get; private set; }
+        public IEnumerable<IViewToken> Apply(ActionCall call, ViewBag views)
+        {
+            Invocations++;
+            return Enumerable.Empty<IViewToken>();
+        }
+    }
 
     public class Test1Facility : IViewFacility
     {
