9512b2c7274bd7014fa73aed60799cf175d4a011
Ryan Rauh <rauh.ryan@gmail.com>
2011-06-20 09:29:01 -0500

    removed the code lines that alex suggested

--- src/TestPackage4/TestPackage4Registry.cs
+++ src/TestPackage4/TestPackage4Registry.cs
@@ -4,23 +4,13 @@ using Spark;
 
 namespace TestPackage4
 {
-	public class TestPackage4Registry : FubuPackageRegistry, IFubuRegistryExtension
+	public class TestPackage4Registry : FubuPackageRegistry
 	{
 	    public TestPackage4Registry()
 	    {
 	        Applies.ToThisAssembly();
 
             Actions.IncludeClassesSuffixedWithController();
-
-	        Views
-                .TryToAttachViewsInPackages()
-                .TryToAttachWithDefaultConventions();
-
 	    }
-
-        //public void Configure(FubuRegistry registry)
-        //{
-        //    registry.Import(this, "pak4");
-        //}
 	}
 }
