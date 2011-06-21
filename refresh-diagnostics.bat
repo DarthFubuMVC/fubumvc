35419f0031229f858b0179941e5d9408a2a1e24c
Ryan Rauh <rauh.ryan@gmail.com>
2011-06-21 12:59:58 -0500

    tweeked the batch file to install the diagnostics bottle to the testing project

--- refresh-diagnostics.bat
+++ refresh-diagnostics.bat
@@ -1,5 +1,5 @@
 del "src\FubuMVC.HelloWorld\bin\FubuMVC.Diagnostics.dll"
 rmdir /S "src\FubuMVC.HelloWorld\bin\fubu-packages"
 "src\Fubu\bin\Debug\fubu.exe" create-pak -f diagnostics diagnostics.zip
-"src\Fubu\bin\Debug\fubu.exe" install-pak diagnostics.zip fubu-hello
+"src\Fubu\bin\Debug\fubu.exe" install-pak diagnostics.zip fubu-testing
 del diagnostics.zip
