87d42907af0f4ed51b9f2339c048832e837db50c
jmarnold <rncodepoet@gmail.com>
2011-06-28 11:26:59 -0500

    added nuspec for diagnostics package

--- refresh-diagnostics.bat
+++
@@ -1,5 +0,0 @@
-del "src\FubuMVC.HelloWorld\bin\FubuMVC.Diagnostics.dll"
-rmdir /S "src\FubuMVC.HelloWorld\bin\fubu-packages"
-"src\Fubu\bin\Debug\fubu.exe" create-pak -f diagnostics diagnostics.zip
-"src\Fubu\bin\Debug\fubu.exe" install-pak diagnostics.zip fubu-hello
-del diagnostics.zip
