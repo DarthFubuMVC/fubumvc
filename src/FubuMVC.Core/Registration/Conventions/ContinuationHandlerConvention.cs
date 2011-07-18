a491f3f12b2e019a409a7bd5b466ecb098a318c7
mkmurray <charliebrown928@hotmail.com>
2011-07-17 23:40:24 -0600

    Finish cleanup of Redirectable merge

--- src/FubuMVC.Core/Registration/Conventions/ContinuationHandlerConvention.cs
+++ src/FubuMVC.Core/Registration/Conventions/ContinuationHandlerConvention.cs
@@ -1,9 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using FubuCore;
 using FubuMVC.Core.Continuations;
 using FubuMVC.Core.Registration.Nodes;
-using System;
 
 namespace FubuMVC.Core.Registration.Conventions
 {
@@ -11,11 +11,13 @@ namespace FubuMVC.Core.Registration.Conventions
     {
         public void Configure(BehaviorGraph graph)
         {
-            graph.Actions().Where(x => x.OutputType().CanBeCastTo<FubuContinuation>()).Each(call =>
-            {
-                call.AddAfter(new ContinuationNode());
-                graph.Observer.RecordCallStatus(call, "Adding ContinuationNode directly after action call");
-            });
+            graph.Actions()
+                .Where(x => x.OutputType().CanBeCastTo<FubuContinuation>())
+                .Each(call =>
+                {
+                    call.AddAfter(new ContinuationNode());
+                    graph.Observer.RecordCallStatus(call, "Adding ContinuationNode directly after action call");
+                });
         }
     }
 
@@ -23,15 +25,20 @@ namespace FubuMVC.Core.Registration.Conventions
     {
         public void Configure(BehaviorGraph graph)
         {
-            graph.Actions().Where(x => x.OutputType().CanBeCastTo<Redirectable>()).Each(call => {
-                var outputType = call.OutputType();
-                if (outputType.IsGenericType) {
-                    var genericParameter = outputType.GetGenericArguments()[0];
-                    var node = typeof(RedirectableNode<>).MakeGenericType(genericParameter);
-                    call.AddAfter((BehaviorNode)Activator.CreateInstance(node));
-                    graph.Observer.RecordCallStatus(call, "Adding redirectableNode directly after action call");
-                }
-            });
+            graph.Actions()
+                .Where(x => x.OutputType().CanBeCastTo<Redirectable>())
+                .Each(call =>
+                {
+                    var outputType = call.OutputType();
+
+                    if (outputType.IsGenericType)
+                    {
+                        var genericParameter = outputType.GetGenericArguments().First();
+                        var node = typeof(RedirectableNode<>).MakeGenericType(genericParameter);
+                        call.AddAfter((BehaviorNode)Activator.CreateInstance(node));
+                        graph.Observer.RecordCallStatus(call, "Adding redirectableNode directly after action call");
+                    }
+                });
         }
     }
 }
