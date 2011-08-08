e1481ce5e225be3442c21630e090edd67f7554d9
smerrell <merrell.sam@gmail.com>
2011-08-08 09:19:55 -0600

    Added several docs rake tasks, the pdf ones need more work

--- docs/sphinx.rb
+++ docs/sphinx.rb
@@ -14,68 +14,84 @@ namespace :docs do
     end
   end
 
-  desc "to make standalone HTML files"
+  desc "Cleans the built documentation directories"
+  task :clean do
+    rm_rf "#{DOCS_BUILD_DIR}"
+  end
+
+  desc "Make standalone HTML files"
   task :html do
     run_sphinx "-b html #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/html"
   end
 
-  desc "to make HTML files named index.html in directories"
-  task :dirhtml do
-  end
+  #desc "to make HTML files named index.html in directories"
+  #task :dirhtml do
+  #end
 
-  desc "to make a single large HTML file"
+  desc "Make a single large HTML file"
   task :singlehtml do
+    run_sphinx "-b singlehtml #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/singlehtml"
   end
 
-  desc "to make pickle files"
-  task :pickle do
-  end
+  #desc "to make pickle files"
+  #task :pickle do
+  #end
 
-  desc "to make JSON files"
+  desc "Make JSON files"
   task :json do
+    run_sphinx "-b json #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/json"
   end
 
-  desc "to make HTML files and a HTML help project"
-  task :htmlhelp do
-  end
+  #desc "to make HTML files and a HTML help project"
+  #task :htmlhelp do
+  #end
 
-  desc "to make HTML files and a qthelp project"
-  task :qthelp do
-  end
+  #desc "to make HTML files and a qthelp project"
+  #task :qthelp do
+  #end
 
-  desc "to make HTML files and a Devhelp project"
-  task :devhelp do
-  end
+  #desc "to make HTML files and a Devhelp project"
+  #task :devhelp do
+  #end
 
-  desc "to make an epub"
+  desc "Make an epub"
   task :epub do
+    run_sphinx "-b epub #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/epub"
   end
 
-  desc "to make LaTeX files, you can set PAPER=a4 or PAPER=letter"
+  desc "Make LaTeX files, you can set PAPER=a4 or PAPER=letter"
   task :latex do
+    run_sphinx "-b latex #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/latex"
   end
 
-  desc "to make LaTeX files and run them through pdflatex"
+  desc "Make LaTeX files and run them through pdflatex"
   task :latexpdf do
+    run_sphinx "-b latex #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/latex"
+    puts "Running LaTeX files through pdflatex..."
+    sh "make -C $(DOCS_BUILD_DIR)/latex all-pdf"
   end
 
-  desc "to make text files"
+  desc "Make text files"
   task :text do
+    run_sphinx "-b text #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/text"
   end
 
-  desc "to make manual pages"
-  task :man do
-  end
+  #desc "to make manual pages"
+  #task :man do
+  #end
 
-  desc "to make an overview of all changed/added/deprecated items"
+  desc "Make an overview of all changed/added/deprecated items"
   task :changes do
+    run_sphinx "-b changes #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/changes"
   end
 
-  desc "to check all external links for integrity"
+  desc "Check all external links for integrity"
   task :linkcheck do
+    run_sphinx "-b linkcheck #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/linkcheck"
   end
 
-  desc "to run all doctests embedded in the documentation (if enabled)"
+  desc "Run all doctests embedded in the documentation (if enabled)"
   task :doctest do
+    run_sphinx "-b doctest #{ALLSPHINXOPTS} #{DOCS_BUILD_DIR}/doctest"
   end
 end
