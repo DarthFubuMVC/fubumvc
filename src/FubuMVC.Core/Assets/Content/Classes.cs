e93f8400f08966ce54dda07e055d6dec70e5aaaa
Jeremy Miller <jeremydmiller@yahoo.com>
2011-08-24 13:12:30 -0500

    little bit of bookkeeping and moving classes out into individual files

--- src/FubuMVC.Core/Assets/Content/Classes.cs
+++ src/FubuMVC.Core/Assets/Content/Classes.cs
@@ -1,27 +1,5 @@
-using System.Collections.Generic;
-using FubuMVC.Core.Assets.Files;
-
 namespace FubuMVC.Core.Assets.Content
 {
-    public interface IContentPipeline
-    {
-        string ReadContentsFrom(string file);
-        IAssetTransformer GetTransformer<T>() where T : IAssetTransformer;
-    }
-
-    public interface IContentSource
-    {
-        string GetContent(IContentPipeline pipeline);
-        IEnumerable<AssetFile> Files { get; }
-
-        IEnumerable<IContentSource> InnerSources { get; }
-    }
-
-    public interface IAssetTransformer
-    {
-        string Transform(string contents, IEnumerable<AssetFile> files);
-    }
-
     // Given an enumerable of AssetFile's, make the transformation plan
     public class TransformationPlanner
     {
