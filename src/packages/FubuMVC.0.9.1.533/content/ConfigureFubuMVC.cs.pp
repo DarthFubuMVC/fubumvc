5ccfd4d339eba60c5bd92d22f2a58bf7141635a5
Dru Sellers <dru@drusellers.com>
2011-09-07 13:30:33 -0500

    adding lib updates

---
+++ src/packages/FubuMVC.0.9.1.533/content/ConfigureFubuMVC.cs.pp
@@ -0,0 +1,27 @@
+using FubuMVC.Core;
+
+namespace $rootnamespace$
+{
+    public class ConfigureFubuMVC : FubuRegistry
+    {
+        public ConfigureFubuMVC()
+        {
+            // This line turns on the basic diagnostics and request tracing
+            IncludeDiagnostics(true);
+
+            // All public methods from concrete classes ending in "Controller"
+            // in this assembly are assumed to be action methods
+            Actions.IncludeClassesSuffixedWithController();
+
+            // Policies
+            Routes
+                .IgnoreControllerNamesEntirely()
+                .IgnoreMethodSuffix("Html")
+                .RootAtAssemblyNamespace();
+
+            // Match views to action methods by matching
+            // on model type, view name, and namespace
+            Views.TryToAttachWithDefaultConventions();
+        }
+    }
+}
