8ecbf2480cae3824f9e52f0e323f727de7f4882d
Ryan Rauh <rauh.ryan@gmail.com>
2011-09-07 20:14:02 -0500

    added tests for conditional behavior

--- src/FubuMVC.Tests/Behaviors/ConditionalBehaviorInvokerTester.cs
+++ src/FubuMVC.Tests/Behaviors/ConditionalBehaviorInvokerTester.cs
@@ -1,3 +1,4 @@
+using System;
 using FubuMVC.Core.Behaviors;
 using FubuTestingSupport;
 using NUnit.Framework;
@@ -37,7 +38,63 @@ namespace FubuMVC.Tests.Behaviors
             MockFor<IConditionalBehavior>().AssertWasNotCalled(x => x.Invoke());
             MockFor<IActionBehavior>().AssertWasNotCalled(x => x.Invoke());
         }
+    }
 
+    public class when_condition_is_true : InteractionContext<ConditionalBehavior>
+    {
+        protected override void beforeEach()
+        {
+            Services.Inject<Func<bool>>(() => true);
+        }
 
+        [Test]
+        public void inner_behavior_should_call_invoke()
+        {
+            //ACT
+            ClassUnderTest.Invoke();
+            
+            //ASSERT
+            MockFor<IActionBehavior>().AssertWasCalled(x => x.Invoke());
+            MockFor<IActionBehavior>().AssertWasNotCalled(x => x.InvokePartial());
+        }
+        [Test]
+        public void inner_behavior_should_call_invoke_partial()
+        {
+            //ACT
+            ClassUnderTest.InvokePartial();
+
+            //ASSERT
+            MockFor<IActionBehavior>().AssertWasNotCalled(x => x.Invoke());
+            MockFor<IActionBehavior>().AssertWasCalled(x => x.InvokePartial());
+        }
+    }
+
+    public class when_condition_is_false : InteractionContext<ConditionalBehavior>
+    {
+        protected override void beforeEach()
+        {
+            Services.Inject<Func<bool>>(() => false);
+        }
+
+        [Test]
+        public void inner_behavior_should_not_call_invoke()
+        {
+            //ACT
+            ClassUnderTest.Invoke();
+
+            //ASSERT
+            MockFor<IActionBehavior>().AssertWasNotCalled(x => x.Invoke());
+            MockFor<IActionBehavior>().AssertWasNotCalled(x => x.InvokePartial());
+        }
+        [Test]
+        public void inner_behavior_should_not_call_invoke_partial()
+        {
+            //ACT
+            ClassUnderTest.InvokePartial();
+
+            //ASSERT
+            MockFor<IActionBehavior>().AssertWasNotCalled(x => x.Invoke());
+            MockFor<IActionBehavior>().AssertWasNotCalled(x => x.InvokePartial());
+        }
     }
 }
