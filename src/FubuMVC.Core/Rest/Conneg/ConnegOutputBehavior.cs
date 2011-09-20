9d0165446cee83e2fe038f31dcf1ba992ba733a3
Jeremy Miller <jeremydmiller@yahoo.com>
2011-09-20 17:52:25 -0500

    Checkpoint:  implemented the logic to select the correct IMediaWriter based on the accept-types

---
+++ src/FubuMVC.Core/Rest/Conneg/ConnegOutputBehavior.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using FubuMVC.Core.Behaviors;
+using FubuMVC.Core.Http;
+using FubuMVC.Core.Rest.Media;
+using FubuMVC.Core.Runtime;
+using System.Linq;
+
+namespace FubuMVC.Core.Rest.Conneg
+{
+    public class ConnegOutputBehavior<T> : BasicBehavior
+    {
+        // TODO -- move to MimeTypes when Assets is put in place
+        public static readonly string HttpFormMimetype = "application/x-www-form-urlencoded";
+
+        private readonly IValueSource<T> _source;
+        private readonly IFubuRequest _request;
+        private readonly IOutputWriter _writer;
+        private readonly IEnumerable<IMediaWriter<T>> _writers;
+
+        public ConnegOutputBehavior(IValueSource<T> source, IFubuRequest request, IOutputWriter writer, IEnumerable<IMediaWriter<T>> writers) : base(PartialBehavior.Executes)
+        {
+            _source = source;
+            _request = request;
+            _writer = writer;
+            _writers = writers;
+        }
+
+        protected override DoNext performInvoke()
+        {
+            throw new NotImplementedException();
+
+            var mimeTypes = _request.Get<CurrentMimeType>();
+            if (mimeTypes.AcceptTypes.Contains(HttpFormMimetype))
+            {
+                return DoNext.Continue;
+            }
+
+            var writer = SelectWriter(mimeTypes);
+            if (writer == null)
+            {
+                _writer.WriteResponseCode(HttpStatusCode.NotAcceptable);
+            }
+            else
+            {
+                writer.Write(_source.FindValues(), _writer);
+            }
+
+            return DoNext.Stop;
+
+            
+        }
+
+        public IMediaWriter<T> SelectWriter(CurrentMimeType mimeTypes)
+        {
+            foreach (var acceptType in mimeTypes.AcceptTypes)
+            {
+                if (acceptType == "*/*")
+                {
+                    // TODO -- use MimeType.Html after Assets is merged in
+                    return _writers.FirstOrDefault(x => x.Mimetypes.Contains("text/html"))
+                           ?? _writers.FirstOrDefault();
+                }
+
+                var writer = _writers.FirstOrDefault(x => x.Mimetypes.Contains(acceptType));
+                if (writer != null) return writer;
+            }
+
+            return null;
+        }
+    }
+}
