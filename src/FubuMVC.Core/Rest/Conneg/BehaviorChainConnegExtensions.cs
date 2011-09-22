af511be2e6c703582d06e61e6b2601ef605694fb
Jeremy Miller <jeremydmiller@yahoo.com>
2011-09-22 08:20:21 -0500

    deleted the old ConnegBehaviorConvention, got new functionality in for manipulating conneg nodes into behavior chains

---
+++ src/FubuMVC.Core/Rest/Conneg/BehaviorChainConnegExtensions.cs
@@ -0,0 +1,65 @@
+using System;
+using FubuMVC.Core.Registration.Nodes;
+using System.Linq;
+using System.Collections.Generic;
+using FubuMVC.Core.Rest.Media.Formatters;
+
+namespace FubuMVC.Core.Rest.Conneg
+{
+    public static class BehaviorChainConnegExtensions
+    {
+        // This should not do anything if there are conneg nodes
+        public static void ApplyConneg(this BehaviorChain chain)
+        {
+            var inputType = chain.InputType();
+            if (chain.ConnegInputNode() == null && inputType != null)
+            {
+                var inputNode = new ConnegInputNode(inputType);
+                var action = chain.FirstCall();
+                action.AddBefore(inputNode);
+            }
+
+            var actionOutputType = chain.ActionOutputType();
+            if (chain.ConnegOutputNode() == null && actionOutputType != null)
+            {
+                var outputNode = new ConnegOutputNode(actionOutputType);
+                var action = chain.Last(x => x is ActionCall);
+                action.AddAfter(outputNode);
+            }
+        }
+
+        public static void MakeSymmetricJson(this BehaviorChain chain)
+        {
+            chain.RemoveConneg();
+            chain.ApplyConneg();
+
+            chain.ConnegInputNode().JsonOnly();
+            chain.ConnegOutputNode().JsonOnly();
+        }
+
+        public static void MakeAsymmetricJson(this BehaviorChain chain)
+        {
+            chain.RemoveConneg();
+            chain.ApplyConneg();
+
+            chain.ConnegInputNode().UseFormatter<JsonFormatter>();
+            chain.ConnegOutputNode().JsonOnly();
+        }
+
+        public static ConnegInputNode ConnegInputNode(this BehaviorChain chain)
+        {
+            return chain.FirstOrDefault(x => x is ConnegInputNode) as ConnegInputNode;
+        }
+
+        public static ConnegOutputNode ConnegOutputNode(this BehaviorChain chain)
+        {
+            return chain.FirstOrDefault(x => x is ConnegOutputNode) as ConnegOutputNode;
+        }
+
+        public static void RemoveConneg(this BehaviorChain chain)
+        {
+            chain.OfType<ConnegNode>().ToList().Each(x => x.Remove());
+        }
+
+    }
+}
