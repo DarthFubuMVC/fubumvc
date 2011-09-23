7cf85d7dce47de17bb18d219b9086afd3646f2f7
Jeremy Miller <jeremydmiller@yahoo.com>
2011-09-23 08:34:14 -0500

    added the CurrentMimeTypeModelBinder

--- src/FubuMVC.Core/Http/CurrentMimeType.cs
+++ src/FubuMVC.Core/Http/CurrentMimeType.cs
@@ -2,10 +2,6 @@ namespace FubuMVC.Core.Http
 {
     public class CurrentMimeType
     {
-        // TODO -- really, really have to make sure this works in model binding
-        // cause it doesn't right now
-        // May need a custom model binder for this.
-
         public CurrentMimeType()
         {
         }
