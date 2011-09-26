43bb7b7b6d805372337bdda4a0ae768a76f8898f
Jeremy Miller <jeremydmiller@yahoo.com>
2011-09-26 16:11:15 -0500

    checkpoint:  skeletonizing the additions to Feed<T>

--- src/FubuMVC.Core/Rest/Media/Atom/AtomClasses.cs
+++ src/FubuMVC.Core/Rest/Media/Atom/AtomClasses.cs
@@ -1,38 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.ServiceModel.Syndication;
-using FubuCore.Reflection;
 using FubuLocalization;
-using FubuMVC.Core.Rest.Media.Projections;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Rest.Media.Xml;
+using FubuMVC.Core.Urls;
 
 namespace FubuMVC.Core.Rest.Media.Atom
 {
-    // Just use SyndicationLink
+    public interface IFeedDefinition<T>
+    {
+        IXmlMediaWriter<T> BuildExtensionWriter(IUrlRegistry urls);
 
-    public class Feed<T>
+        // TODO -- think we need to inject a system date time provider thingie
+        void ConfigureFeed(SyndicationFeed feed, DateTime updated);
+        void ConfigureItem(SyndicationItem item, IValues<T> values);
+    }
+
+
+    public class Feed<T> : IResourceRegistration, IFeedDefinition<T>
     {
         private readonly IList<Action<SyndicationFeed>> _alterations = new List<Action<SyndicationFeed>>();
         private readonly IList<FeedItem<T>> _maps = new List<FeedItem<T>>();
-
-        public SyndicationFeed CreateFeed(DateTime updated, IEnumerable<IValues<T>> targets)
-        {
-            var feed = new SyndicationFeed{
-                LastUpdatedTime = updated,
-                Items = targets.Select(target =>
-                {
-                    var item = new SyndicationItem();
-                    _maps.Each(map => map.ConfigureItem(item, target));
-
-                    return item;
-                })
-            };
-
-            _alterations.Each(x => x(feed));
-
-            return feed;
-        }
 
         public void Title(StringToken title)
         {
@@ -53,6 +43,26 @@ namespace FubuMVC.Core.Rest.Media.Atom
         {
             throw new NotImplementedException();
         }
+
+        void IResourceRegistration.Modify(ConnegGraph graph, BehaviorGraph behaviorGraph)
+        {
+            throw new NotImplementedException();
+        }
+
+        IXmlMediaWriter<T> IFeedDefinition<T>.BuildExtensionWriter(IUrlRegistry urls)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IFeedDefinition<T>.ConfigureFeed(SyndicationFeed feed, DateTime updated)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IFeedDefinition<T>.ConfigureItem(SyndicationItem item, IValues<T> values)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public static class SyndicationStringExtensions
@@ -60,73 +70,6 @@ namespace FubuMVC.Core.Rest.Media.Atom
         public static TextSyndicationContent ToContent(this string text)
         {
             return new TextSyndicationContent(text);
-        }
-    }
-
-    public class FeedItem<T>
-    {
-        private readonly IList<Action<IValues<T>, SyndicationItem>> _modifications
-            = new List<Action<IValues<T>, SyndicationItem>>();
-
-        public FeedItem()
-        {
-        }
-
-        public FeedItem(Action<FeedItem<T>> configure)
-        {
-            configure(this);
-        }
-
-        private Action<IValues<T>, SyndicationItem> alter
-        {
-            set { _modifications.Add(value); }
-        }
-
-        public void ConfigureItem(SyndicationItem item, IValues<T> target)
-        {
-            _modifications.Each(x => x(target, item));
-        }
-
-        private void modify<TArg>(Expression<Func<T, TArg>> property, Action<SyndicationItem, TArg> modification)
-        {
-            var accessor = ReflectionHelper.GetAccessor(property);
-            alter = (target, item) =>
-            {
-                var value = target.ValueFor(accessor);
-
-                if (value != null)
-                {
-                    modification(item, (TArg) value);
-                }
-            };
-        }
-
-
-        public void Title(Expression<Func<T, string>> expression)
-        {
-            modify(expression, (item, value) => item.Title = value.ToContent());
-        }
-
-        public void Id(Expression<Func<T, object>> expression)
-        {
-            modify(expression, (item, value) => item.Id = value.ToString());
-        }
-
-        public void UpdatedByProperty(Expression<Func<T, DateTime>> property)
-        {
-            var accessor = ReflectionHelper.GetAccessor(property);
-            alter = (target, item) => item.LastUpdatedTime = (DateTime) target.ValueFor(accessor);
-        }
-
-
-        public void Extension(string xsd, Action<Projection<T>> configure)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Extension<TProjection>(string xsd) where TProjection : Projection<T>
-        {
-            throw new NotImplementedException();
         }
     }
 }
