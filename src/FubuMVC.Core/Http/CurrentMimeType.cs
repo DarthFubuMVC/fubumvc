05f021411059c78e7763a1f01ae28826c6b5eff6
Jeremy Miller <jeremydmiller@yahoo.com>
2011-09-28 11:20:59 -0500

    being able to read charset's in the Content-Type header

--- src/FubuMVC.Core/Http/CurrentMimeType.cs
+++ src/FubuMVC.Core/Http/CurrentMimeType.cs
@@ -1,18 +1,41 @@
+using System;
+using FubuCore;
+using System.Linq;
+
 namespace FubuMVC.Core.Http
 {
     public class CurrentMimeType
     {
+        // TODO -- move to MimeTypes when Assets is put in place
+        public static readonly string HttpFormMimetype = "application/x-www-form-urlencoded";
+
         public CurrentMimeType()
         {
         }
 
+        // If contenttype is null, use the url encoded thing
         public CurrentMimeType(string contentType, string acceptType)
         {
-            ContentType = new MimeTypeList(contentType);
+            contentType = contentType ?? HttpFormMimetype;
+            if (contentType.Contains(";"))
+            {
+                var parts = contentType.ToDelimitedArray(';');
+                contentType = parts.First();
+
+                if (parts.Last().Contains("charset"))
+                {
+                    Charset = parts.Last().Split('=').Last();
+                }
+            }
+
+            ContentType = contentType;
+            
             AcceptTypes = new MimeTypeList(acceptType);
         }
 
-        public MimeTypeList ContentType { get; set; }
+        public string ContentType { get; set; }
         public MimeTypeList AcceptTypes { get; set; }
+
+        public string Charset { get; set; }
     }
 }
