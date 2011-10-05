8b76016dc33bba995b00a4417dc68dec6661b17a
Jeremy Miller <jeremydmiller@yahoo.com>
2011-10-05 11:07:29 -0500

    checkpoint:  Can build all webdriver types, but guess which browser requires some fiddling to make it work? (IE)

--- src/Serenity/ApplicationUnderTest.cs
+++ src/Serenity/ApplicationUnderTest.cs
@@ -71,8 +71,6 @@ namespace Serenity
 
             Driver.Close();
             Driver.SafeDispose();
-
-            
         }
 
         public IWebDriver Driver
--- src/Serenity/Classes.cs
+++ src/Serenity/Classes.cs
@@ -1,38 +1,54 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using FubuCore.Configuration;
+using FubuCore.Util;
+using FubuMVC.Core;
 using FubuMVC.Core.Urls;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 using StoryTeller.Engine;
 
 namespace Serenity
 {
-
-
-
-    public abstract class FubuMvcSystem : BasicSystem
+    public class FubuMvcSystem : BasicSystem
     {
-        private readonly IList<IApplicationUnderTest> _applications = new List<IApplicationUnderTest>();
-        // The first one should be considered to be the main one
-        
-        public void AddApplication(IApplicationUnderTest application)
+        private readonly SerenityApplications _applications = new SerenityApplications();
+        private readonly Func<IWebDriver> _browserBuilder;
+
+        public FubuMvcSystem()
         {
-            _applications.Add(application);
+            _browserBuilder = WebDriverSettings.Read().DriverBuilder();
         }
 
-        //public override object Get(Type type)
-        //{
-        //    return 
-        //}
+        public SerenityApplications Applications
+        {
+            get { return _applications; }
+        }
+
+        public void AddApplication(IApplicationSource source)
+        {
+            var settings = ApplicationSettings.ReadByName(source.Name);
+            var application = new ApplicationUnderTest(source, settings, _browserBuilder);
+            _applications.AddApplication(application);
+        }
+
+        public void AddApplication<T>() where T : IApplicationSource, new()
+        {
+            AddApplication(new T());
+        }
 
         public override void RegisterServices(ITestContext context)
         {
-            // TODO -- does need to push in the IApplicationUnderTest
-            throw new NotSupportedException();
+            context.Store(_applications);
+            context.Store(_applications.PrimaryApplication());
         }
 
         public override void SetupEnvironment()
         {
-            // Need to spin up the browser
             _applications.Each(x => x.Ping());
         }
 
@@ -40,10 +56,43 @@ namespace Serenity
         {
             _applications.Each(x => x.Teardown());
         }
+    }
 
+    public class SerenityApplications : IEnumerable<IApplicationUnderTest>
+    {
+        private readonly Cache<string, IApplicationUnderTest> _applications = new Cache<string, IApplicationUnderTest>();
+        private IApplicationUnderTest _primary;
 
+        public IEnumerator<IApplicationUnderTest> GetEnumerator()
+        {
+            return _applications.GetEnumerator();
+        }
 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
 
+        public void AddPrimaryApplication(IApplicationUnderTest application)
+        {
+            AddApplication(application);
+            _primary = application;
+        }
+
+        public void AddApplication(IApplicationUnderTest application)
+        {
+            if (!_applications.Any())
+            {
+                _primary = application;    
+            }
+
+            _applications[application.Name] = application;
+        }
+
+        public IApplicationUnderTest PrimaryApplication()
+        {
+            return _primary ?? _applications.GetAll().FirstOrDefault();
+        }
     }
 
 
@@ -51,41 +100,87 @@ namespace Serenity
     public interface IApplicationUnderTest
     {
         string Name { get; }
+        IUrlRegistry Urls { get; }
+        IWebDriver Driver { get; }
 
         T GetInstance<T>();
         IEnumerable<T> GetAll<T>();
 
-        IUrlRegistry Urls { get; }
-
         void Ping();
 
         void Teardown();
+    }
 
-        IWebDriver Driver { get; }
+    // TODO -- Add Safari/who knows what in the future?
+    // Make it open ended so you can just say the type of an IWebDriver class later
+    public enum BrowserType
+    {
+        Firefox,
+        IE,
+        Chrome
     }
 
 
-    // This will be used to set up the application
     public class WebDriverSettings
     {
-        public string BrowserName { get; set;}
+        public static readonly string Filename = "browser.settings"; 
+
+        public WebDriverSettings()
+        {
+            // Why is the default Firefox you ask?  Because it's the first one that Jeremy got working on his box.
+            Browser = BrowserType.Firefox;
+        }
+
+        public BrowserType Browser { get; set; }
+
+        public static WebDriverSettings Read()
+        {
+            var settings = SettingsData.ReadFromFile(SettingCategory.core, Filename);
+            return SettingsProvider.For(settings).SettingsFor<WebDriverSettings>();
+        }
+
+        public Func<IWebDriver> DriverBuilder()
+        {
+            switch (Browser)
+            {
+                case BrowserType.Chrome:
+                    return () => new ChromeDriver();
+
+                case BrowserType.IE:
+                    return () => new InternetExplorerDriver();
+
+                case BrowserType.Firefox:
+                    return () => new FirefoxDriver();
+
+                default:
+                    throw new ArgumentOutOfRangeException("Unrecognized browser");
+            }
+        }
     }
-    
+
     // An application
     public class ApplicationSettings
     {
-        public string Name { get; set; }
         public string PhysicalPath { get; set; }
         public string RootUrl { get; set; }
+
+        public static ApplicationSettings Read(string file)
+        {
+            var settings = SettingsData.ReadFromFile(SettingCategory.core, file);
+            return SettingsProvider.For(settings).SettingsFor<ApplicationSettings>();
+        }
+
+        public static ApplicationSettings ReadByName(string name)
+        {
+            return Read(name + ".application");
+        }
     }
 
     public class ScreenFixture : Fixture
     {
-        
     }
 
     public abstract class ScreenFixture<T> : Fixture
     {
-        
     }
 }
