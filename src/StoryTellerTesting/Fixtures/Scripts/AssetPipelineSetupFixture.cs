0f6361ed63264c61c67ad49012743b0accea3374
Jeremy Miller <jeremydmiller@yahoo.com>
2011-10-13 08:24:58 -0500

    checkpoint:  changed test path to reflect assets, first pass at AssetPipelineSetupFixture

---
+++ src/StoryTellerTesting/Fixtures/Scripts/AssetPipelineSetupFixture.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using Bottles;
+using FubuCore.Util;
+using StoryTeller.Engine;
+using FubuCore;
+using StoryTeller;
+
+namespace IntegrationTesting.Fixtures.Scripts
+{
+    public class AssetPipelineSetupFixture : Fixture
+    {
+        private CommandRunner _runner;
+
+        private static readonly string TemporaryPackagesFolder =
+            AppDomain.CurrentDomain.BaseDirectory.AppendPath("packages");
+
+        private string _packageDirectory;
+        private FileSystem _fileSystem;
+
+        public AssetPipelineSetupFixture()
+        {
+            this["WriteContents"] = this["WriteLine"]
+                .AsTable("Asset File Contents")
+                .After(flushContents);
+
+            
+        }
+
+        public override void SetUp(ITestContext context)
+        {
+            _fileSystem = new FileSystem();
+            _runner = context.Retrieve<CommandRunner>();
+            removeAnyExistingPackages();
+        }
+
+        private void removeAnyExistingPackages()
+        {
+            _runner.RunFubu("packages fubu-testing -removeall");
+
+
+            
+            _fileSystem.CreateDirectory(TemporaryPackagesFolder);
+            _fileSystem.CleanDirectory(TemporaryPackagesFolder);
+        }
+
+        public override void TearDown()
+        {
+            _runner.RunFubu("restart fubu-testing");
+        }
+
+        [FormatAs("For package {packageName}")]
+        public void ForPackage(string packageName)
+        {
+            _packageDirectory = TemporaryPackagesFolder.AppendPath(packageName);
+            _fileSystem.CreateDirectory(_packageDirectory);
+
+            var manifest = new PackageManifest{
+                Role = "module",
+                Name = packageName
+            };
+
+            _fileSystem.PersistToFile(manifest, _packageDirectory, PackageManifest.FILE);
+        }
+
+        private readonly Cache<string, IList<string>> _contents = new Cache<string, IList<string>>(key => new List<string>());
+        private string _currentFile;
+
+        private void flushContents()
+        {
+            _contents.Each((file, contents) =>
+            {
+                var fileInPackage = _packageDirectory.AppendPath(file);
+                _fileSystem.AlterFlatFile(fileInPackage, list => list.AddRange(contents));
+            });
+
+            _contents.ClearAll();
+        }
+
+        [Hidden]
+        public void WriteLine(string File, string Contents)
+        {
+            _contents[File].Add(Contents);
+        }
+
+        [Hidden]
+        [FormatAs("File {file}")]
+        public void ForFile(string file)
+        {
+            _currentFile = file;
+        }
+
+        [Hidden]
+        public void WriteFileLine(string Content)
+        {
+            _contents[_currentFile].Add(Content);
+        }
+
+        public IGrammar WriteFile()
+        {
+            return Paragraph("Write package file", x =>
+            {
+                x += this["ForFile"];
+                x += this["WriteFileLine"].AsTable("The contents are").LeafName("lines");
+                x += flushContents;
+            });
+        }
+    }
+}
