d444aa00a3b7f05609f9eb561470e294df5b0cd4
jmarnold <rncodepoet@gmail.com>
2011-10-15 21:27:53 -0500

    Removed ActionSourceMatcher

--- src/FubuMVC.Core/Registration/IActionSource.cs
+++ src/FubuMVC.Core/Registration/IActionSource.cs
@@ -8,34 +8,4 @@ namespace FubuMVC.Core.Registration
     {
         IEnumerable<ActionCall> FindActions(TypePool types);
     }
-
-    public class ActionSourceMatcher
-    {
-        private BehaviorGraph _graph;
-        private readonly List<IActionSource> _actionSources = new List<IActionSource>();
-        public void BuildBehaviors(TypePool pool, BehaviorGraph graph)
-        {
-            _graph = graph;
-            buildActions(pool).Each(registerBehavior);
-            _graph = null;
-        }
-
-        public void AddSource(IActionSource source)
-        {
-            _actionSources.Add(source);
-        }
-
-        private IEnumerable<ActionCall> buildActions(TypePool types)
-        {
-            return _actionSources.SelectMany(actionSource => actionSource.FindActions(types));
-        }
-
-        //TODO -- Really similar to BehaviorMatcher. Can we reuse?
-        private void registerBehavior(ActionCall call)
-        {
-            var chain = new BehaviorChain();
-            chain.AddToEnd(call);
-            _graph.AddChain(chain);
-        }
-    }
 }
