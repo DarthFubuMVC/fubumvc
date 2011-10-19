ecc376b0dec15b50135a70945e0f95f0b29b8a0e
Jeremy Miller <jeremydmiller@yahoo.com>
2011-10-19 12:40:19 -0500

    first spike w/ Kayak and Routing playing nice together

--- src/FubuKayak/Program.cs
+++ src/FubuKayak/Program.cs
@@ -1,7 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Web;
+using System.Web.Routing;
+using FubuCore;
+using FubuMVC.OwinHost;
+using Gate;
+using Gate.Helpers;
+using Gate.Kayak;
+using HtmlTags;
+using Kayak;
 
 namespace FubuKayak
 {
@@ -9,6 +19,100 @@ namespace FubuKayak
     {
         static void Main(string[] args)
         {
+            RouteTable.Routes.Add(new Route("what/is/it", new FakeHandler()));
+            RouteTable.Routes.Add(new Route("my/name/{is}", new FakeHandler()));
+
+
+            var ep = new IPEndPoint(IPAddress.Any, 5500);
+            Console.WriteLine("Listening on " + ep);
+            KayakGate.Start(new SchedulerDelegate(), ep, Startup.Configuration);
+        }
+    }
+
+    public class FakeHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return null;
+        }
+    }
+
+    public class Startup
+    {
+        // called automatically when Kayak starts up.
+        public static void Configuration(IAppBuilder builder)
+        {
+            // we'll create a very simple pipeline:
+
+            builder
+                // insert a middleware between Kayak and Nancy
+                // to ensure Kayak objects are only accessed 
+                // on Kayak's worker thread.
+                .RescheduleCallbacks()
+
+                // cap off the pipeline with Nancy
+                .Run(ExecuteRequest);
+        }
+
+        public static void ExecuteRequest(IDictionary<string, object> env, ResultDelegate result, Action<Exception> fault)
+        {
+            var request = new Request(env);
+            var context = new GateHttpContext(request);
+
+            var document = new HtmlDocument();
+
+            document.Add("h1").Text("Path:  " + request.Path);
+
+            var data = RouteTable.Routes.GetRouteData(context);
+            if (data != null)
+            {
+                document.Add("p").Text("Route is " + data.Route.As<Route>().Url);
+                
+                
+                if (data.Values != null)
+                {
+                    document.Push("table");
+                    data.Values.Each(pair =>
+                    {
+                        document.Push("tr");
+                        document.Add("td").Text(pair.Key);
+                        document.Add("td").Text(pair.Value == null ? string.Empty : pair.Value.ToString());
+                        document.Pop();
+                        
+                    });
+
+                    document.Pop();
+                }
+            }
+
+            var response = new Response(result);
+
+
+
+            response.ContentType = "text/html";
+            response.Write(document.ToString());
+
+            response.Finish();
+        }
+    }
+
+    
+
+    public class SchedulerDelegate : ISchedulerDelegate
+    {
+        public void OnException(IScheduler scheduler, Exception e)
+        {
+            // called whenever an exception occurs on Kayak's event loop.
+            // this is good place for logging. here's a start:
+            Console.WriteLine("Exception on scheduler");
+            Console.Out.WriteStackTrace(e);
+        }
+
+        public void OnStop(IScheduler scheduler)
+        {
+            // called when Kayak's run loop is about to exit.
+            // this is a good place for doing clean-up or other chores.
+            Console.WriteLine("Scheduler is stopping.");
         }
     }
 }
--- src/FubuMVC.OwinHost/GateHttpContext.cs
+++ src/FubuMVC.OwinHost/GateHttpContext.cs
@@ -1,11 +1,41 @@
+using System;
 using System.Web;
+using Gate.Helpers;
 
 namespace FubuMVC.OwinHost
 {
     public class GateHttpContext : HttpContextBase
     {
-        public GateHttpContext(Gate.R)
+        private readonly GateHttpRequest _request;
+
+        public GateHttpContext(Request request)
         {
+            _request = new GateHttpRequest(request);
+        }
+
+        public override HttpRequestBase Request
+        {
+            get { return _request; }
+        }
+    }
+
+    public class GateHttpRequest : HttpRequestBase
+    {
+        private readonly Request _request;
+
+        public GateHttpRequest(Request request)
+        {
+            _request = request;
+        }
+
+        public override string PathInfo
+        {
+            get { return _request.Path.TrimStart('/'); }
+        }
+
+        public override string AppRelativeCurrentExecutionFilePath
+        {
+            get { return "~/"; }
         }
     }
 }
