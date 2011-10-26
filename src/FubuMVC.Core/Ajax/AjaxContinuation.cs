12fbc2f9e769afc6cc3b93eb44e36624d3f1904a
Jeremy Miller <jeremydmiller@yahoo.com>
2011-10-26 13:29:16 -0500

    added the AjaxContinuation and matching unit tests

---
+++ src/FubuMVC.Core/Ajax/AjaxContinuation.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using FubuCore.Util;
+using FubuLocalization;
+using FubuCore;
+using System.Linq;
+
+namespace FubuMVC.Core.Ajax
+{
+    public class AjaxError
+    {
+        // error/warning/I don't know.
+        public string category { get; set; }
+
+        // Use this to attach the server side validation errors 
+        public string field { get; set; }
+        public string message { get; set; }
+    }
+
+    public class AjaxContinuation
+    {
+        // Probably put some convenience static builder methods here for things like
+        public static AjaxContinuation Successful()
+        {
+            return new AjaxContinuation{
+                Success = true
+            };
+        }
+
+        public static AjaxContinuation ForMessage(StringToken message)
+        {
+            return ForMessage(message.ToString());
+        }
+
+        public static AjaxContinuation ForMessage(string message)
+        {
+            return new AjaxContinuation{
+                Message = message
+            };
+        }
+
+        private readonly Cache<string, object> _data = new Cache<string, object>();
+        private readonly IList<AjaxError> _errors = new List<AjaxError>();
+        
+        // You'll want to smuggle in your own data in all likelihood
+        [IndexerName("Data")]
+        public object this[string key]
+        {
+            get
+            {
+                return _data[key];
+            }
+            set
+            {
+                _data[key] = value;
+            }
+        }
+
+        // I don't care enough about Law of Demeter here
+        // to do anything differently
+        public IList<AjaxError> Errors
+        {
+            get { return _errors; }
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        // *This* will be serialized to Json so that the resulting blob
+        // of json data is easier to work with in JavaScript
+        public IDictionary<string, object> ToDictionary()
+        {
+            var dict = new Dictionary<string, object>{
+                {"success", Success}
+            };
+
+            Message.IfNotNull(x => dict.Add("message", x));
+
+            _data.Each(dict.Add);
+
+            if (_errors.Any())
+            {
+                dict.Add("errors", _errors.ToArray());
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ src/FubuMVC.Tests/Ajax/AjaxContinuationTester.cs
@@ -0,0 +1,81 @@
+using FubuCore;
+using FubuLocalization;
+using FubuMVC.Core.Ajax;
+using NUnit.Framework;
+using FubuTestingSupport;
+using System.Linq;
+
+namespace FubuMVC.Tests.Ajax
+{
+    [TestFixture]
+    public class AjaxContinuationTester
+    {
+        private AjaxContinuation theContinuation;
+
+        [SetUp]
+        public void SetUp()
+        {
+            theContinuation = new AjaxContinuation();
+        }
+
+        [Test]
+        public void success_is_placed_into_the_dictionary()
+        {
+            theContinuation.Success = false;
+            theContinuation.ToDictionary()["success"].As<bool>().ShouldBeFalse();
+
+            theContinuation.Success = true;
+            theContinuation.ToDictionary()["success"].As<bool>().ShouldBeTrue();
+        }
+
+        [Test]
+        public void message_is_placed_into_the_dictionary_if_it_exists()
+        {
+            theContinuation.ToDictionary().ContainsKey("message").ShouldBeFalse();
+
+            theContinuation.Message = "something";
+
+            theContinuation.ToDictionary()["message"].ShouldEqual("something");
+        }
+
+        [Test]
+        public void errors_are_only_written_to_the_dictionary_if_they_exist()
+        {
+            theContinuation.ToDictionary().ContainsKey("errors").ShouldBeFalse();
+
+            theContinuation.Errors.Add(new AjaxError(){message = "bad!"});
+
+            theContinuation.ToDictionary()["errors"].ShouldBeOfType<AjaxError[]>()
+                .Single().message.ShouldEqual("bad!");
+        }
+
+        [Test]
+        public void Successful_builder_method()
+        {
+            var success = AjaxContinuation.Successful();
+        
+            success.Errors.Any().ShouldBeFalse();
+            success.Success.ShouldBeTrue();
+        }
+
+        [Test]
+        public void ForMessage_builder_method()
+        {
+            var continuation = AjaxContinuation.ForMessage("some message");
+
+            continuation.Success.ShouldBeFalse();
+            continuation.Message.ShouldEqual("some message");
+        }
+
+        [Test]
+        public void ForMessage_via_StringToken()
+        {
+            var token = StringToken.FromKeyString("key", "some message");
+
+            var continuation = AjaxContinuation.ForMessage(token);
+
+            continuation.Success.ShouldBeFalse();
+            continuation.Message.ShouldEqual(token.ToString());
+        }
+    }
+}
