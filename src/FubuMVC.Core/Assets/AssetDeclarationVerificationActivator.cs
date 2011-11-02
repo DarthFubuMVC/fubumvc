a55541a14d735d34f661c629b976f087d1bb931f
Ryan Rauh <rauh.ryan@gmail.com>
2011-11-02 10:12:16 -0500

    added a more robust error message to the asset pipeline

--- src/FubuMVC.Core/Assets/AssetDeclarationVerificationActivator.cs
+++ src/FubuMVC.Core/Assets/AssetDeclarationVerificationActivator.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Bottles;
 using Bottles.Diagnostics;
+using FubuMVC.Core.Assets.Diagnostics;
 using FubuMVC.Core.Assets.Files;
 
 namespace FubuMVC.Core.Assets
@@ -8,20 +9,22 @@ namespace FubuMVC.Core.Assets
     public class AssetDeclarationVerificationActivator : IActivator
     {
         private readonly AssetGraph _graph;
+        private readonly AssetLogs _assetLogs;
         private readonly IAssetPipeline _pipeline;
 
         public static bool Latched { get; set; }
 
-        public AssetDeclarationVerificationActivator(IAssetPipeline pipeline, AssetGraph graph)
+        public AssetDeclarationVerificationActivator(IAssetPipeline pipeline, AssetGraph graph, AssetLogs assetLogs)
         {
             _pipeline = pipeline;
             _graph = graph;
+            _assetLogs = assetLogs;
         }
 
         // No automated tests for this.
         public void Activate(IEnumerable<IPackageInfo> packages, IPackageLog log)
         {
-            var checker = new AssetDeclarationChecker(_pipeline, log);
+            var checker = new AssetDeclarationChecker(_pipeline, log, _assetLogs);
             _graph.AllDependencies().Each(x => checker.VerifyFileDependency(x.Name));
         }
 
