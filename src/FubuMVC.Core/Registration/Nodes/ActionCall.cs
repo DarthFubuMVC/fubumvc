d2ac60f661b0e3ee203e1e55e75f466b9066d46c
Jeremy Miller <jeremydmiller@yahoo.com>
2011-11-14 19:37:53 -0600

    merging Kevin's changes

--- src/FubuMVC.Core/Registration/Nodes/ActionCall.cs
+++ src/FubuMVC.Core/Registration/Nodes/ActionCall.cs
@@ -21,10 +21,7 @@ namespace FubuMVC.Core.Registration.Nodes
         }
 
         public override BehaviorCategory Category { get { return BehaviorCategory.Call; } }
-<<<<<<< HEAD
-=======
-        public override string Description { get { return "{0}.{1}({2}) : {3}".ToFormat(HandlerType.Name, Method.Name, getInputParameters(), HasOutput ? Method.ReturnType.Name : "void"); } }
->>>>>>> Removed compiler warnings
+
 
         public void ForAttributes<T>(Action<T> action) where T : Attribute
         {
