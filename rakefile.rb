2cf7f023bc16ba305e99a44cfe10e0930dd1bb9e
Jeremy Miller <jeremydmiller@yahoo.com>
2011-11-15 08:00:13 -0600

    turning off nuget publishing for now

--- rakefile.rb
+++ rakefile.rb
@@ -27,7 +27,7 @@ desc "**Default**, compiles and runs tests"
 task :default => [:compile, :unit_test]
 
 desc "Target used for the CI server"
-task :ci => [:update_all_dependencies, :default, "template:build", :history, :publish]
+task :ci => [:update_all_dependencies, :default, "template:build", :history]
 
 desc "Update the version information for the build"
 assemblyinfo :version do |asm|
