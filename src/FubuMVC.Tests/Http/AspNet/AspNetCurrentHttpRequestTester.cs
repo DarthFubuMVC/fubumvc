df6f6e8ee4f138e10d9d8831babd00e4210d5c01
Ryan Rauh <rauh.ryan@gmail.com>
2011-11-22 14:19:06 -0600

    fixing storyteller tests
    I've fixed this one before, its failing for a really silly reason

---
+++ src/FubuMVC.Tests/Http/AspNet/AspNetCurrentHttpRequestTester.cs
@@ -0,0 +1,17 @@
+﻿using FubuMVC.Core.Http.AspNet;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuMVC.Tests.Http.AspNet
+{
+
+    [TestFixture]
+    public class AspNetCurrentHttpRequestTester : InteractionContext<AspNetCurrentHttpRequest>
+    {
+        [Test]
+        public void to_full_url_should_pass_through_a_well_formed_relative_url()
+        {
+            
+        }
+    }
+}
