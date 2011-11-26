f4e06a3ce1a94069edfbd1992b80025616c885e1
jmarnold <rncodepoet@gmail.com>
2011-11-26 01:26:28 -0600

    added end to end test around git cloning

---
+++ src/FubuMVC.Tests/Templating/NewCommandEndToEndTester.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Bottles.Exploding;
+using Bottles.Zipping;
+using Fubu;
+using FubuCore;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuMVC.Tests.Templating
+{
+    [TestFixture]
+    public class NewCommandEndToEndTester
+    {
+        private NewCommand _command;
+        private FileSystem _fileSystem;
+        private ZipFileService _zipService;
+        private NewCommandInput _commandInput;
+
+        [SetUp]
+        public void before_each()
+        {
+            _command = new NewCommand();
+            _fileSystem = new FileSystem();
+            _zipService = new ZipFileService(_fileSystem);
+            _commandInput = new NewCommandInput();
+        }
+
+        [Test]
+        public void clone_git_repo_and_append_to_existing_solution()
+        {
+            var tmpDir = FileSystem.Combine("Templating", Guid.NewGuid().ToString());
+            var repoZip = FileSystem.Combine("Templating", "repo.zip");
+            _zipService.ExtractTo(repoZip, tmpDir, ExplodeOptions.DeleteDestination);
+
+            var solutionFile = FileSystem.Combine("Templating", "sample", "myproject.txt");
+            var oldContents = _fileSystem.ReadStringFromFile(solutionFile);
+            var solutionDir = _fileSystem.GetDirectory(solutionFile);
+
+            _commandInput.GitFlag = "file:///{0}".ToFormat(_fileSystem.GetFullPath(tmpDir).Replace("\\", "/"));
+            _commandInput.ProjectName = "MyProject";
+            _commandInput.SolutionFlag = solutionFile;
+            _commandInput.OutputFlag = solutionDir;
+
+            _command
+                .Execute(_commandInput)
+                .ShouldBeTrue();
+
+            _fileSystem
+                .DirectoryExists("Templating", "sample", "MyProject")
+                .ShouldBeTrue();
+
+            _fileSystem
+                .DirectoryExists("Templating", "sample", "MyProject.Tests")
+                .ShouldBeTrue();
+
+            _fileSystem
+                .FileExists("Templating", "sample", "MyProject", "MyProject.csproj")
+                .ShouldBeTrue();
+
+            _fileSystem
+                .FileExists("Templating", "sample", "MyProject.Tests", "MyProject.Tests.csproj")
+                .ShouldBeTrue();
+
+            var solutionContents = _fileSystem.ReadStringFromFile(solutionFile);
+            var lines = solutionContents.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var guid = _command.KeywordReplacer.Replace("GUID1");
+            lines[2].ShouldEqual("Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"MyProject\", \"MyProject\\MyProject.csproj\", \"{" + guid +  "}\"");
+            lines[3].ShouldEqual("EndProject");
+
+            // and then cleanup
+            killTempDir(tmpDir);
+            _fileSystem.DeleteDirectory("Templating", "sample", "MyProject");
+            _fileSystem.DeleteDirectory("Templating", "sample", "MyProject.Tests");
+            _fileSystem.WriteStringToFile(solutionFile, oldContents);
+        }
+
+        private void killTempDir(string dir)
+        {
+            var info = new DirectoryInfo(dir);
+            info
+                .EnumerateDirectories(".git")
+                .First()
+                .EnumerateFiles("*.*", SearchOption.AllDirectories)
+                .Each(fileInfo =>
+                {
+                    fileInfo.IsReadOnly = false;
+                });
+            info.Attributes &= ~FileAttributes.ReadOnly;
+
+            _fileSystem.DeleteDirectory(dir);
+        }
+    }
+}
