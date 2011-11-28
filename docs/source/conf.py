a50d19bae756ec266bcc179af8930e3dce64cfd3
Sam Merrell <merrell.sam@gmail.com>
2011-11-28 09:04:07 -0700

    Fixing possible whitespace prob w/ conf.py
    Even if this isn't what was causing the RTD syntax problem, it was
    inconsistent with the rest of the file regarding spacing.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -182,8 +182,8 @@ htmlhelp_basename = 'FubuMVCdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'FubuMVC.tex', u'FubuMVC Documentation',
-   u'FubuMVC Contributors', 'manual'),
+    ('index', 'FubuMVC.tex', u'FubuMVC Documentation',
+     u'FubuMVC Contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
