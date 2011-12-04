d0abe21bcb833b5f528036e314aed66fd9764a66
Corey Kaylor <corey@kaylors.net>
2011-12-03 17:42:57 -0700

    Added behavior for intercepting exceptions for async actions

---
+++ src/FubuMVC.Core/Behaviors/AsyncInterceptExceptionBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FubuMVC.Core.Behaviors
+{
+    public abstract class AsyncInterceptExceptionBehavior<T> : IActionBehavior where T : Exception
+    {
+        public IActionBehavior Inner { get; set; }
+
+        public void Invoke()
+        {
+            InnerInvoke(x => x.Invoke());
+        }
+
+        public void InvokePartial()
+        {
+            InnerInvoke(x => x.InvokePartial());
+        }
+
+        private void InnerInvoke(Action<IActionBehavior> behaviorAction)
+        {
+            var task = Task.Factory.StartNew(() => behaviorAction(Inner));
+            task.ContinueWith(x =>
+            {
+                try
+                {
+                    //this will allow a catching an exception rather than inspecting task data
+                    x.Wait();
+                }
+                catch (T exception)
+                {
+                    if (!ShouldHandle(exception))
+                        throw;
+                    Handle(exception);
+                }
+            }, TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+
+        public virtual bool ShouldHandle(T exception)
+		{
+			return true;
+		}
+
+		public abstract void Handle(T exception);
+    }
+}
