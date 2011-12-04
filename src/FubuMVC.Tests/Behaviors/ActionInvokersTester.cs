8968e07f0eef757a21d6ef1022df28ef47a60c41
Corey Kaylor <corey@kaylors.net>
2011-12-04 07:09:32 -0700

    Added tests for action invoker async actions

--- src/FubuMVC.Tests/Behaviors/ActionInvokersTester.cs
+++ src/FubuMVC.Tests/Behaviors/ActionInvokersTester.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using FubuMVC.Core.Behaviors;
 using FubuMVC.Core.Runtime;
 using FubuTestingSupport;
@@ -96,5 +97,67 @@ namespace FubuMVC.Tests.Behaviors
         }
     }
 
+    [TestFixture]
+    public class execute_zero_in_one_out_async : InteractionContext<ZeroInOneOutActionInvoker<ITargetController, Task<Output>>>
+    {
+        private Task<Output> expectedOutput;
 
+        protected override void beforeEach()
+        {
+            expectedOutput = Task<Output>.Factory.StartNew(() => new Output());
+
+            Func<ITargetController, Task<Output>> action = x => x.ZeroInOneOutAsync();
+            Services.Inject(action);
+
+            MockFor<ITargetController>().Expect(x => x.ZeroInOneOutAsync()).Return(expectedOutput);
+
+            ClassUnderTest.Invoke();
+        }
+
+        [Test]
+        public void should_execute_the_controller_action()
+        {
+            VerifyCallsFor<ITargetController>();
+        }
+
+        [Test]
+        public void should_have_stored_the_resulting_task_in_the_fubu_request()
+        {
+            MockFor<IFubuRequest>().AssertWasCalled(x => x.Set(expectedOutput));
+        }
+    }
+
+    [TestFixture]
+    public class execute_one_in_one_out_no_result_async : InteractionContext<OneInOneOutActionInvoker<ITargetController, Input, Task>>
+    {
+        private Input theInput;
+        private Task expectedOutput;
+
+        protected override void beforeEach()
+        {
+            expectedOutput = Task.Factory.StartNew(() => { });
+            Func<ITargetController, Input, Task> action = (x, input) => x.OneInZeroOutAsync(input);
+            Services.Inject(action);
+
+            theInput = new Input();
+
+            MockFor<ITargetController>().Expect(x => x.OneInZeroOutAsync(Arg.Is(theInput))).Return(expectedOutput);
+            MockFor<IFubuRequest>().Expect(x => x.Get<Input>()).Return(theInput);
+
+            ClassUnderTest.Invoke();
+        }
+
+        [Test]
+        public void should_invoke_the_controller_method()
+        {
+            MockFor<ITargetController>().AssertWasCalled(x => x.OneInZeroOutAsync(theInput));
+        }
+
+
+        [Test]
+        public void should_have_stored_the_resulting_task_in_the_fubu_request()
+        {
+            MockFor<IFubuRequest>().AssertWasCalled(x => x.Set(expectedOutput));
+        }
+    }
 }
