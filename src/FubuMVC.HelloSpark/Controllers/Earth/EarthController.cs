b7d333060d180c22c9c274547252eb1d92496596
Corey Kaylor <corey@kaylors.net>
2011-12-06 07:06:11 -0700

    Removed old sample code

--- src/FubuMVC.HelloSpark/Controllers/Earth/EarthController.cs
+++ src/FubuMVC.HelloSpark/Controllers/Earth/EarthController.cs
@@ -1,9 +1,4 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using FubuMVC.Core.Continuations;
-using FubuMVC.HelloSpark.Controllers.Air;
-
-namespace FubuMVC.HelloSpark.Controllers.Earth
+﻿namespace FubuMVC.HelloSpark.Controllers.Earth
 {
     public class EarthController
     {
@@ -11,67 +6,10 @@ namespace FubuMVC.HelloSpark.Controllers.Earth
         {
             return whereAreWe;
         }
-
-        public Task<EarthViewModelAsync> RockAsync(EarthAsyncInputModel whereAreWe)
-        {
-            return Task<EarthViewModelAsync>.Factory.StartNew(() =>
-            {
-                Thread.Sleep(1000);
-                return new EarthViewModelAsync{RawUrl = whereAreWe.RawUrl};
-            }, TaskCreationOptions.AttachedToParent);
-        }
-
-        public Task RockAsyncNoResult(EarthAsyncNoResultInputModel input)
-        {
-            return Task.Factory.StartNew(() => Thread.Sleep(1000), 
-                TaskCreationOptions.AttachedToParent);
-        }
-
-        public Task<FubuContinuation> RockAsyncRedirect()
-        {
-            return Task<FubuContinuation>.Factory.StartNew(() =>
-            {
-                Thread.Sleep(1000);
-                return FubuContinuation.RedirectTo<AirRequest>();
-            }, TaskCreationOptions.AttachedToParent);
-        }
-
-        public Task<EarthAsyncPartialViewModel> RockPartialAsync(EarthAsyncPartialInputModel input)
-        {
-            return Task<EarthAsyncPartialViewModel>.Factory.StartNew(() =>
-            {
-                Thread.Sleep(1000);
-                return new EarthAsyncPartialViewModel {Message = "Slept for a while and returned partial model"};
-            }, TaskCreationOptions.AttachedToParent);
-        }
     }
 
     public class EarthViewModel
     {
         public string RawUrl { get; set; }
-    }
-
-    public class EarthViewModelAsync
-    {
-        public string RawUrl { get; set; }
-    }
-
-    public class EarthAsyncInputModel
-    {
-        public string RawUrl { get; set; }
-    }
-
-    public class EarthAsyncNoResultInputModel
-    {
-        public string RawUrl { get; set; }
-    }
-
-    public class EarthAsyncPartialViewModel
-    {
-        public string Message { get; set; }
-    }
-
-    public class EarthAsyncPartialInputModel
-    {
     }
 }
