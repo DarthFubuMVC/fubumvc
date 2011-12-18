f4748d8aa202e18b5604badf2c606481e1c9b0a6
Jeremy Miller <jeremydmiller@yahoo.com>
2011-12-18 10:52:41 -0600

    removed the now superfluous FormattedValues

--- src/FubuMVC.Core/Resources/Media/IValues.cs
+++ src/FubuMVC.Core/Resources/Media/IValues.cs
@@ -33,30 +33,6 @@ namespace FubuMVC.Core.Resources.Media
         }
     }
 
-    // TODO -- might want something besides IDisplayFormatter
-    [MarkedForTermination("Pulling inside something else instead")]
-    public class FormattedValues<T> : IValues<T>
-    {
-        private readonly IDisplayFormatter _formatter;
-        private readonly IValues<T> _inner;
-
-        public FormattedValues(IDisplayFormatter formatter, IValues<T> inner)
-        {
-            _formatter = formatter;
-            _inner = inner;
-        }
-
-        public object ValueFor(Accessor accessor)
-        {
-            var innerValue = _inner.ValueFor(accessor);
-            return _formatter.GetDisplay(accessor, innerValue);
-        }
-
-        public T Subject
-        {
-            get { return _inner.Subject; }
-        }
-    }
 
     public static class ValuesExtensions
     {
