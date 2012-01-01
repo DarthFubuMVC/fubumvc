ee4b1656f03919b6262fee2718604af1c0699eba
Corey Kaylor <corey@kaylors.net>
2012-01-01 08:37:14 -0700

    Added partial to hello page

--- src/FubuMVC.HelloRazor/Controllers/Home/HomeController.cs
+++ src/FubuMVC.HelloRazor/Controllers/Home/HomeController.cs
@@ -2,10 +2,25 @@
 {
     public class HomeController
     {
-         public HelloWorldViewModel SayHello(HelloWorldInputModel input)
-         {
-             return new HelloWorldViewModel{Message = "Hello World! FubuMVC + Razor"};
-         }
+        public HelloWorldViewModel SayHello(HelloWorldInputModel input)
+        {
+            return new HelloWorldViewModel {Message = "Hello World! FubuMVC + Razor"};
+        }
+
+        public HelloPartialViewModel SayHelloPartial(HelloPartialInputModel input)
+        {
+            return new HelloPartialViewModel {Message = "Hello from Partial"};
+        }
+        
+    }
+
+    public class HelloPartialViewModel
+    {
+        public string Message { get; set; }
+    }
+
+    public class HelloPartialInputModel
+    {
     }
 
     public class HelloWorldInputModel
