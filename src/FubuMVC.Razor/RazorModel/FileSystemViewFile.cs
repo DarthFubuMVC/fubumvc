e4764b604040446717d18630bc6375e1bba472cf
Corey Kaylor <corey@kaylors.net>
2012-01-02 13:10:29 -0700

    Added support for VB views just in case

--- src/FubuMVC.Razor/RazorModel/FileSystemViewFile.cs
+++ src/FubuMVC.Razor/RazorModel/FileSystemViewFile.cs
@@ -7,16 +7,24 @@ namespace FubuMVC.Razor.FileSystem
         private readonly string _fullPath;
         private long _lastModified;
         private string _source;
+        private string _extension;
 
         public long LastModified
         {
             get { return File.GetLastWriteTimeUtc(_fullPath).Ticks; }
         }
 
+        public string Extension
+        {
+            get { return _extension; }
+        }
+
         public FileSystemViewFile(string fullPath)
         {
             _fullPath = fullPath;
             _lastModified = LastModified;
+            var fileInfo = new FileInfo(fullPath);
+            _extension = fileInfo.Extension;
         }
 
         public Stream OpenViewStream()
@@ -42,6 +50,7 @@ namespace FubuMVC.Razor.FileSystem
 
     public interface IViewFile
     {
+        string Extension { get; }
         long LastModified { get; }
         bool IsCurrent();
         Stream OpenViewStream();
