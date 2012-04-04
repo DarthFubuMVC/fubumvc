063db813825d358d7f4a7d6bd67d3ccd117c1dd6
Jeremy Miller <jeremydmiller@yahoo.com>
2012-04-04 17:52:44 -0500

    turning auto-publishing nugets back on because we like the way it does it now

--- rakefile.rb
+++ rakefile.rb
@@ -27,7 +27,7 @@ desc "**Default**, compiles and runs tests"
 task :default => [:compile, :unit_test]
 
 desc "Target used for the CI server"
-task :ci => [:update_all_dependencies, :default, :history]
+task :ci => [:update_all_dependencies, :default, :history, :package]
 
 desc "Update the version information for the build"
 assemblyinfo :version do |asm|
