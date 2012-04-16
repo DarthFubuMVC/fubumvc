b7eff970ed5d26bc557bb4b0628a390410376671
Ryan Rauh <rauh.ryan@gmail.com>
2012-04-16 15:20:38 -0500

    added more basic asset diagnostics

--- src/FubuMVC.Core/Diagnostics/Assets/BasicAssetDiagnostics.cs
+++ src/FubuMVC.Core/Diagnostics/Assets/BasicAssetDiagnostics.cs
@@ -1,50 +1,132 @@
 using System;
 using System.IO;
+using FubuMVC.Core.Assets;
 using FubuMVC.Core.Assets.Combination;
 using System.Collections.Generic;
 using FubuMVC.Core.Assets.Content;
-using FubuMVC.Core.Assets.Files;
+using FubuMVC.Core.Assets.Diagnostics;
+using FubuMVC.Core.Diagnostics.HtmlWriting;
 using FubuMVC.Core.Runtime;
 using System.Linq;
+using FubuMVC.Core.Urls;
+using HtmlTags;
 
 namespace FubuMVC.Core.Diagnostics.Assets
 {
 
     public abstract class AssetDiagnosticRequest
     {
+        [QueryString]
         public string Name { get; set; }
     }
 
-    public class AssetNamesRequest : AssetDiagnosticRequest{}
-    public class AssetFilesRequest : AssetDiagnosticRequest{}
     public class ContentPlanRequest : AssetDiagnosticRequest{}
-    public class AssetSourcesRequest : AssetDiagnosticRequest{}
+    public class AssetLogRequest : AssetDiagnosticRequest { }
 
+    [FubuDiagnostics("Basic Asset Graph Information")]
     public class BasicAssetDiagnostics
     {
         private readonly IAssetCombinationCache _cache;
         private readonly IContentPlanCache _contentPlanCache;
-        private readonly IAssetPipeline _pipeline;
+        private readonly AssetLogsCache _logs;
+        private readonly IUrlRegistry _urlRegistry;
+        private readonly IAssetRequirements _requirements;
+        private readonly IAssetTagPlanCache _plans;
 
-        public BasicAssetDiagnostics(IAssetCombinationCache cache, IContentPlanCache contentPlanCache, IAssetPipeline pipeline)
+        public BasicAssetDiagnostics(IAssetCombinationCache cache, 
+            IContentPlanCache contentPlanCache,
+            AssetLogsCache logs,
+            IUrlRegistry urlRegistry,
+            IAssetRequirements requirements,
+            IAssetTagPlanCache plans)
         {
             _cache = cache;
             _contentPlanCache = contentPlanCache;
-            _pipeline = pipeline;
+            _logs = logs;
+            _urlRegistry = urlRegistry;
+            _requirements = requirements;
+            _plans = plans;
         }
 
+        [FubuDiagnostics("View asset logs")]
+        public HtmlDocument Assets()
+        {
+            var assetList = new HtmlTag("ul");
+
+            _logs.Entries
+                .OrderByDescending(l => l.Logs.Count)
+                .ThenBy(l => l.Name)
+                .Each(log => assetList.Append(new HtmlTag("li")
+                                                   .Append("a", tag => tag.Attr("href", _urlRegistry.UrlFor(new AssetLogRequest { Name = log.Name }))
+                                                       .Text(log.Name))));
+            return DiagnosticHtml.BuildDocument(_urlRegistry, "Asset Logs", assetList);
+        }
+
+        [FubuDiagnostics("log for asset", ShownInIndex = false)]
+        public HtmlDocument AssetLogs(AssetLogRequest request)
+        {
+            var logs = _logs.FindByName(request.Name);
+
+            var assetList = new HtmlTag("ul");
+
+
+            _requirements.Require(request.Name);
+            var keys = _requirements.DequeueAssetsToRender();
+            var plan = keys.Select(k => _plans.PlanFor(k));
+
+            var dependencies = new HtmlTag("ul");
+
+            plan.Each(p =>
+            {
+                p.Subjects.Each(s => dependencies.Append(new HtmlTag("li").Append(new LinkTag(s.Name, _urlRegistry.UrlFor(new AssetLogRequest() { Name = s.Name })))));
+            });
+            assetList.Add("li").Text("Order written").Append(dependencies);
+
+            logs.Logs
+                .Each(log => assetList.Append(new HtmlTag("li")
+                                                       .Text(log.Message)).Append("ul/li", tag => tag.Text(log.Provenance)));
+
+            return DiagnosticHtml.BuildDocument(_urlRegistry, "Logs for " + request.Name, assetList);
+
+        }
+
+        [FubuDiagnostics("View content plans")]
+        public HtmlDocument ContentPlans()
+        {
+            var assetList = new HtmlTag("ul");
+
+            _logs.Entries
+                .OrderByDescending(l => l.Logs.Count)
+                .ThenBy(l => l.Name)
+                .Each(log => assetList.Append(new HtmlTag("li")
+                                                   .Append("a", tag => tag.Attr("href", _urlRegistry.UrlFor(new ContentPlanRequest { Name = log.Name }))
+                                                       .Text(log.Name))));
+            return DiagnosticHtml.BuildDocument(_urlRegistry, "Content Plans", assetList);
+        }
+
+        [FubuDiagnostics("content plan for {asset}", ShownInIndex = false)]
         public string get_asset_Name(ContentPlanRequest request)
         {
-            var plan = _contentPlanCache.PlanFor(request.Name);
-            var description = new ContentPlanPreviewer();
-            plan.AcceptVisitor(description);
+            try
+            {
+                var plan = _contentPlanCache.PlanFor(request.Name);
+                var description = new ContentPlanPreviewer();
+                plan.AcceptVisitor(description);
 
-            var writer = new StringWriter();
-            description.Descriptions.Each(x => writer.WriteLine(x));
+                var writer = new StringWriter();
+                description.Descriptions.Each(x => writer.WriteLine(x));
 
-            return writer.ToString();
+                return writer.ToString();
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                return e.Message;
+            }
+          
+ 
         }
 
+        [FubuDiagnostics("View combinations")]
         public string get_assets_combinations()
         {
             var writer = new StringWriter();
@@ -64,43 +146,6 @@ namespace FubuMVC.Core.Diagnostics.Assets
 
             return writer.ToString();
         }
-
-        private string toCombinationReport(AssetDiagnosticRequest request, Func<AssetFile, string> description)
-        {
-            var plan = _contentPlanCache.PlanFor(request.Name);
-
-            if (plan == null)
-            {
-                return "Not found";
-            }
-
-            var writer = new StringWriter();
-
-
-            plan.GetAllSources().SelectMany(x => x.Files).Each(x => writer.WriteLine(description(x)));
-
-            return writer.ToString();
-        }
-
-        public string get_asset_Name_contents(AssetNamesRequest request) 
-        {
-            return toCombinationReport(request, file => file.Name);
-        }
-
-        public string get_asset_Name_files(AssetFilesRequest request)
-        {
-            return toCombinationReport(request, file => file.FullPath);
-        }
-
-        public string get_asset_Name_sources(AssetSourcesRequest request)
-        {
-            return toCombinationReport(request, file =>
-            {
-                var path = _pipeline.AssetPathOf(file);
-                return path.ToFullName();
-            });
-        }
-
 
     }
 }
