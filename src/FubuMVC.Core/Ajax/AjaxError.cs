008b3fbe1b74765e1292a99064d2ebdc1b74eb01
Josh Arnold <rncodepoet@gmail.com>
2012-05-29 09:15:07 -0500

    Added a label property to AjaxError for localization purposes

--- src/FubuMVC.Core/Ajax/AjaxError.cs
+++ src/FubuMVC.Core/Ajax/AjaxError.cs
@@ -7,6 +7,8 @@ namespace FubuMVC.Core.Ajax
 
         // Use this to attach the server side validation errors 
         public string field { get; set; }
+		// Use this to display the accessor (via localization)
+		public string label { get; set; }
         public string message { get; set; }
     }
 }
