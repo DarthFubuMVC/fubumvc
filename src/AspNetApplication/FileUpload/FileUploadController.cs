a47368005746a1b75225f4a0d89b0b34bc5e6ee6
Chad Myers <chad.myers@gmail.com>
2012-06-08 18:22:00 -0500

    Add ASP.NET file upload end-to-end tests #328

---
+++ src/AspNetApplication/FileUpload/FileUploadController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web;
+using FubuMVC.WebForms;
+
+namespace AspNetApplication.FileUpload
+{
+    public class FileUploadTestInput{ }
+
+    public class FileUploadInput
+    {
+        public HttpPostedFileBase File1 { get; set; }
+    }
+
+    public class FileUploadOutput
+    {
+        public string Text { get; set; }
+    }
+
+    public class FileUploadView : FubuPage<FileUploadOutput>{}
+
+    public class FileUploadController
+    {
+        public FileUploadOutput get_file_upload_test(FileUploadTestInput input)
+        {
+            return new FileUploadOutput
+            {
+                Text = "Please upload a file"
+            };
+        }
+
+        public FileUploadOutput post_file_upload_test(FileUploadInput input)
+        {
+            var text = (input.File1 == null) ? "File upload failure!" : "File upload success! " + input.File1.FileName;
+
+            return new FileUploadOutput
+            {
+                Text = text
+            };
+        }
+    
+    }
+}
---
+++ src/FubuMVC.AspNetTesting/can_upload_a_file.cs
@@ -0,0 +1,16 @@
+﻿using AspNetApplication.FileUpload;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuMVC.AspNetTesting
+{
+    [TestFixture]
+    public class can_upload_a_file
+    {
+        [Test]
+        public void upload_a_file_using_input_type_file_with_form_multipart()
+        {
+            TestApplication.Endpoints.PostFile(new FileUploadInput(), "File1").ReadAsText().ShouldContain("success");
+        }
+    }
+}
