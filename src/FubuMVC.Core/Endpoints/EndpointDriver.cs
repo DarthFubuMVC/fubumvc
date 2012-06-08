be599b286b8a7673cda82b0086584dc730a45708
Chad Myers <chad.myers@gmail.com>
2012-06-08 18:20:54 -0500

    Add PostFile to EndpointDriver Add debugging output ot EnpointDriver Add User Agent to Get requests from EndpointDriver
    #328

--- src/FubuMVC.Core/Endpoints/EndpointDriver.cs
+++ src/FubuMVC.Core/Endpoints/EndpointDriver.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq.Expressions;
 using System.Net;
@@ -70,6 +71,44 @@ namespace FubuMVC.Core.Endpoints
             });
         }
 
+        public HttpResponse PostFile<T>(T target, string fileInputFormName, string accept="*/*")
+        {
+            var boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            var boundarybytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            var wr = (HttpWebRequest) requestForUrlTarget(target, "POST");
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Accept = "*/*";
+            wr.Method = "POST";
+            wr.KeepAlive = true;
+            wr.Credentials = CredentialCache.DefaultCredentials;
+
+            using (var rs = wr.GetRequestStream())
+            {
+                const string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+                var formitem = string.Format(formdataTemplate, "foo", "bar");
+                var formitembytes = Encoding.UTF8.GetBytes(formitem);
+                rs.Write(formitembytes, 0, formitembytes.Length);
+
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+                const string headerTemplate =
+                    "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+                var header = string.Format(headerTemplate, fileInputFormName, "test.pdf", "application/pdf");
+                var headerbytes = Encoding.UTF8.GetBytes(header);
+                rs.Write(headerbytes, 0, headerbytes.Length);
+
+                var fileBytes = Encoding.UTF8.GetBytes("test.pdf contents");
+                rs.Write(fileBytes, 0, fileBytes.Length);
+
+                var trailer = Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+                rs.Write(trailer, 0, trailer.Length);
+            }
+
+            return wr.ToHttpCall();
+        }
+
         public HttpResponse PostJson<T>(T target, string contentType = "text/json", string accept = "*/*")
         {
             return post(target, contentType, accept, stream =>
@@ -130,9 +169,12 @@ namespace FubuMVC.Core.Endpoints
         /// <returns></returns>
         public HttpResponse Get(string url, string acceptType = "*/*", string etag = null)
         {
-            var request = WebRequest.Create(url);
+            Debug.WriteLine("EndpointDriver getting url {0}".ToFormat(url));
+
+            var request = (HttpWebRequest) WebRequest.Create(url);
             request.Method = "GET";
             request.ContentType = MimeType.HttpFormMimetype;
+            request.UserAgent = "EndpointDriver User Agent 1.0";
             request.As<HttpWebRequest>().Accept = acceptType;
 
             if (etag.IsNotEmpty())
