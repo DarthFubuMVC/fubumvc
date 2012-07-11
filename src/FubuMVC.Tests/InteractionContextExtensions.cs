38aeb6560c25dbd7d865d0d8b451605b3b5f65fa
Jeremy Miller <jeremydmiller@yahoo.com>
2012-07-11 08:41:42 -0500

    checkpoint:  added some extensions to InteractionContext for testing logging

---
+++ src/FubuMVC.Tests/InteractionContextExtensions.cs
@@ -0,0 +1,20 @@
+using FubuMVC.Core.Runtime.Logging;
+using FubuTestingSupport;
+using StructureMap.AutoMocking;
+using FubuCore;
+
+namespace FubuMVC.Tests
+{
+    public static class InteractionContextExtensions
+    {
+        public static void RecordLogging<T>(this RhinoAutoMocker<T> mocker) where T : class
+        {
+            mocker.Inject<ILogger>(new RecordingLogger());
+        }
+
+        public static RecordingLogger RecordedLog<T>(RhinoAutoMocker<T> mocker) where T : class
+        {
+            return mocker.Get<ILogger>().As<RecordingLogger>();
+        }
+    }
+}
