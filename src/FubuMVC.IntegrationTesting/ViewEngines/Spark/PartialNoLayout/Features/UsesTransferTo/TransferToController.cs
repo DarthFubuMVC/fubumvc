c3086db1899f7cc5ec9fd9817a3af5bea3848999
Ryan Rauh <rauh.ryan@gmail.com>
2012-07-23 11:06:49 -0500

    Spark integration test for a Chain that uses TransferTo

---
+++ src/FubuMVC.IntegrationTesting/ViewEngines/Spark/PartialNoLayout/Features/UsesTransferTo/TransferToController.cs
@@ -0,0 +1,13 @@
+﻿using FubuMVC.Core.Continuations;
+using FubuMVC.IntegrationTesting.ViewEngines.Spark.PartialNoLayout.Features.HelloPartial;
+
+namespace FubuMVC.IntegrationTesting.ViewEngines.Spark.PartialNoLayout.Features.UsesTransferTo
+{
+    public class TransferToController
+    {
+         public FubuContinuation Tranfer()
+         {
+             return FubuContinuation.TransferTo(new HelloPartialInputModel());
+         }
+    }
+}
