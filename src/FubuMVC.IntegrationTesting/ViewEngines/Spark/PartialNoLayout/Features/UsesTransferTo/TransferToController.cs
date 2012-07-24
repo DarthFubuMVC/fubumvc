96e86657c1b52f7c1ce3b40901964db601af1951
Ryan Rauh <rauh.ryan@gmail.com>
2012-07-24 10:43:12 -0500

    Integration test for spark redirect to normal behavior

--- src/FubuMVC.IntegrationTesting/ViewEngines/Spark/PartialNoLayout/Features/UsesTransferTo/TransferToController.cs
+++ src/FubuMVC.IntegrationTesting/ViewEngines/Spark/PartialNoLayout/Features/UsesTransferTo/TransferToController.cs
@@ -1,5 +1,6 @@
 ﻿using FubuMVC.Core.Continuations;
 using FubuMVC.IntegrationTesting.ViewEngines.Spark.PartialNoLayout.Features.HelloPartial;
+using FubuMVC.IntegrationTesting.ViewEngines.Spark.PartialNoLayout.Features.UsesPartial;
 
 namespace FubuMVC.IntegrationTesting.ViewEngines.Spark.PartialNoLayout.Features.UsesTransferTo
 {
@@ -8,6 +9,13 @@ namespace FubuMVC.IntegrationTesting.ViewEngines.Spark.PartialNoLayout.Features.
          public FubuContinuation Tranfer()
          {
              return FubuContinuation.TransferTo(new HelloPartialInputModel());
+         }
+    }
+    public class RedirectToController
+    {
+         public FubuContinuation Redirect()
+         {
+             return FubuContinuation.RedirectTo<UsesPartialController>(x => x.Execute());
          }
     }
 }
