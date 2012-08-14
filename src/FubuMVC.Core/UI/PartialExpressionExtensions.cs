70b30ed7be8821c61becca36b67575ed7b41ff6b
Matt S. <ventaur@gmail.com>
2012-08-14 17:08:29 -0400

    Updated Partial extensions to return IHtmlString.
    Razor calls to Partial() in particular needed to be wrapped in Raw()
    before this update.

--- src/FubuMVC.Core/UI/PartialExpressionExtensions.cs
+++ src/FubuMVC.Core/UI/PartialExpressionExtensions.cs
@@ -1,3 +1,5 @@
+using System.Web;
+
 using FubuMVC.Core.Runtime;
 using FubuMVC.Core.View;
 
@@ -5,25 +7,25 @@ namespace FubuMVC.Core.UI
 {
     public static class PartialExpressionExtensions
     {
-        public static string Partial<TInputModel>(this IFubuPage page) where TInputModel : class
+        public static IHtmlString Partial<TInputModel>(this IFubuPage page) where TInputModel : class
         {
-            return InvokePartial<TInputModel>(page, null);
+            return new HtmlString(InvokePartial<TInputModel>(page, null));
         }
 
-        public static string PartialFor(this IFubuPage page, object input, bool withModelBinding = false)
+        public static IHtmlString PartialFor(this IFubuPage page, object input, bool withModelBinding = false)
         {
-            return page.Get<IPartialInvoker>().InvokeObject(input, withModelBinding);
+            return new HtmlString(page.Get<IPartialInvoker>().InvokeObject(input, withModelBinding));
         }
 
-        public static string Partial<TInputModel>(this IFubuPage page, TInputModel model, bool withModelBinding = false) where TInputModel : class
+        public static IHtmlString Partial<TInputModel>(this IFubuPage page, TInputModel model, bool withModelBinding = false) where TInputModel : class
         {
             if (typeof(TInputModel) == typeof(object))
             {
-                return page.Get<IPartialInvoker>().InvokeObject(model, withModelBinding);
+                return new HtmlString(page.Get<IPartialInvoker>().InvokeObject(model, withModelBinding));
             }
 
             page.Get<IFubuRequest>().Set(model);
-            return InvokePartial<TInputModel>(page, null);
+            return new HtmlString(InvokePartial<TInputModel>(page, null));
         }
 
         public static string InvokePartial<TInputModel>(IFubuPage page, string prefix) where TInputModel : class
