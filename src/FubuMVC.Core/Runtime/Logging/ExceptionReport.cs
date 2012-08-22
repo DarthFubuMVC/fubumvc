dd1fd967d6ec0d179ae5db7596203343109685b1
Jeremy Miller <jeremydmiller@yahoo.com>
2012-08-22 10:35:56 -0500

    gave ExceptionReport an Equals method

--- src/FubuMVC.Core/Runtime/Logging/ExceptionReport.cs
+++ src/FubuMVC.Core/Runtime/Logging/ExceptionReport.cs
@@ -18,5 +18,36 @@ namespace FubuMVC.Core.Runtime.Logging
         public string Message { get; set; }
         public string ExceptionText { get; set; }
         public object CorrelationId { get; set; }
+
+        public bool Equals(ExceptionReport other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.Message, Message) && Equals(other.ExceptionText, ExceptionText) && Equals(other.CorrelationId, CorrelationId);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (ExceptionReport)) return false;
+            return Equals((ExceptionReport) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int result = (Message != null ? Message.GetHashCode() : 0);
+                result = (result*397) ^ (ExceptionText != null ? ExceptionText.GetHashCode() : 0);
+                result = (result*397) ^ (CorrelationId != null ? CorrelationId.GetHashCode() : 0);
+                return result;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Message: {0}, ExceptionText: {1}, CorrelationId: {2}", Message, ExceptionText, CorrelationId);
+        }
     }
 }
