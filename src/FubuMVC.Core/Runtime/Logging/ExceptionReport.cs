5536eb08fb69864bb83ab69bd271cc96a30b19e3
Jeremy Miller <jeremydmiller@yahoo.com>
2012-08-27 11:54:07 -0500

    visualization improvements for SetValueReport

--- src/FubuMVC.Core/Runtime/Logging/ExceptionReport.cs
+++ src/FubuMVC.Core/Runtime/Logging/ExceptionReport.cs
@@ -8,12 +8,19 @@ namespace FubuMVC.Core.Runtime.Logging
         public ExceptionReport(string message, Exception exception)
         {
             Message = message;
+            ExceptionType = exception.GetType().Name;
             ExceptionText = exception.ToString();
+        }
+
+        public ExceptionReport(Exception exception) : this(exception.Message, exception)
+        {
         }
 
         public ExceptionReport()
         {
         }
+
+        public string ExceptionType { get; set; }
 
         public string Message { get; set; }
         public string ExceptionText { get; set; }
@@ -23,7 +30,8 @@ namespace FubuMVC.Core.Runtime.Logging
         {
             if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
-            return Equals(other.Message, Message) && Equals(other.ExceptionText, ExceptionText) && Equals(other.CorrelationId, CorrelationId);
+            return Equals(other.Message, Message) && Equals(other.ExceptionText, ExceptionText) &&
+                   Equals(other.CorrelationId, CorrelationId);
         }
 
         public override bool Equals(object obj)
@@ -38,7 +46,7 @@ namespace FubuMVC.Core.Runtime.Logging
         {
             unchecked
             {
-                int result = (Message != null ? Message.GetHashCode() : 0);
+                var result = (Message != null ? Message.GetHashCode() : 0);
                 result = (result*397) ^ (ExceptionText != null ? ExceptionText.GetHashCode() : 0);
                 result = (result*397) ^ (CorrelationId != null ? CorrelationId.GetHashCode() : 0);
                 return result;
@@ -47,7 +55,8 @@ namespace FubuMVC.Core.Runtime.Logging
 
         public override string ToString()
         {
-            return string.Format("Message: {0}, ExceptionText: {1}, CorrelationId: {2}", Message, ExceptionText, CorrelationId);
+            return string.Format("Message: {0}, ExceptionText: {1}, CorrelationId: {2}", Message, ExceptionText,
+                                 CorrelationId);
         }
     }
 }
