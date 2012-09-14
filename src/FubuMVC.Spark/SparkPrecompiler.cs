b4a93364002cc4f7d1c734eb77480b0268d01d75
Alex Henning Johannessen <ahjohannessen@gmail.com>
2012-09-14 22:50:07 +0100

    Added Spark Precompiler Activator.

---
+++ src/FubuMVC.Spark/SparkPrecompiler.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bottles;
+using Bottles.Diagnostics;
+using FubuMVC.Core;
+using FubuMVC.Spark.Registration;
+using FubuMVC.Spark.Rendering;
+using FubuMVC.Spark.SparkModel;
+
+namespace FubuMVC.Spark
+{
+    public class SparkPrecompiler : IActivator
+    {
+        private readonly ISparkTemplateRegistry _templates;
+        private readonly IViewEntryProviderCache _providerCache;
+
+        public SparkPrecompiler(ISparkTemplateRegistry templates, IViewEntryProviderCache providerCache)
+        {
+            _templates = templates;
+            _providerCache = providerCache;
+        }
+
+        public void Activate(IEnumerable<IPackageInfo> packages, IPackageLog log)
+        {
+            if(FubuMode.InDevelopment()) return;
+            Task.Factory.StartNew(() => Precompile(log));
+        }
+
+        public void Precompile(IPackageLog log)
+        {
+            _templates.Where(t => t.Descriptor is SparkDescriptor).Each(view =>
+            {
+                var sparkDescriptor = (SparkDescriptor) view.Descriptor;
+                var viewDef = sparkDescriptor.ToViewDefinition();
+
+                log.TrapErrors(() =>
+                {
+                    _providerCache.GetViewEntry(viewDef.ViewDescriptor);
+                    _providerCache.GetViewEntry(viewDef.PartialDescriptor);
+                });
+            });
+        }
+    }
+}
