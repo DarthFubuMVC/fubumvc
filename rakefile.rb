400ad1f7bcdcceae41d3f6108cfaec3b533326fc
Jeremy Miller <jeremydmiller@yahoo.com>
2012-09-24 11:03:12 -0500

    separated out the unit tests for fubu.exe

--- rakefile.rb
+++ rakefile.rb
@@ -105,7 +105,7 @@ task :test => [:unit_test]
 desc "Runs unit tests"
 task :unit_test => :compile do
   runner = NUnitRunner.new :compilemode => COMPILE_TARGET, :source => 'src', :platform => 'x86'
-  runner.executeTests ['FubuMVC.Tests', 'FubuMVC.Spark.Tests', 'FubuMVC.Razor.Tests', 'FubuMVC.OwinHost.Testing']
+  runner.executeTests ['FubuMVC.Tests', 'FubuMVC.Spark.Tests', 'FubuMVC.Razor.Tests', 'FubuMVC.OwinHost.Testing', 'fubu.Testing']
 end
 
 desc "Runs the integration tests"
