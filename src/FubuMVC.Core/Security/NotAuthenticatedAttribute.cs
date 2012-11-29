3e507f6765c25aa0696eb8d11b810c7d3b66b8f6
jeremydmiller <jeremydmiller@yahoo.com>
2012-11-29 08:59:12 -0600

    moved the NotAuthenticated attribute to fubumvc

---
+++ src/FubuMVC.Core/Security/NotAuthenticatedAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FubuMVC.Core.Security
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = false)]
+    public class NotAuthenticatedAttribute : Attribute
+    {
+        
+    }
+}
