230079605111689a6ad6a2391f86449d14cdf749
Matt S <ventaur@gmail.com>
2012-12-09 12:53:52 -0500

    Initial setup of Autofac projects with some tweaks to get it compiling.

---
+++ src/FubuMVC.Autofac/AutofacContainerFacility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Autofac;
+
+using FubuCore.Binding;
+
+using FubuMVC.Core.Behaviors;
+using FubuMVC.Core.Bootstrapping;
+using FubuMVC.Core.Registration.ObjectGraph;
+using FubuMVC.Core.Runtime;
+
+
+namespace FubuMVC.Autofac {
+	public class AutofacContainerFacility : IContainerFacility, IServiceFactory {
+		private readonly IContainer _container;
+
+
+		public AutofacContainerFacility(IContainer container) {
+			_container = container;
+		}
+
+
+		public IServiceFactory BuildFactory() {
+			throw new NotImplementedException();
+		}
+
+		public void Register(Type serviceType, ObjectDef def) {
+			throw new NotImplementedException();
+		}
+
+		public void Inject(Type abstraction, Type concretion) {
+			throw new NotImplementedException();
+		}
+
+
+		public IActionBehavior BuildBehavior(ServiceArguments arguments, Guid behaviorId) {
+			throw new NotImplementedException();
+		}
+
+		public T Get<T>() {
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<T> GetAll<T>() {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/FubuMVC.Autofac/NestedAutofacContainerBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Autofac;
+using Autofac.Core;
+using Autofac.Core.Registration;
+
+using FubuCore.Binding;
+
+using FubuMVC.Core.Behaviors;
+
+
+namespace FubuMVC.Autofac {
+	public class NestedAutofacContainerBehavior : IActionBehavior, IDisposable {
+		private readonly IContainer _container;
+		private readonly ServiceArguments _arguments;
+		private readonly Guid _behaviorId;
+
+		private ILifetimeScope _nestedScope;
+
+
+		public NestedAutofacContainerBehavior(IContainer container, ServiceArguments arguments, Guid behaviorId) {
+			_container = container;
+			_arguments = arguments;
+			_behaviorId = behaviorId;
+		}
+
+
+		public void Invoke() {
+			IActionBehavior behavior = StartInnerBehavior();
+			behavior.Invoke();
+		}
+
+		public IActionBehavior StartInnerBehavior() {
+			_nestedScope = _container.BeginLifetimeScope();
+			// TODO: Configure the services.
+			
+
+			var behavior = _nestedScope.Resolve<IActionBehavior>(new PositionalParameter(0, _behaviorId.ToString()));
+			return behavior;
+		}
+
+		public void InvokePartial() {
+			throw new NotSupportedException();
+		}
+
+		public void Dispose() {
+			_nestedScope.Dispose();
+		}
+	}
+}
