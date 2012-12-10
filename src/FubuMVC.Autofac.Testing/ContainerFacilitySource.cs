5f3b9b1e29be7868e0395a20a3754730abe20a41
Matt S <ventaur@gmail.com>
2012-12-10 16:26:11 -0500

    Tweaked comments and format for some testing classes.

--- src/FubuMVC.Autofac.Testing/ContainerFacilitySource.cs
+++ src/FubuMVC.Autofac.Testing/ContainerFacilitySource.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-using Autofac.Core;
+using Autofac;
 
 using FubuCore;
 using FubuCore.Binding;
@@ -11,49 +11,41 @@ using FubuMVC.Core.Registration.ObjectGraph;
 using FubuMVC.Core.Runtime;
 
 
-namespace FubuMVC.Autofac.Testing
-{
-	public static class ContainerFacilitySource
-	{
-		 public static IServiceFactory New(Action<IContainerFacility> configure) {
-			 var facility = new AutofacContainerFacility(Container.Empty);
-			 configure(facility);
+namespace FubuMVC.Autofac.Testing {
+	public static class ContainerFacilitySource {
+		public static IServiceFactory New(Action<IContainerFacility> configure) {
+			var builder = new ContainerBuilder();
+			var facility = new AutofacContainerFacility(builder.Build());
+			configure(facility);
 
-			 // A ContainerFacility cannot be considered "ready" for business until BuildFactory() has been
-			 // called
-			 return facility.BuildFactory();
-		 }
+			// A ContainerFacility cannot be considered "ready" for business until BuildFactory() has been called.
+			return facility.BuildFactory();
+		}
 
-		 public static IServiceLocator Services(Action<IContainerFacility> configure)
-		 {
-			 var facility = new AutofacContainerFacility(Container.Empty);
-			 configure(facility);
+		public static IServiceLocator Services(Action<IContainerFacility> configure) {
+			var builder = new ContainerBuilder();
+			var facility = new AutofacContainerFacility(builder.Build());
+			configure(facility);
 
-			 // A ContainerFacility cannot be considered "ready" for business until BuildFactory() has been
-			 // called
-			 return facility.BuildFactory().Get<IServiceLocator>();
-		 }
+			// A ContainerFacility cannot be considered "ready" for business until BuildFactory() has been called.
+			return facility.BuildFactory().Get<IServiceLocator>();
+		}
 
-		 public static IActionBehavior BuildBehavior(ServiceArguments arguments,
-													 ObjectDef behaviorDef, Action<IContainerFacility> configuration)
-		 {
-			 var id = Guid.NewGuid();
-			 behaviorDef.Name = id.ToString();
+		public static IActionBehavior BuildBehavior(ServiceArguments arguments, ObjectDef behaviorDef, Action<IContainerFacility> configuration) {
+			var id = Guid.NewGuid();
+			behaviorDef.Name = id.ToString();
 
-			 var facility = New(x => {
-				 configuration(x);
+			var facility = New(x => {
+				configuration(x);
 
+				x.Register(typeof(IActionBehavior), behaviorDef);
+			});
 
-				 x.Register(typeof (IActionBehavior), behaviorDef);
-			 });
+			var behavior = facility.BuildBehavior(arguments, id);
 
-			 var behavior = facility.BuildBehavior(arguments, id);
-
-			 // StartInnerBehavior() is not part of the core interface,
-			 // but I had to have something to get at the real top level
-			 // behavior within the context of a StructureMap nested
-			 // container
-			 return behavior.As<NestedAutofacContainerBehavior>().StartInnerBehavior();
-		 }
+			// StartInnerBehavior() is not part of the core interface, but I had to have something to get at the real top level
+			// behavior within the context of an Autofac nested container.
+			return behavior.As<NestedAutofacContainerBehavior>().StartInnerBehavior();
+		}
 	}
 }
