0589e78246e841e412b7305cd59cc69605b79ee3
Neil Sorensen <nsorensen@ancestry.com>
2012-12-20 14:55:52 -0700

    Updating the default fubu template to work with the latest version of FubuMVC

--- fubuTemplates/default/src/FUBUPROJECTNAME/ConfigureFubuMVC.cs
+++ fubuTemplates/default/src/FUBUPROJECTNAME/ConfigureFubuMVC.cs
@@ -6,9 +6,6 @@ namespace FUBUPROJECTNAME
     {
         public ConfigureFubuMVC()
         {
-            // This line turns on the basic diagnostics and request tracing
-            IncludeDiagnostics(true);
-
             // All public methods from concrete classes ending in "Controller"
             // in this assembly are assumed to be action methods
             Actions.IncludeClassesSuffixedWithController();
@@ -18,10 +15,6 @@ namespace FUBUPROJECTNAME
                 .IgnoreControllerNamesEntirely()
                 .IgnoreMethodSuffix("Html")
                 .RootAtAssemblyNamespace();
-
-            // Match views to action methods by matching
-            // on model type, view name, and namespace
-            Views.TryToAttachWithDefaultConventions();
         }
     }
 }
