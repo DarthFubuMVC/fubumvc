71d7741e935c9000c6dfef271fe56437b98ec1e3
jeremydmiller <jeremydmiller@yahoo.com>
2012-12-30 21:36:33 -0600

    added the FilterAttribute.  Closes GH-457

---
+++ src/FubuMVC.Core/Registration/FilterAttribute.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Reflection;
+using System.Runtime.Serialization;
+using FubuMVC.Core.Registration.Nodes;
+using FubuCore;
+using System.Linq;
+
+namespace FubuMVC.Core.Registration
+{
+    /// <summary>
+    /// Can be used to add one-off ActionFilter's to a single method or action class.
+    /// Can only be used with classes that have exactly one public method
+    /// </summary>
+    public class FilterAttribute : ModifyChainAttribute
+    {
+        private readonly Type _filterType;
+        private readonly MethodInfo _method;
+
+        public FilterAttribute(Type filterType)
+        {
+            _filterType = filterType;
+            _method = DetermineMethod(filterType);
+        }
+
+        public override void Alter(ActionCall call)
+        {
+            var filter = new ActionFilter(_filterType, _method);
+            call.AddBefore(filter);
+        }
+
+        public static MethodInfo DetermineMethod(Type type)
+        {
+            var methods = type.GetMethods().Where(m => m.DeclaringType != typeof (object)).ToList();
+            if (methods.Count() != 1)
+            {
+                throw new InvalidActionFilterTypeException(type);
+            }
+
+            return methods.Single();
+        }
+    }
+
+    [Serializable]
+    public class InvalidActionFilterTypeException : Exception
+    {
+        public InvalidActionFilterTypeException(Type filterType) : base("Type {0} cannot be used.  Only types with one single public method can be used with the [Filter(type)] attribute".ToFormat(filterType.FullName))
+        {
+        }
+
+        protected InvalidActionFilterTypeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/FubuMVC.Tests/Registration/FilterAttributeTester.cs
@@ -0,0 +1,106 @@
+﻿using FubuMVC.Core.Continuations;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.Nodes;
+using FubuTestingSupport;
+using NUnit.Framework;
+using System.Linq;
+
+namespace FubuMVC.Tests.Registration
+{
+    [TestFixture]
+    public class FilterAttributeTester
+    {
+        [Test]
+        public void one_method_is_valid()
+        {
+            FilterAttribute.DetermineMethod(typeof (ValidFilter))
+                           .Name.ShouldEqual("Filter");
+        }
+
+        [Test]
+        public void no_public_methods_are_invalid()
+        {
+            Exception<InvalidActionFilterTypeException>.ShouldBeThrownBy(() => {
+                FilterAttribute.DetermineMethod(typeof (InvalidFilter2));
+            });
+        }
+
+        [Test]
+        public void multiple_public_methods_are_invalid()
+        {
+            Exception<InvalidActionFilterTypeException>.ShouldBeThrownBy(() =>
+            {
+                FilterAttribute.DetermineMethod(typeof(InvalidFilter));
+            });
+        }
+
+        [Test]
+        public void private_methods_are_invalid()
+        {
+            Exception<InvalidActionFilterTypeException>.ShouldBeThrownBy(() =>
+            {
+                FilterAttribute.DetermineMethod(typeof(InvalidFilter3));
+            });
+        }
+
+        [Test]
+        public void adds_the_filter_before_the_action_call()
+        {
+            var graph = BehaviorGraph.BuildFrom(x => {
+                x.Actions.IncludeType<FilterTargetEndpoint>();
+            });
+
+            var chain = graph.BehaviorFor<FilterTargetEndpoint>(x => x.get_hello());
+
+            var filter = chain.OfType<ActionFilter>().Single();
+            filter.HandlerType.ShouldEqual(typeof (ValidFilter));
+            filter.Method.Name.ShouldEqual("Filter");
+
+            filter.Next.ShouldBeOfType<ContinuationNode>()
+                .Next.ShouldBeOfType<ActionCall>();
+        }
+    }
+
+    public class FilterTargetEndpoint
+    {
+        [Filter(typeof(ValidFilter))]
+        public string get_hello()
+        {
+            return "Hello";
+        }
+    }
+
+    public class ValidFilter
+    {
+        public FubuContinuation Filter()
+        {
+            return FubuContinuation.NextBehavior();
+        }
+    }
+
+    public class InvalidFilter
+    {
+        public FubuContinuation Filter()
+        {
+            return FubuContinuation.NextBehavior();
+        }
+
+        public FubuContinuation Filter2()
+        {
+            return FubuContinuation.NextBehavior();
+        }
+    }
+
+    public class InvalidFilter2
+    {
+        
+    }
+
+    public class InvalidFilter3
+    {
+        private FubuContinuation Filter()
+        {
+            return FubuContinuation.NextBehavior();
+        }
+    }
+}
