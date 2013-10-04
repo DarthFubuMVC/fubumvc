bf8004bd8b4002a19f0b786a9a7bd4e1f34a9312
Sam Merrell <merrell.sam@gmail.com>
2013-10-04 15:01:46 -0600

    Setup initial docs structure
    * Complete overview

---
+++ src/FubuMVC.Tests/Docs/Introduction/Overview/HomeController.cs
@@ -0,0 +1,20 @@
+﻿namespace FubuMVC.Tests.Docs.Introduction.Overview
+{
+    // SAMPLE: overview-homecontroller
+    public class HomeController
+    {
+        public HomeViewModel Home(HomeInputModel inputModel)
+        {
+            return new HomeViewModel();
+        }
+    }
+
+    public class HomeInputModel
+    {
+    }
+
+    public class HomeViewModel
+    {
+    }
+    // ENDSAMPLE
+}
