1a0e3213c370b70f1a91c49457f613154a550d76
Joe McBride <joe@uicraftsman.com>
2013-10-24 12:44:56 -0600

    Fixes a stack overflow in the StandInCurrentHttpRequest.

---
+++ src/FubuMVC.Tests/Http/StandInCurrentHttpRequestTester.cs
@@ -0,0 +1,42 @@
+﻿using FubuMVC.Core.Http;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuMVC.Tests.Http
+{
+    [TestFixture]
+    public class StandInCurrentHttpRequestTester
+    {
+        private StandInCurrentHttpRequest theRequest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            theRequest = new StandInCurrentHttpRequest();
+        }
+
+        [Test]
+        public void fullurl_returns_stubfullurl_field()
+        {
+            theRequest.FullUrl().ShouldEqual(theRequest.StubFullUrl);
+        }
+
+        [Test]
+        public void rawurl_returns_therawurl_field()
+        {
+            theRequest.RawUrl().ShouldEqual(theRequest.TheRawUrl);
+        }
+
+        [Test]
+        public void relativeurl_returns_therelativeurl_field()
+        {
+            theRequest.RelativeUrl().ShouldEqual(theRequest.TheRelativeUrl);
+        }
+
+        [Test]
+        public void httpmethod_returns_thehttpmethod_field()
+        {
+            theRequest.HttpMethod().ShouldEqual(theRequest.TheHttpMethod);
+        }
+    }
+}
