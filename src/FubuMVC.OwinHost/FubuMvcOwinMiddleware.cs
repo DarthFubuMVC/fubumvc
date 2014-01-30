b77868fd78191c47bc8d385c42ff56520e667751
jeremydmiller <jeremydmiller@yahoo.com>
2014-01-30 10:19:44 -0600

    added the new base class for FubuMVC centric OWIN middleware.  Closes GH-585

---
+++ src/FubuMVC.OwinHost/FubuMvcOwinMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FubuMVC.Core.Http;
+
+namespace FubuMVC.OwinHost
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public abstract class FubuMvcOwinMiddleware
+    {
+        private readonly AppFunc _inner;
+
+        protected FubuMvcOwinMiddleware(AppFunc inner)
+        {
+            _inner = inner;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            var continuation = Invoke(new OwinCurrentHttpRequest(environment), new OwinHttpWriter(environment));
+
+            return continuation.ToTask(environment, _inner);
+        }
+
+        public abstract MiddlewareContinuation Invoke(ICurrentHttpRequest request, IHttpWriter writer);
+    }
+}
---
+++ src/FubuMVC.OwinHost/MiddlewareContinuation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FubuMVC.Core;
+
+namespace FubuMVC.OwinHost
+{
+    public class MiddlewareContinuation
+    {
+        public static MiddlewareContinuation Continue(Action action = null)
+        {
+            return new MiddlewareContinuation {DoNext = DoNext.Continue, Action = action};
+        }
+
+        public static MiddlewareContinuation StopHere(Action action = null)
+        {
+            return new MiddlewareContinuation { DoNext = DoNext.Stop, Action = action };
+        }
+
+        public DoNext DoNext;
+        public Action Action;
+
+        public Task ToTask(IDictionary<string, object> environment, Func<IDictionary<string, object>, Task> inner)
+        {
+            if (DoNext == DoNext.Continue)
+            {
+                if (Action != null)
+                {
+                    Action();
+                }
+
+                return inner(environment);
+            }
+
+            return Task.Factory.StartNew(Action ?? (() => { }));
+        }
+    }
+}
