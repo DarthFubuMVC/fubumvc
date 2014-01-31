2ecc905da3c397651075b1492193a208a7cd7425
jeremydmiller <jeremydmiller@yahoo.com>
2014-01-31 10:39:19 -0600

    implemented WriteStatusCodeContinuation

---
+++ src/FubuMVC.OwinHost.Testing/Middleware/StaticFiles/WriteStatusCodeContinuationTester.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using FubuMVC.Core.Http;
+using FubuMVC.OwinHost.Middleware.StaticFiles;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace FubuMVC.OwinHost.Testing.Middleware.StaticFiles
+{
+    [TestFixture]
+    public class WriteStatusCodeContinuationTester
+    {
+        [Test]
+        public void writes_the_status_code_and_reason()
+        {
+            var writer = MockRepository.GenerateMock<IHttpWriter>();
+
+
+            var continuation = new WriteStatusCodeContinuation(writer, HttpStatusCode.Accepted, "it's all good");
+
+            continuation.Write(writer);
+
+            writer.AssertWasCalled(x => x.WriteResponseCode(HttpStatusCode.Accepted, "it's all good"));
+        }
+    }
+}
---
+++ src/FubuMVC.OwinHost/Middleware/StaticFiles/WriteStatusCodeContinuation.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using FubuMVC.Core;
+using FubuMVC.Core.Http;
+
+namespace FubuMVC.OwinHost.Middleware.StaticFiles
+{
+    public class WriteStatusCodeContinuation : WriterContinuation
+    {
+        private readonly HttpStatusCode _status;
+        private readonly string _reason;
+
+        public WriteStatusCodeContinuation(IHttpWriter writer, HttpStatusCode status, string reason)
+            : base(writer, DoNext.Stop)
+        {
+            _status = status;
+            _reason = reason;
+        }
+
+        public override void Write(IHttpWriter writer)
+        {
+            writer.WriteResponseCode(_status, _reason);
+        }
+
+        public HttpStatusCode Status
+        {
+            get { return _status; }
+        }
+
+        public string Reason
+        {
+            get { return _reason; }
+        }
+
+        protected bool Equals(WriteStatusCodeContinuation other)
+        {
+            return _status == other._status && string.Equals(_reason, other._reason);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((WriteStatusCodeContinuation) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((int) _status*397) ^ (_reason != null ? _reason.GetHashCode() : 0);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Stopping with Code: {0}, Reason: {1}", _status, _reason);
+        }
+    }
+}
