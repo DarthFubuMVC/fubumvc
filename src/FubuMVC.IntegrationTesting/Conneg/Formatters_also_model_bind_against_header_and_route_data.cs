6519c0ac3520c91c014e76ec76aecc0056fdfd1f
jeremydmiller <jeremydmiller@yahoo.com>
2014-02-26 08:43:19 -0600

    beginning of a test for model binding against route data

---
+++ src/FubuMVC.IntegrationTesting/Conneg/Formatters_also_model_bind_against_header_and_route_data.cs
@@ -0,0 +1,34 @@
+﻿using FubuMVC.Core;
+using FubuMVC.Katana;
+using FubuMVC.StructureMap;
+using NUnit.Framework;
+
+namespace FubuMVC.IntegrationTesting.Conneg
+{
+    [TestFixture, Ignore]
+    public class Formatters_also_model_bind_against_header_and_route_data
+    {
+        [Test]
+        public void applies_model_binding_after_serialization_with_json()
+        {
+            using (var server = FubuApplication.DefaultPolicies().StructureMap().RunEmbedded(autoFindPort: true))
+            {
+                // TODO -- need a better EndpointDriver first
+            }
+        }
+    }
+
+    public class RouteFilledInput
+    {
+
+        public string Name { get; set; }
+    }
+
+    public class RouteFilledEndpoint
+    {
+        public RouteFilledInput get_filled(RouteFilledInput input)
+        {
+            return input;
+        }
+    }
+}
