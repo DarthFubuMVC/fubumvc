d1ed3d07aa08e40382f4539137d929c18e140b71
jeremydmiller <jeremydmiller@yahoo.com>
2014-03-18 15:42:57 -0500

    integration tests for the 'none' master layout.  Closes GH-697

---
+++ src/FubuMVC.IntegrationTesting/Views/Razor/No_layout_with_use_none.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace FubuMVC.IntegrationTesting.Views.Razor
+{
+    [TestFixture]
+    public class No_layout_with_use_none : ViewIntegrationContext
+    {
+        public No_layout_with_use_none()
+        {
+            RazorView<NoLayoutModel>("View1").Write(@"
+@layout none
+
+<h1>Some stuff</h1>
+
+");
+
+            RazorView("Shared/Application").WriteLine("Text from the Application layout");
+        }
+
+        [Test]
+        public void should_not_use_any_layout_if_layout_equals_none()
+        {
+            Scenario.Get.Action<NoLayoutEndpoint>(x => x.get_no_layout());
+
+            Scenario.ContentShouldContain("<h1>Some stuff</h1>");
+            Scenario.ContentShouldNotContain("Text from the Application layout");
+        }
+    }
+
+    public class NoLayoutModel{}
+
+    public class NoLayoutEndpoint
+    {
+        public NoLayoutModel get_no_layout()
+        {
+            return new NoLayoutModel();
+        }
+    }
+}
