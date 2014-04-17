e8c84b37a77da16c9b873ef43a515900499c4cc0
jeremydmiller <jeremydmiller@yahoo.com>
2014-04-17 10:40:03 -0500

    finished importing fubu.exe. Closes GH-722

--- rakefile.rb
+++ rakefile.rb
@@ -18,7 +18,7 @@ require 'fuburake'
 	sln.assembly_bottle 'FubuMVC.Diagnostics'
 	
 	sln.integration_test = ['FubuMVC.IntegrationTesting']
-	sln.ci_steps = [:integration_test]
+	sln.ci_steps = [:integration_test, :archive_gem]
 end
 
 
@@ -28,4 +28,66 @@ task :full => [:default, :integration_test]
 desc "Target used for CI on Mono"
 task :mono_ci => [:compile, :unit_test, :integration_test]
 
+desc "Replaces the existing installed gem with the new version for local testing"
+task :local_gem => [:create_gem] do
+	sh 'gem uninstall fubu -a -x'
+	Dir.chdir 'pkg' do
+	    sh 'gem install fubu'
+    end
+end
 
+desc "Moves the gem to the archive folder"
+task :archive_gem => [:create_gem] do
+	copyOutputFiles "pkg", "*.gem", "artifacts"
+end
+
+desc "Outputs the command line usage"
+task :dump_usages => [:compile] do
+  sh "src/Fubu/bin/#{@solution.compilemode}/fubu.exe dump-usages fubu src/Fubu.Docs/fubu.cli.xml"
+end
+
+desc "Creates the gem for fubu.exe"
+task :create_gem => [:compile] do
+    require "rubygems/package"
+	cleanDirectory 'bin';	
+	cleanDirectory 'pkg'
+	
+	Dir.mkdir 'bin' unless Dir.exists?('bin')
+	Dir.mkdir 'pkg' unless Dir.exists?('pkg')
+	
+	copyOutputFiles "src/Fubu/bin/#{@solution.compilemode}", '*.dll', 'bin'
+	copyOutputFiles "src/Fubu/bin/#{@solution.compilemode}", 'Fubu.exe', 'bin'
+	copyOutputFiles "src/Fubu/bin/#{@solution.compilemode}", 'chromedriver.exe', 'bin'
+	FileUtils.cp_r 'templates', 'bin'
+	
+	FileUtils.copy 'fubu', 'bin'
+
+
+	spec = Gem::Specification.new do |s|
+	  s.platform    = Gem::Platform::RUBY
+	  s.name        = 'fubu'
+	  s.version     = @solution.options[:build_number]
+	  s.files = Dir['bin/**/*']
+	  s.bindir = 'bin'
+	  s.executables << 'fubu'
+	  
+	  s.summary     = 'Command line tools for FubuMVC development'
+	  s.description = 'Command line tools for FubuMVC development'
+	  
+	  s.add_runtime_dependency "rake",["~>10.0"]
+	  s.add_runtime_dependency "bundler",[">=1.3.5"]
+	  
+	  s.authors           = ['Jeremy D. Miller', 'Josh Arnold', 'Chad Myers', 'Joshua Flanagan']
+	  s.email             = 'fubumvc-devel@googlegroups.com'
+	  s.homepage          = 'http://fubu-project.org'
+	  s.rubyforge_project = 'fubu'
+	end   
+    puts "ON THE FLY SPEC FILES"
+    puts spec.files
+    puts "=========="
+
+    Gem::Package::build spec, true
+	
+	FileUtils.mv "fubu-#{@solution.options[:build_number]}.gem", "pkg/fubu-#{@solution.options[:build_number]}.gem"
+	
+end
