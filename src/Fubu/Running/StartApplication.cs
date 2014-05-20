f9e0c721e7dabfc86bba623b705a65467ea9ca3d
jeremydmiller <jeremydmiller@yahoo.com>
2014-05-19 20:00:07 -0500

    embedded the beginning of the web socket script text

--- src/Fubu/Running/StartApplication.cs
+++ src/Fubu/Running/StartApplication.cs
@@ -1,15 +1,32 @@
-﻿using FubuCore;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using FubuCore;
 
 namespace Fubu.Running
 {
     public class StartApplication
     {
+        public static readonly string WebSocketsContent = "WebsocketsRefresh.txt";
+
         public string ApplicationName { get; set; }
         public int PortNumber { get; set; }
         public string PhysicalPath { get; set; }
 
         public bool UseProductionMode { get; set; }
         public string HtmlHeadInjectedText { get; set; }
+
+        public void UseWebSocketsAddress(string address)
+        {
+            Assembly.GetExecutingAssembly().GetManifestResourceNames().Each(x => {
+                Debug.WriteLine(x);
+            });
+
+            var text = Assembly.GetExecutingAssembly().GetManifestResourceStream(GetType(),WebSocketsContent)
+                .ReadAllText();
+
+            HtmlHeadInjectedText = text.Replace("%WEB_SOCKET_ADDRESS%", address);
+        }
 
         public override string ToString()
         {
---
+++ src/fubu.Testing/Running/StartApplicationTester.cs
@@ -0,0 +1,21 @@
+﻿using Fubu.Running;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace fubu.Testing.Running
+{
+    [TestFixture]
+    public class StartApplicationTester
+    {
+        [Test]
+        public void use_web_sockets_address_sets_the_injection_text()
+        {
+            var start = new StartApplication();
+            start.UseWebSocketsAddress("ws:foo");
+
+            start.HtmlHeadInjectedText.ShouldContain("ws:foo");
+            start.HtmlHeadInjectedText.ShouldContain("<script");
+            start.HtmlHeadInjectedText.ShouldContain("</script>");
+        }
+    }
+}
