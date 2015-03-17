33506c7b091bcf97b11f50c82e495ab414050dbe
jeremydmiller <jeremydmiller@yahoo.com>
2015-03-17 07:26:58 -0500

    the top bar navigation is mostly in place

--- javascript/header.jsx
+++ javascript/header.jsx
@@ -8,11 +8,29 @@ var Nav = rb.Nav;
 var DropdownButton = rb.DropdownButton;
 var MenuItem = rb.MenuItem;
 var NavItem = rb.NavItem;
-
+var Router = require('react-router');
 
 
 var Header = React.createClass({
+	mixins: [ Router.State ],
+
 	render: function(){
+		var path = this.getPathname();
+		var sectionLinks = [];
+		if (path != '/'){
+			var sectionKey = path.split('/')[1];
+			var activeSection = FubuDiagnostics.section(sectionKey);
+			sectionLinks = activeSection.activeViews().map(view => {
+				return (
+					<NavItem href={view.anchor} title={view.description}>{view.title}</NavItem>
+				);
+			});
+		}
+
+		var sectionItems = FubuDiagnostics.sections.map(section => {
+			return (<NavItem href={section.anchor} title={section.description}>{section.title}</NavItem>);
+		});
+
 		return (
 		
 			<div>
@@ -21,10 +39,12 @@ var Header = React.createClass({
 						<a className="navbar-brand" href="#/">FubuMVC Diagnostics</a>
 
 				        <NavItem eventKey="4" href="#/">Application Name Here?</NavItem>
+						{sectionLinks}
 					</Nav>
 					<Nav right={true}>
-				          <NavItem eventKey="1" href="#/language">Fixtures and Grammars</NavItem>
-				          <NavItem eventKey="2" href="#/docs">Documentation</NavItem>
+					    <DropdownButton eventKey={1} title="Sections">
+				          {sectionItems}
+				        </DropdownButton>
 					</Nav>
 				</Navbar>
 
