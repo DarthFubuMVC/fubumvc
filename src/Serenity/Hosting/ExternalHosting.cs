7613cee45930ffd442312b90be9ccae7f5979e5d
jmarnold <rncodepoet@gmail.com>
2015-06-19 10:17:42 -0500

    Make the fully hosting configurable

---
+++ src/Serenity/Hosting/ExternalHosting.cs
@@ -0,0 +1,20 @@
+using FubuMVC.Core;
+
+namespace Serenity.Hosting
+{
+    public class ExternalHosting : ISerenityHosting
+    {
+        public IApplicationUnderTest Start(ApplicationSettings settings, FubuRuntime runtime, IBrowserLifecycle lifecycle)
+        {
+            var application = new ApplicationUnderTest(runtime, settings, lifecycle);
+            application.Ping();
+
+            return application;
+        }
+
+        public void Shutdown()
+        {
+            // Nothing
+        }
+    }
+}
---
+++ src/Serenity/Hosting/KatanaHosting.cs
@@ -0,0 +1,31 @@
+using FubuCore;
+using FubuMVC.Core;
+using FubuMVC.Core.Registration;
+using FubuMVC.Katana;
+
+namespace Serenity.Hosting
+{
+    public class KatanaHosting : ISerenityHosting
+    {
+        private EmbeddedFubuMvcServer _server;
+
+        public IApplicationUnderTest Start(ApplicationSettings settings, FubuRuntime runtime, IBrowserLifecycle lifecycle)
+        {
+            var port = PortFinder.FindPort(settings.Port);
+            _server = new EmbeddedFubuMvcServer(runtime, settings.PhysicalPath, port);
+
+            settings.RootUrl = _server.BaseAddress;
+
+            var graph = runtime.Factory.Get<BehaviorGraph>();
+
+            return new ApplicationUnderTest(runtime, settings, lifecycle);
+        }
+
+        public void Shutdown()
+        {
+            if (_server != null) _server.SafeDispose();
+
+            _server = null;
+        }
+    }
+}
