f6ef2b76540250c7a6f21b3aaf234f7883f1e311
jeremydmiller <jeremydmiller@yahoo.com>
2015-07-15 13:50:14 -0500

    got rid of the awful AppDomain let's look in every possible assembly thing. Closes GH-815

--- src/FubuTransportation/Web/SendsMessageActionSource.cs
+++ src/FubuTransportation/Web/SendsMessageActionSource.cs
@@ -9,36 +9,18 @@ namespace FubuTransportation.Web
 {
     public class SendsMessageActionSource : IActionSource
     {
-        public void Configure(BehaviorGraph graph)
-        {
-            var pool = TypePool.AppDomainTypes();
-            pool.TypesMatching(type => type.IsConcreteTypeOf<ISendMessages>()).Each(type => {
-                type.GetMethods(BindingFlags.Public | BindingFlags.Instance)
-                    .Where(ActionSource.IsCandidate)
-                    .Each(method => {
-                        var transform = new ActionCall(type, method);
-                        var chain = new BehaviorChain();
-                        chain.AddToEnd(transform);
-
-                        graph.AddChain(chain);
-                    });
-            });
-        }
 
         public IEnumerable<ActionCall> FindActions(Assembly applicationAssembly)
         {
-            var pool = TypePool.AppDomainTypes();
-            var types = pool.TypesMatching(type => type.IsConcreteTypeOf<ISendMessages>());
-            foreach (var type in types)
-            {
-                var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance)
-                    .Where(ActionSource.IsCandidate);
-
-                foreach (var method in methods)
+            return TypeRepository.FindTypes(applicationAssembly,
+                TypeClassification.Concretes | TypeClassification.Closed, t => t.CanBeCastTo<ISendMessages>())
+                .Result().SelectMany(type =>
                 {
-                    yield return new ActionCall(type, method);
-                }
-            }
+                    var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                        .Where(ActionSource.IsCandidate);
+
+                    return methods.Select(method => new ActionCall(type, method));
+                });
         }
     }
 }
