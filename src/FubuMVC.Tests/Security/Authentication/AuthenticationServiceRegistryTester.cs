6bdbfa23cff1dfb88449f0347b333abfacfd84bb
jeremydmiller <jeremydmiller@yahoo.com>
2015-07-22 09:21:25 -0500

    removed all coupling to ServiceGraph in registration unit tests

--- src/FubuMVC.Tests/Security/Authentication/AuthenticationServiceRegistryTester.cs
+++ src/FubuMVC.Tests/Security/Authentication/AuthenticationServiceRegistryTester.cs
@@ -1,13 +1,10 @@
-﻿using System.Linq;
-using FubuMVC.Core;
-using FubuMVC.Core.Registration;
+﻿using FubuMVC.Core;
 using FubuMVC.Core.Security.Authentication;
 using FubuMVC.Core.Security.Authentication.Auditing;
 using FubuMVC.Core.Security.Authentication.Cookies;
 using FubuMVC.Core.Security.Authentication.Membership;
 using FubuMVC.Core.Security.Authentication.Membership.FlatFile;
 using FubuMVC.Core.Security.Authentication.Tickets;
-using FubuTestingSupport;
 using NUnit.Framework;
 
 namespace FubuMVC.Tests.Security.Authentication
@@ -15,95 +12,28 @@ namespace FubuMVC.Tests.Security.Authentication
     [TestFixture]
     public class AuthenticationServiceRegistryTester
     {
-        private ServiceGraph theServiceGraph;
-
-        [SetUp]
-        public void SetUp()
+        [Test]
+        public void services_are_registered()
         {
             var registry = new FubuRegistry();
-            registry.Services<AuthenticationServiceRegistry>();
+            registry.Features.Authentication.Enable(true);
 
-            theServiceGraph = BehaviorGraph.BuildFrom(registry).Services;
-        }
+            using (var runtime = FubuApplication.For(registry).Bootstrap())
+            {
+                runtime.Container.DefaultRegistrationIs<ILockedOutRule, LockedOutRule>();
+                runtime.Container.DefaultRegistrationIs<IAuthenticationService, AuthenticationService>();
+                runtime.Container.ShouldHaveRegistration<IActivator, AuthenticationIsConfigured>();
 
-        [Test]
-        public void registers_the_LockedOutRule()
-        {
-            theDefaultServiceIs<ILockedOutRule, LockedOutRule>();
-        }
-
-        [Test]
-        public void registers_IAuthenticationService()
-        {
-            theDefaultServiceIs<IAuthenticationService, AuthenticationService>();
-        }
-
-        [Test]
-        public void registers_the_AuthenticationIsConfigured()
-        {
-            theServiceGraph.ServicesFor<IActivator>().Select(x => x.Type)
-                .ShouldContain(typeof(AuthenticationIsConfigured));
-        }
-
-        [Test]
-        public void registers_default_IAuthenticationSession()
-        {
-            theDefaultServiceIs<IAuthenticationSession, TicketAuthenticationSession>();
-        }
-
-        [Test]
-        public void registers_default_IPrincipalContext()
-        {
-            theDefaultServiceIs<IPrincipalContext, ThreadPrincipalContext>();
-        }
-
-        [Test]
-        public void registers_default_ITicketSource()
-        {
-            theDefaultServiceIs<ITicketSource, CookieTicketSource>();
-        }
-
-        [Test]
-        public void registers_default_ILoginCookieService()
-        {
-            theDefaultServiceIs<ILoginCookieService, LoginCookieService>();
-        }
-
-        [Test]
-        public void registers_default_IEncryptor()
-        {
-            theDefaultServiceIs<IEncryptor, Encryptor>();
-        }
-
-        [Test]
-        public void registers_default_ILoginCookies()
-        {
-            theDefaultServiceIs<ILoginCookies, BasicFubuLoginCookies>();
-        }
-
-        [Test]
-        public void registers_default_IAuthenticationRedirector()
-        {
-            theDefaultServiceIs<IAuthenticationRedirector, AuthenticationRedirector>();
-        }
-
-
-        [Test]
-        public void registers_default_membership_reposotory()
-        {
-            theDefaultServiceIs<IMembershipRepository, FlatFileMembershipRepository>();
-        }
-
-        [Test]
-        public void nullo_auditor_by_default()
-        {
-            theDefaultServiceIs<ILoginAuditor, NulloLoginAuditor>();
-        }
-
-        private void theDefaultServiceIs<TPlugin, TImplementation>()
-        {
-            theServiceGraph.DefaultServiceFor<TPlugin>()
-                .Type.ShouldEqual(typeof(TImplementation));
+                runtime.Container.DefaultRegistrationIs<IAuthenticationSession, TicketAuthenticationSession>();
+                runtime.Container.DefaultRegistrationIs<IPrincipalContext, ThreadPrincipalContext>();
+                runtime.Container.DefaultRegistrationIs<ITicketSource, CookieTicketSource>();
+                runtime.Container.DefaultRegistrationIs<ILoginCookieService, LoginCookieService>();
+                runtime.Container.DefaultRegistrationIs<IEncryptor, Encryptor>();
+                runtime.Container.DefaultRegistrationIs<ILoginCookies, BasicFubuLoginCookies>();
+                runtime.Container.DefaultRegistrationIs<IAuthenticationRedirector, AuthenticationRedirector>();
+                runtime.Container.DefaultRegistrationIs<IMembershipRepository, FlatFileMembershipRepository>();
+                runtime.Container.DefaultRegistrationIs<ILoginAuditor, NulloLoginAuditor>();
+            }
         }
     }
 }
--- src/FubuMVC.Tests/ServiceBus/Monitoring/Service_registration_specs.cs
+++ src/FubuMVC.Tests/ServiceBus/Monitoring/Service_registration_specs.cs
@@ -1,57 +1,23 @@
-﻿using System;
-using System.Linq;
-using FubuCore.Logging;
-using FubuMVC.Core;
-using FubuMVC.Core.Registration;
+﻿using FubuCore.Logging;
+using FubuMVC.Core.ServiceBus.Configuration;
 using FubuMVC.Core.ServiceBus.Monitoring;
-using FubuTestingSupport;
 using NUnit.Framework;
 
-namespace FubuTransportation.Testing.Monitoring
+namespace FubuMVC.Tests.ServiceBus.Monitoring
 {
     [TestFixture]
     public class Service_registration_specs
     {
-        private ServiceGraph services;
-
-        [TestFixtureSetUp]
-        public void SetUp()
-        {
-            FubuMode.RemoveTestingMode();
-            var registry = new FubuRegistry();
-            registry.Services<MonitoringServiceRegistry>();
-            services = BehaviorGraph.BuildFrom(registry)
-                .Services;
-        }
-
-        private void registeredTypeIs<TService, TImplementation>()
-        {
-            registeredTypeIs(typeof(TService), typeof(TImplementation));
-        }
-
-        private void registeredTypeIs(Type service, Type implementation)
-        {
-            services.DefaultServiceFor(service)
-                .Type.ShouldEqual(implementation);
-        }
-
         [Test]
-        public void log_modifier_for_persistent_task_messages_is_registered()
+        public void registrations()
         {
-            services.ServicesFor<ILogModifier>().Any(x => x.Type == typeof(PersistentTaskMessageModifier))
-                .ShouldBeTrue();
-        }
+            using (var runtime = FubuTransport.DefaultPolicies().Bootstrap())
+            {
+                runtime.Container.ShouldHaveRegistration<ILogModifier, PersistentTaskMessageModifier>();
 
-        [Test]
-        public void controller_is_registered()
-        {
-            registeredTypeIs<IPersistentTaskController, PersistentTaskController>();
-        }
-
-        [Test]
-        public void transport_peer_repository()
-        {
-            registeredTypeIs<ITaskMonitoringSource, TaskMonitoringSource>();
+                runtime.Container.DefaultRegistrationIs<IPersistentTaskController, PersistentTaskController>();
+                runtime.Container.DefaultRegistrationIs<ITaskMonitoringSource, TaskMonitoringSource>();
+            }
         }
     }
 }
