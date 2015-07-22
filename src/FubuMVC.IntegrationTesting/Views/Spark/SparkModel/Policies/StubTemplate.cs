da332a2c93fa540302538dd2f4480edf4a9f39cb
jeremydmiller <jeremydmiller@yahoo.com>
2015-07-22 08:28:04 -0500

    moved all the spark engine tests into integration testing. Closes GH-855

---
+++ src/FubuMVC.IntegrationTesting/Views/Spark/SparkModel/Policies/StubTemplate.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using FubuMVC.Core.View;
+using FubuMVC.Core.View.Model;
+using FubuMVC.Core.View.Rendering;
+using FubuMVC.Spark.SparkModel;
+
+namespace FubuMVC.IntegrationTesting.Views.Spark.SparkModel.Policies
+{
+    public class StubTemplate : ISparkTemplate
+    {
+        public string Origin
+        {
+            get; set;
+        }
+
+        public string FilePath
+        {
+            get;
+            set;
+        }
+
+        public string RootPath
+        {
+            get;
+            set;
+        }
+
+        public string ViewPath
+        {
+            get;
+            set;
+        }
+
+
+        public string Namespace { get; set; }
+        public Type ViewModel { get; set; }
+        public string ProfileName { get; set; }
+
+        public Parsing Parsing { get; private set; }
+
+        public string RelativePath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DirectoryPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RelativeDirectoryPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FromHost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPartial()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FullName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AttachViewModels(ViewTypePool types, ITemplateLogger logger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITemplateFile Master { get; set; }
+        public IRenderableView GetView()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRenderableView GetPartialView()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AttachLayouts(string defaultLayoutName, IViewFacility facility, ITemplateFolder folder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/FubuMVC.IntegrationTesting/Views/Spark/modifies_the_WatchSettings.cs
@@ -0,0 +1,25 @@
+﻿using FubuMVC.Core.Assets;
+using FubuMVC.Core.Registration;
+using FubuMVC.Spark;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuMVC.IntegrationTesting.Views.Spark
+{
+    [TestFixture]
+    public class modifies_the_WatchSettings
+    {
+        [Test]
+        public void adds_spark_and_bindings_xml_file()
+        {
+            var graph = BehaviorGraph.BuildFrom(x => {
+                x.Import<SparkViewFacility>();
+            });
+
+            var settings = graph.Settings.Get<AssetSettings>();
+        
+            settings.ContentMatches.ShouldContain("*.spark");
+            settings.ContentMatches.ShouldContain("bindings.xml");
+        }
+    }
+}
