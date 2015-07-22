b0d7668d7bc25a382342f5aa0dee6f1c107c9dba
jeremydmiller <jeremydmiller@yahoo.com>
2015-07-22 08:47:38 -0500

    moved fubu.Testing into integration tests. Closes GH-856

---
+++ src/FubuMVC.IntegrationTesting/CommandLine/usage_graph_smoke_tester.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FubuCore.CommandLine;
+using NUnit.Framework;
+
+namespace FubuMVC.IntegrationTesting.CommandLine
+{
+    [TestFixture]
+    public class usage_graph_smoke_tester
+    {
+        [Test]
+        public void all_commands_usage_graph_works()
+        {
+            var factory = new CommandFactory();
+            factory.RegisterCommands(typeof(IFubuCommand).Assembly);
+
+            factory.AllCommandTypes().Each(t =>
+            {
+
+                try
+                {
+                    var usageGraph = new UsageGraph(t);
+                    usageGraph.WriteUsages("fubu");
+
+                    Assert.IsTrue(usageGraph.Usages.Any(), "Found usages for " + t.FullName);
+                }
+                catch (Exception e)
+                {
+                    throw new ApplicationException("Command type:  " + t.FullName, e);
+                }
+            });
+        }
+
+        [Test]
+        public void vdir()
+        {
+            new UsageGraph(typeof(CreateVdirCommand)).WriteUsages("fubu");
+        }
+
+        public class FakeInput{}
+        class CreateVdirCommand : FubuCommand<FakeInput>
+        {
+            public override bool Execute(FakeInput input)
+            {
+                return true;
+            }
+        }
+    }
+}
