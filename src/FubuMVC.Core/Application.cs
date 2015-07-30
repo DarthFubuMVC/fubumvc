01ed91daed937b594ec84f6844da9e35695a1d05
jeremydmiller <jeremydmiller@yahoo.com>
2015-07-30 07:21:07 -0500

    minor additions to Application

--- src/FubuMVC.Core/Application.cs
+++ src/FubuMVC.Core/Application.cs
@@ -1,12 +1,37 @@
-﻿namespace FubuMVC.Core
+﻿using FubuMVC.Core.Http.Hosting;
+
+namespace FubuMVC.Core
 {
     public class Application<T> where T : FubuRegistry, new()
     {
-         public readonly T Registry = new T(); 
+         public readonly T Registry;
+
+        public Application(T registry)
+        {
+            Registry = registry;
+        }
+
+        public Application()
+        {
+            Registry = new T();
+        }
+
+        public string RootPath;
+        //public int Port;
+        //public IHost Host;
+        //public string Mode;
+
+
     }
 
     public class BasicApplication : Application<FubuRegistry>
     {
-        
+        public BasicApplication(FubuRegistry registry) : base(registry)
+        {
+        }
+
+        public BasicApplication()
+        {
+        }
     }
 }
