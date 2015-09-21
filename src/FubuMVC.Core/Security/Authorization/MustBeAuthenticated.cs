7fcd7a2ba767059cabeb26b5fabf0b78872f8ffd
jeremydmiller <jeremydmiller@yahoo.com>
2015-09-21 08:51:33 -0500

    MustBeAuthenticated authorization rule. Closes GH-953

---
+++ src/FubuMVC.Core/Security/Authorization/MustBeAuthenticated.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Security.Principal;
+using System.Threading;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.Nodes;
+using FubuMVC.Core.Runtime;
+
+namespace FubuMVC.Core.Security.Authorization
+{
+    public class MustBeAuthenticatedAttribute : ModifyChainAttribute
+    {
+        public override void Alter(ActionCall call)
+        {
+            call.ParentChain().Authorization.AddPolicy(new MustBeAuthenticated());
+        }
+    }
+
+    /// <summary>
+    /// The user must be authenticated
+    /// </summary>
+    public class MustBeAuthenticated : IAuthorizationPolicy
+    {
+        public AuthorizationRight RightsFor(IFubuRequestContext request)
+        {
+            var principal = Thread.CurrentPrincipal;
+            return DetermineRights(principal);
+        }
+
+        public static AuthorizationRight DetermineRights(IPrincipal principal)
+        {
+            if (principal == null || principal.Identity == null) return AuthorizationRight.Deny;
+
+            return principal.Identity.IsAuthenticated
+                ? AuthorizationRight.Allow
+                : AuthorizationRight.Deny;
+        }
+    }
+}
--- src/FubuMVC.Tests/Security/Authorization/AllowRoleTester.cs
+++ src/FubuMVC.Tests/Security/Authorization/AllowRoleTester.cs
@@ -1,6 +1,9 @@
+using System.Linq;
 using System.Security.Principal;
 using System.Threading;
+using FubuMVC.Core.Registration.Nodes;
 using FubuMVC.Core.Security.Authorization;
+using Rhino.Mocks;
 using Shouldly;
 using NUnit.Framework;
 
@@ -26,6 +29,53 @@ namespace FubuMVC.Tests.Security.Authorization
         public void negative_test_for_a_role_that_is_not_in_the_current_principal()
         {
             new AllowRole("not in principal").RightsFor(null).ShouldBe(AuthorizationRight.None);
+        }
+    }
+
+    [TestFixture]
+    public class MustBeAuthenticatedTester
+    {
+        [Test]
+        public void no_principal_no_authorization()
+        {
+            MustBeAuthenticated.DetermineRights(null).ShouldBe(AuthorizationRight.Deny);
+        }
+
+        [Test]
+        public void not_authenticated_principal()
+        {
+            var principal = MockRepository.GenerateMock<IPrincipal>();
+            var identity = MockRepository.GenerateMock<IIdentity>();
+            identity.Stub(x => x.IsAuthenticated).Return(false);
+            principal.Stub(x => x.Identity).Return(identity);
+
+            MustBeAuthenticated.DetermineRights(principal).ShouldBe(AuthorizationRight.Deny);
+        }
+
+        [Test]
+        public void is_authenticated()
+        {
+            var principal = MockRepository.GenerateMock<IPrincipal>();
+            var identity = MockRepository.GenerateMock<IIdentity>();
+            identity.Stub(x => x.IsAuthenticated).Return(true);
+            principal.Stub(x => x.Identity).Return(identity);
+
+            MustBeAuthenticated.DetermineRights(principal).ShouldBe(AuthorizationRight.Allow);
+        }
+
+        [Test]
+        public void attribute_places_the_rule()
+        {
+            var chain = BehaviorChain.For<MustBeAuthenticatedTester>(x => x.Go());
+
+            chain.Authorization.Policies.Single()
+                .ShouldBeOfType<MustBeAuthenticated>();
+        }
+
+        [MustBeAuthenticated]
+        public string Go()
+        {
+            return "ok";
         }
     }
 }
