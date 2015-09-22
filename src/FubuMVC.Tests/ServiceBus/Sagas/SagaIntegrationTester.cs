89c7eb5ff739bcbfa0f6a7888de63e8cb75398b8
jeremydmiller <jeremydmiller@yahoo.com>
2015-09-22 10:17:15 -0500

    taking SagaIntegrationTester out of testing

--- src/FubuMVC.Tests/ServiceBus/Sagas/SagaIntegrationTester.cs
+++ src/FubuMVC.Tests/ServiceBus/Sagas/SagaIntegrationTester.cs
@@ -19,7 +19,7 @@ using StructureMap;
 
 namespace FubuMVC.Tests.ServiceBus.Sagas
 {
-    [TestFixture]
+    [TestFixture, Explicit("About to be converted to ST")]
     public class SagaIntegrationTester
     {
         private SagaLogger theLogger;
@@ -146,7 +146,7 @@ namespace FubuMVC.Tests.ServiceBus.Sagas
     {
         public SagaTestRegistry()
         {
-            AlterSettings<TransportSettings>(x => x.EnableInMemoryTransport = true);
+            //AlterSettings<TransportSettings>(x => x.EnableInMemoryTransport = true);
 
             Channel(x => x.Queue)
                 .AcceptsMessagesInAssemblyContainingType<SagaTestRegistry>()
