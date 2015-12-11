379a664146f386fcc7550029832f21aa95c6b966
jeremydmiller <jeremydmiller@yahoo.com>
2015-12-11 07:16:26 -0600

    updated the StructureMap nuget version in nuspec and paket to released version of SM

--- rakefile.rb
+++ rakefile.rb
@@ -65,7 +65,7 @@ end
 
 desc 'Compile the code'
 task :compile => [:npm, :clean, :version] do
-	sh "paket.exe install"
+	sh "paket.exe restore"
 	sh "C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe src/FubuMVC.sln   /property:Configuration=#{COMPILE_TARGET} /v:m /t:rebuild /nr:False /maxcpucount:2"
 end
 
