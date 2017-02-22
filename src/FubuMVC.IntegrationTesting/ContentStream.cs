4616ddf5757bfeceabac469270176f7138560953
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-02-22 13:06:09 -0600

    moved ContentStream out of the View tests

---
+++ src/FubuMVC.IntegrationTesting/ContentStream.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using FubuMVC.IntegrationTesting.Views;
+
+namespace FubuMVC.IntegrationTesting
+{
+    public class ContentStream
+    {
+        private readonly string _path;
+        private readonly StringWriter _writer = new StringWriter();
+
+        public ContentStream(string folder, string name, string extension)
+        {
+            _path = Path.Combine(folder, name + extension);
+        }
+
+        public ContentStream WriteLine(string format, params object[] parameters)
+        {
+            _writer.WriteLine(format, parameters);
+            return this;
+        }
+
+        public void Write(string text)
+        {
+            _writer.WriteLine(text.Replace("'", "\"").TrimStart());
+        }
+
+        public void DumpContents()
+        {
+            ViewIntegrationContext.fileSystem.WriteStringToFile(_path, _writer.ToString());
+        }
+    }
+}
--- src/FubuMVC.IntegrationTesting/Views/ViewIntegrationContext.cs
+++ src/FubuMVC.IntegrationTesting/Views/ViewIntegrationContext.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading;
 using FubuCore;
@@ -166,33 +165,6 @@ namespace FubuMVC.IntegrationTesting.Views
         protected ITemplateFile ViewForModel<T>()
         {
             return Views.ViewsFor(typeof (T)).OfType<ITemplateFile>().FirstOrDefault();
-        }
-    }
-
-    public class ContentStream
-    {
-        private readonly string _path;
-        private readonly StringWriter _writer = new StringWriter();
-
-        public ContentStream(string folder, string name, string extension)
-        {
-            _path = Path.Combine(folder, name + extension);
-        }
-
-        public ContentStream WriteLine(string format, params object[] parameters)
-        {
-            _writer.WriteLine(format, parameters);
-            return this;
-        }
-
-        public void Write(string text)
-        {
-            _writer.WriteLine(text.Replace("'", "\"").TrimStart());
-        }
-
-        public void DumpContents()
-        {
-            ViewIntegrationContext.fileSystem.WriteStringToFile(_path, _writer.ToString());
         }
     }
 
