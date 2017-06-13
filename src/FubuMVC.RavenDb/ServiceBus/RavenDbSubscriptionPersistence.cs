8f772ae98fb8c0558e8e5d609d392e8e9e97698e
JordanZaerr <jordan.zaerr@extendhealth.com>
2017-06-13 09:58:06 -0600

    Fix large number of subscriptions not loading appropriately

--- src/FubuMVC.RavenDb/ServiceBus/RavenDbSubscriptionPersistence.cs
+++ src/FubuMVC.RavenDb/ServiceBus/RavenDbSubscriptionPersistence.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using FubuMVC.Core.ServiceBus.Subscriptions;
+using Raven.Abstractions.Indexing;
 using Raven.Client;
+using Raven.Client.Linq;
 
 namespace FubuMVC.RavenDb.ServiceBus
 {
@@ -10,21 +12,31 @@ namespace FubuMVC.RavenDb.ServiceBus
     {
         private readonly ITransaction _transaction;
         private readonly IDocumentStore _store;
+        private const string SubscriptionIndex = "Subscriptions/ByNodeNameAndRole";
 
         public RavenDbSubscriptionPersistence(ITransaction transaction, IDocumentStore store)
         {
             _transaction = transaction;
             _store = store;
+            PushSubscriptionIndex();
+        }
+
+        public void PushSubscriptionIndex()
+        {
+            _store.DatabaseCommands.PutIndex(SubscriptionIndex, new IndexDefinition
+            {
+                Map = "from doc in docs.Subscriptions select new { NodeName = doc.NodeName, Role = doc.Role }",
+
+            }, true);
         }
 
         public IEnumerable<Subscription> LoadSubscriptions(string name, SubscriptionRole role)
         {
             using (var session = _store.OpenSession())
             {
-                return session.Query<Subscription>()
-                    .Customize(x => x.WaitForNonStaleResultsAsOfLastWrite())
-                    .Where(x => x.NodeName == name && x.Role == role)
-                    .ToList();
+                var query = session.Query<Subscription>(SubscriptionIndex)
+                        .Where(x => x.NodeName == name && x.Role == role);
+                return Stream(session, query).ToList();
             }
         }
 
@@ -61,7 +73,6 @@ namespace FubuMVC.RavenDb.ServiceBus
         {
             using (var session = _store.OpenSession())
             {
-
                 return session.Query<TransportNode>()
                     .Customize(x => x.WaitForNonStaleResultsAsOfLastWrite())
                     .ToArray();
@@ -72,9 +83,7 @@ namespace FubuMVC.RavenDb.ServiceBus
         {
             using (var session = _store.OpenSession())
             {
-                return session
-                    .Query<Subscription>()
-                    .Customize(x => x.WaitForNonStaleResultsAsOfLastWrite()).ToArray();
+                return Stream(session, session.Query<Subscription>(SubscriptionIndex)).ToArray();
             }
         }
 
@@ -104,6 +113,21 @@ namespace FubuMVC.RavenDb.ServiceBus
                 var docs = session.Load<Subscription>(subscriptions.Select(x => x.Id).Cast<ValueType>());
                 docs.Each(x => session.Delete(x));
             });
+        }
+
+        private IEnumerable<T> Stream<T>(IDocumentSession session, IRavenQueryable<T> query)
+        {
+            //Stream doesn't support waiting for non-stale results.
+            //Stream also throws an error if the stream is empty.
+            var result = query.Customize(x => x.WaitForNonStaleResultsAsOfLastWrite()).FirstOrDefault();
+            if (result == null)
+            {
+                yield break;
+            }
+
+            var stream = session.Advanced.Stream(query);
+            while (stream.MoveNext())
+                yield return stream.Current.Document;
         }
     }
 }
